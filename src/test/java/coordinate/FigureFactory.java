6310f1307e4745808ffcd43372a777288eb90f09
Kim Young Jae <youngjae.havi@linecorp.com>
2019-08-01 22:15:26 +0900

    feat: FigureFactory remove if

--- src/test/java/coordinate/FigureFactory.java
+++ src/test/java/coordinate/FigureFactory.java
@@ -1,21 +1,36 @@
 package coordinate;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class FigureFactory {
     static Figure getInstance(List<Point> points) {
-        if (points.size() == 2) {
-            return new Line(points);
+        return Arrays.stream(FigureCreatorType.values())
+                .filter(f -> f.getSize() == points.size())
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 도형입니다."))
+                .create(points);
+    }
+
+    public enum FigureCreatorType implements FigureCreator {
+        LINE(2) {
+            public Figure create(List<Point> points) { return new Line(points); }
+        },
+        TRIANGLE(3) {
+            public Figure create(List<Point> points) { return new Triangle(points); }
+        },
+        RECTANGL(4) {
+            public Figure create(List<Point> points) { return new Rectangle(points); }
+        };
+
+        private int size;
+
+        FigureCreatorType(int size) {
+            this.size = size;
         }
 
-        if (points.size() == 3) {
-            return new Triangle(points);
+        public int getSize() {
+            return size;
         }
-
-        if (points.size() == 4) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
     }
 }
