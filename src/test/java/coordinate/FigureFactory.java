60470294e30447d4c16c620a49744ba2011b8316
Taehyun <Htaehyun@gmail.com>
2019-08-01 23:58:37 +0900

    feat if문 제거 연습

--- src/test/java/coordinate/FigureFactory.java
+++ src/test/java/coordinate/FigureFactory.java
@@ -1,21 +1,23 @@
 package coordinate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
 public class FigureFactory {
+    private static Map<Integer, Function<List<Point>, Figure>> figureMap;
+    static {
+        figureMap = new HashMap<>();
+        figureMap.put(2, Line::new);
+        figureMap.put(3, Triangle::new);
+        figureMap.put(4, Rectangle::new);
+    }
+
     static Figure getInstance(List<Point> points) {
-        if (points.size() == 2) {
-            return new Line(points);
-        }
-
-        if (points.size() == 3) {
-            return new Triangle(points);
-        }
-
-        if (points.size() == 4) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+        return Optional.ofNullable(figureMap.get(points.size()))
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 도형입니다."))
+                .apply(points);
     }
 }
--- src/test/java/webserver/http/QueryStringTest.java
+++ src/test/java/webserver/http/QueryStringTest.java
@@ -1,6 +1,5 @@
 package webserver.http;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
