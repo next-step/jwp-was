01b4f9d70b2659db68e5f769030658f22c57c2db
ehdtnn <ehdtnn@gmail.com>
2019-08-01 21:22:52 +0900

    request review test (#18)
    * implement requestLine
    
    * implement requestLine

---
+++ src/test/java/webserver/RequestLineTest.java
@@ -0,0 +1,17 @@
+package webserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestLineTest {
+    @Test
+    void parse() {
+        RequestLine requestLine = RequestLine.parse("GET /users HTTP/1.1");
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+    }
+
+}
+
+
