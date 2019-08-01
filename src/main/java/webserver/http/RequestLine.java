4508be21f49ef793dbec31e2fb4757840e293e18
hongsii <hongsii.dev@gmail.com>
2019-08-02 00:31:13 +0900

    feat : QueryString 구현

--- src/main/java/webserver/http/RequestLine.java
+++ src/main/java/webserver/http/RequestLine.java
@@ -1,18 +1,43 @@
 package webserver.http;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestLine {
+
+    private static final String SEPARATOR = " ";
+    private static final String QUERY_PREFIX = "\\?";
+    private static final String QUERY_DELIMITER = "&";
+    private static final String QUERY_KEY_VALUE_DELIMITER = "=";
 
     private String method;
     private String path;
+    private Map<String, String> params;
 
-    public RequestLine(String method, String path) {
+    private RequestLine(String method, String path, Map<String, String> params) {
         this.method = method;
         this.path = path;
+        this.params = Collections.unmodifiableMap(params);
     }
 
     public static RequestLine parse(String rawRequestLine) {
-        String[] requestLine = rawRequestLine.split(" ");
-        return new RequestLine(requestLine[0], requestLine[1]);
+        String[] requestLine = rawRequestLine.split(SEPARATOR);
+        String[] requestUri = requestLine[1].split(QUERY_PREFIX);
+        return new RequestLine(requestLine[0], requestUri[0], parseQueryString(requestUri));
+    }
+
+    private static Map<String, String> parseQueryString(String[] requestUri) {
+        if (requestUri.length == 1) {
+            return Collections.emptyMap();
+        }
+
+        Map<String, String> params = new HashMap<>();
+        for (String param : requestUri[1].split(QUERY_DELIMITER)) {
+            String[] entry = param.split(QUERY_KEY_VALUE_DELIMITER);
+            params.put(entry[0], entry[1]);
+        }
+        return params;
     }
 
     public String getMethod() {
@@ -21,5 +46,9 @@ public class RequestLine {
 
     public String getPath() {
         return path;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
     }
 }
--- src/test/java/webserver/http/RequestLineTest.java
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -1,5 +1,6 @@
 package webserver.http;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,10 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RequestLineTest {
 
     @Test
+    @DisplayName("Parse Request-Line")
     void parse() {
-        RequestLine requestLine = RequestLine.parse("GET /users HTTP/1.1");
+        RequestLine requestLine = RequestLine.parse("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1");
 
         assertThat(requestLine.getMethod()).isEqualTo("GET");
         assertThat(requestLine.getPath()).isEqualTo("/users");
+        assertThat(requestLine.getParams())
+                .hasSize(3)
+                .containsEntry("userId", "javajigi")
+                .containsEntry("password", "password")
+                .containsEntry("name", "JaeSung");
     }
 }
