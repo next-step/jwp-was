f615c3ab4965e02f83de1259eac63540a6a4d233
Jinho Yim <jinhoyim@outlook.com>
2019-08-01 21:22:19 +0900

    initial implement RequestLine (#20)

---
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -0,0 +1,13 @@
+package webserver.http;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestLineTest {
+    @Test
+    void parse() {
+        RequestLine requestLine = RequestLine.parse("Get /users HTTP/1.1");
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+    }
+}
