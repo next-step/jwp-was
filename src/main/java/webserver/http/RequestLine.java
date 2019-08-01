5bb2ff5af4a53b782aee80b040b6815308b43f24
korkorna <korkorna@gmail.com>
2019-08-02 00:36:19 +0900

    feat(querystring paring)

--- src/main/java/webserver/http/RequestLine.java
+++ src/main/java/webserver/http/RequestLine.java
@@ -3,11 +3,11 @@ package webserver.http;
 public class RequestLine {
     private final String version;
     private final String method;
-    private final String path;
+    private final Parts parts;
 
-    public RequestLine(String method, String path, String version) {
+    public RequestLine(String method, Parts parts, String version) {
         this.method = method;
-        this.path = path;
+        this.parts = parts;
         this.version = version;
     }
 
@@ -20,7 +20,11 @@ public class RequestLine {
     }
 
     public String getPath() {
-        return path;
+        return parts.getPath();
+    }
+
+    public String getParameters(String key) {
+        return parts.getQuery(key);
     }
 
     public static RequestLine parse(String requestLine) {
@@ -28,6 +32,7 @@ public class RequestLine {
         String method = splitedRequestLine[0];
         String path = splitedRequestLine[1];
         String version = splitedRequestLine[2];
-        return new RequestLine(method, path, version);
+        return new RequestLine(method, Parts.parse(path), version);
     }
+
 }
