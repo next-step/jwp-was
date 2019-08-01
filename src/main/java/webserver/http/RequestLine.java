6256d7ad064afd288ec5abbc8406afa2c00ec489
pplenty <newtype26@naver.com>
2019-08-01 21:21:56 +0900

    Step0 (#13)
    * request line parser 구현
    
    * request line parser 구현
    
    * request line package 이동

---
+++ src/main/java/webserver/http/RequestLine.java
@@ -0,0 +1,30 @@
+package webserver.http;
+
+/**
+ * @author : yusik
+ * @date : 2019-08-01
+ */
+public class RequestLine {
+
+    private static final String LINE_SEPARATOR = " ";
+    private String method;
+    private String path;
+
+    public RequestLine(String method, String path) {
+        this.method = method;
+        this.path = path;
+    }
+
+    public static RequestLine parse(String requestLine) {
+        String[] parsedLines = requestLine.split(LINE_SEPARATOR);
+        return new RequestLine(parsedLines[0], parsedLines[1]);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
---
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -0,0 +1,19 @@
+package webserver.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author : yusik
+ * @date : 2019-08-01
+ */
+public class RequestLineTest {
+
+    @Test
+    void parse() {
+        RequestLine requestLine = RequestLine.parse("GET /users HTTP/1.1");
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+    }
+}
