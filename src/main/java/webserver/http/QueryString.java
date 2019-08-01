87d2a982c3b506507c71d191eff13e2c10bd1b41
Taehyun <Htaehyun@gmail.com>
2019-08-02 00:22:37 +0900

    refactor QueryString parse 메소드 리펙토링

--- src/main/java/webserver/http/QueryString.java
+++ src/main/java/webserver/http/QueryString.java
@@ -2,12 +2,16 @@ package webserver.http;
 
 import javafx.util.Pair;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class QueryString {
+    private final static String PATH_SEPARATOR = "?";
+    private final static String PARAMETER_SEPARATOR = "&";
+    private final static String KEY_VALUE_SEPARATOR = "=";
+
     private Map<String, Object> parameterMap;
 
     private QueryString(Map<String, Object> parameterMap) {
@@ -15,18 +19,22 @@ public class QueryString {
     }
 
     public static QueryString parse(String path) {
-        String queryString = path.substring(path.indexOf("?") + 1);
-        String[] values = queryString.split("&");
-        Map<String, Object> parameterMap = Stream.of(values)
-                .filter(value -> value.contains("="))
+        Map<String, Object> parameterMap = getParameterStrings(path).stream()
                 .map(QueryString::makeKeyValuePair)
                 .collect(Collectors.toMap(Pair::getKey, Pair::getValue));
 
         return new QueryString(parameterMap);
     }
 
+    private static List<String> getParameterStrings(String path) {
+        String queryString = path.substring(path.indexOf(PATH_SEPARATOR) + 1);
+        return Stream.of(queryString.split(PARAMETER_SEPARATOR))
+                .filter(value -> value.contains(KEY_VALUE_SEPARATOR))
+                .collect(Collectors.toList());
+    }
+
     private static Pair<String, Object> makeKeyValuePair(String v) {
-        String[] param = v.split("=");
+        String[] param = v.split(KEY_VALUE_SEPARATOR);
         return new Pair<>(param[0], param[1]);
     }
 
