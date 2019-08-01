4fc25b7ad3f60adb99ed9eaf8a729be5e1241bc0
Kim Young Jae <youngjae.havi@linecorp.com>
2019-08-01 21:53:31 +0900

    feat: query string parsing function

--- src/main/java/webserver/RequestLine.java
+++ src/main/java/webserver/RequestLine.java
@@ -1,22 +1,55 @@
 package webserver;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+
+import static com.github.jknack.handlebars.internal.lang3.StringUtils.SPACE;
+import static utils.StringUtils.*;
+
 /**
  * Created by youngjae.havi on 2019-08-01
  */
 public class RequestLine {
     private String method;
     private String path;
+    private MultiValueMap<String, String> queryStringMap;
     private String httpVersion;
 
-    public RequestLine(String method, String path, String httpVersion) {
+    public RequestLine(String method, String path, MultiValueMap<String, String> queryStringMap, String httpVersion) {
         this.method = method;
         this.path = path;
+        this.queryStringMap = queryStringMap;
         this.httpVersion = httpVersion;
     }
 
     public static RequestLine parse(String requestLine) {
-        String[] lines = requestLine.split(" ");
-        return new RequestLine(lines[0], lines[1], lines[2]);
+        String[] lines = requestLine.split(SPACE);
+        String[] splitPathAndQuery = lines[1].split(START_QUERY);
+        String queryString = splitPathAndQuery[1];
+        MultiValueMap<String, String> queryStringMap = new LinkedMultiValueMap<>();
+
+        if (!StringUtils.isEmpty(queryString)) {
+            makeQueryString(queryString, queryStringMap);
+        }
+
+        return new RequestLine(lines[0], splitPathAndQuery[0], queryStringMap, lines[2]);
+    }
+
+    private static void makeQueryString(String queryString, MultiValueMap<String, String> queryStringMap) {
+        String[] splitQuery = queryString.split(SPLIT_QUERY);
+        if (splitQuery.length > 0) {
+            mappingQuery(queryStringMap, splitQuery);
+        }
+    }
+
+    private static void mappingQuery(MultiValueMap<String, String> queryStringMap, String[] splitQuery) {
+        for (String query : splitQuery) {
+            String[] keyValue = query.split(SPLIT_KEY_VALUE);
+            queryStringMap.put(keyValue[0], Arrays.asList(keyValue[1].split(COMMA)));
+        }
     }
 
     public String getMethod() {
@@ -29,5 +62,9 @@ public class RequestLine {
 
     public String getHttpVersion() {
         return httpVersion;
+    }
+
+    public MultiValueMap<String, String> getQueryString() {
+        return this.queryStringMap;
     }
 }
--- src/test/java/webserver/http/RequestLineTest.java
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -11,10 +11,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RequestLineTest {
 
     @Test
-    void parse() {
+    void basic_parsing() {
         RequestLine requestLine = RequestLine.parse("GET /users HTTP/1.1");
 
         assertThat(requestLine.getMethod()).isEqualTo("GET");
         assertThat(requestLine.getPath()).isEqualTo("/users");
+        assertThat(requestLine.getHttpVersion()).isEqualTo("HTTP/1.1");
+    }
+
+    @Test
+    void query_string_parsing() {
+        RequestLine requestLine = RequestLine.parse("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1");
+
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+        assertThat(requestLine.getQueryString()).hasSize(3);
+        assertThat(requestLine.getQueryString()).containsKeys("userId", "password", "name");
+        assertThat(requestLine.getHttpVersion()).isEqualTo("HTTP/1.1");
     }
 }
