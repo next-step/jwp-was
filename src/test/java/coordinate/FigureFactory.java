e0658a7d08f00e033785d43637fa1832d4084307
korkorna <korkorna@gmail.com>
2019-08-02 01:06:19 +0900

    feat (if문 제거)

--- src/test/java/coordinate/FigureFactory.java
+++ src/test/java/coordinate/FigureFactory.java
@@ -1,19 +1,53 @@
 package coordinate;
 
-import java.util.List;
+import java.util.*;
 
 public class FigureFactory {
+
+    private enum DefaultFigureCreator implements FigureCreator {
+        LINE(2) {
+            @Override
+            public Figure create(List<Point> points) {
+                return new Line(points);
+            }
+        },
+        Triangle(3) {
+            @Override
+            public Figure create(List<Point> points) {
+                return new Triangle(points);
+            }
+        },
+        Rectangle(4) {
+            @Override
+            public Figure create(List<Point> points) {
+                return new Rectangle(points);
+            }
+        };
+
+        private int pointCount;
+
+        DefaultFigureCreator(int pointCount) {
+            this.pointCount = pointCount;
+        }
+
+        public boolean isEqual(int pointCount) {
+            return this.pointCount == pointCount;
+        }
+
+        public static Optional<Figure> figureBy(List<Point> points) {
+            return Arrays.stream(DefaultFigureCreator.values())
+                    .filter(figure -> figure.isEqual(points.size()))
+                    .map(defaultFigureCreator -> defaultFigureCreator.create(points))
+                    .findFirst();
+        }
+    }
+
     static Figure getInstance(List<Point> points) {
-        if (points.size() == 2) {
-            return new Line(points);
-        }
+        Optional<Figure> maybeFigure = DefaultFigureCreator.figureBy(points);
 
-        if (points.size() == 3) {
-            return new Triangle(points);
-        }
-
-        if (points.size() == 4) {
-            return new Rectangle(points);
+        if (maybeFigure.isPresent()) {
+            return maybeFigure
+                    .get();
         }
 
         throw new IllegalArgumentException("유효하지 않은 도형입니다.");
