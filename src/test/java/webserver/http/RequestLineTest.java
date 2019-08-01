d954b028c51f227967b7cd3ba19ff59fad4d67ed
hongsii <hongsii.dev@gmail.com>
2019-08-02 00:47:40 +0900

    test : 테스트케이스 추가

--- src/test/java/webserver/http/RequestLineTest.java
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -1,23 +1,39 @@
 package webserver.http;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RequestLineTest {
 
-    @Test
     @DisplayName("Parse Request-Line")
-    void parse() {
-        RequestLine requestLine = RequestLine.parse("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1");
+    @ParameterizedTest(name = "case : {0} -> method : {1}, path : {2}, params : {3}")
+    @MethodSource("parseRequestLine")
+    void parse(String rawRequestLine, String expectedMethod, String expectedPath, Map<String, String> expectedParams) {
+        RequestLine requestLine = RequestLine.parse(rawRequestLine);
 
-        assertThat(requestLine.getMethod()).isEqualTo("GET");
-        assertThat(requestLine.getPath()).isEqualTo("/users");
-        assertThat(requestLine.getParams())
-                .hasSize(3)
-                .containsEntry("userId", "javajigi")
-                .containsEntry("password", "password")
-                .containsEntry("name", "JaeSung");
+        assertThat(requestLine.getMethod()).isEqualTo(expectedMethod);
+        assertThat(requestLine.getPath()).isEqualTo(expectedPath);
+        assertThat(requestLine.getParams()).containsAllEntriesOf(expectedParams);
+    }
+
+    private static Stream<Arguments> parseRequestLine() {
+        return Stream.of(
+                Arguments.of("GET /users HTTP/1.1", "GET", "/users", Collections.emptyMap()),
+                Arguments.of("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1", "GET", "/users",
+                        new HashMap<String, String>() {{
+                            put("userId", "javajigi");
+                            put("password", "password");
+                            put("name", "JaeSung");
+                        }})
+        );
     }
 }
