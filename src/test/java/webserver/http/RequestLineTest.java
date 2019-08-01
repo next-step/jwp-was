949b6accfa7e1b7e7250319490c1accbff54f87c
dave915 <40193460+dave915@users.noreply.github.com>
2019-08-01 21:21:51 +0900

    implement requestline (#22)

---
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -0,0 +1,16 @@
+package webserver.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestLineTest {
+
+    @Test
+    void parse() {
+        String path = "GET /users HTTP/1.1";
+        RequestLine requestLine = RequestLine.parse(path);
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+    }
+}
