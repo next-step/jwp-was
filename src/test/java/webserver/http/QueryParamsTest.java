7fff7e0cca7dae3cf9612e2d18697413e4bd73b2
circlee <circlee7@gmail.com>
2019-08-02 16:21:27 +0900

    fix: unit test에 DisplayName 추가

--- src/test/java/webserver/http/QueryParamsTest.java
+++ src/test/java/webserver/http/QueryParamsTest.java
@@ -1,5 +1,6 @@
 package webserver.http;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class QueryParamsTest {
 
+    @DisplayName("path QueryString parse: QueryString 없는 케이스")
     @ParameterizedTest
     @ValueSource(strings = {"/users"})
     public void parseByPathNoQuery(String path){
@@ -14,6 +16,7 @@ public class QueryParamsTest {
         assertThat(queryParams.getParameter("userId")).isNull();
     }
 
+    @DisplayName("path QueryString parse: QueryString value, value 구분자 없는 케이스")
     @ParameterizedTest
     @ValueSource(strings = {"/users?userId"})
     public void parseByPathQueryEmptyValue1(String path){
@@ -21,6 +24,7 @@ public class QueryParamsTest {
         assertThat(queryParams.getParameter("userId")).isNull();
     }
 
+    @DisplayName("path QueryString parse: QueryString value 없는 케이스")
     @ParameterizedTest
     @ValueSource(strings = {"/users?userId="})
     public void parseByPathQueryEmptyValue2(String path){
@@ -28,6 +32,7 @@ public class QueryParamsTest {
         assertThat(queryParams.getParameter("userId")).isEmpty();
     }
 
+    @DisplayName("path QueryString parse: QueryString 파라미터 조회")
     @ParameterizedTest
     @ValueSource(strings = {"/users?userId=javajigi&password=password&name=JaeSung"})
     public void parseByPath(String path){
@@ -35,6 +40,7 @@ public class QueryParamsTest {
         assertThat(queryParams.getParameter("userId")).isEqualTo("javajigi");
     }
 
+    @DisplayName("path QueryString parse: QueryString 동일 이름의 중복 파라미터 조회")
     @ParameterizedTest
     @ValueSource(strings = {"/users?userId=javajigi&userId=JaeSung"})
     public void parsebyPathSameName(String path){
