afc708702cb9e603e21a02445bac747c0edb3007
조정훈 <jeonghoon.cho@woowahan.com>
2019-08-03 01:06:49 +0900

    refactor(figure) : enum refactor

--- src/test/java/coordinate/Type.java
+++ src/test/java/coordinate/Type.java
@@ -6,29 +6,42 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public enum Type {
-    LINE(2, Line::new),
-    RECTANGLE(4, Rectangle::new),
-    TRIANGLE(3, Triangle::new);
+public enum Type implements FigureCreator {
+    LINE(2) {
+        @Override
+        public Figure create(List<Point> points) {
+            return new Line(points);
+        }
+    },
+    RECTANGLE(4) {
+        @Override
+        public Figure create(List<Point> points) {
+            return new Rectangle(points);
+        }
+    },
+    TRIANGLE(3) {
+        @Override
+        public Figure create(List<Point> points) {
+            return new Triangle(points);
+        }
+    };
 
     private final int numberOfPoint;
-    private final FigureCreator figureCreator;
 
-    private static final Map<Integer, FigureCreator> figures;
+    private static final Map<Integer, Type> figures;
 
     static {
         figures = Arrays.stream(Type.values())
-                .collect(Collectors.toMap(type -> type.numberOfPoint, type -> type.figureCreator, (figureCreator1, figureCreator2) -> figureCreator1));
+                .collect(Collectors.toMap(type -> type.numberOfPoint, type -> type));
     }
 
-    Type(int numberOfPoint, FigureCreator figureCreator) {
+    Type(int numberOfPoint) {
         this.numberOfPoint = numberOfPoint;
-        this.figureCreator = figureCreator;
     }
 
     public static Figure of(List<Point> points) {
         return Optional.ofNullable(figures.get(points.size()))
-                .map(creator -> creator.create(points))
+                .map(type -> type.create(points))
                 .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 도형입니다."));
     }
 }
