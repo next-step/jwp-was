2d4b31dc7ccfff20e3153bea85a2d23d2f760504
circlee <circlee7@gmail.com>
2019-08-02 16:15:13 +0900

    style: modified variable naming

--- src/main/java/webserver/http/QueryParams.java
+++ src/main/java/webserver/http/QueryParams.java
@@ -17,13 +17,13 @@ public class QueryParams {
 
     private static final Pattern QUERY_PARAM_PATTERN = Pattern.compile("^[^=\\s]+(=(\\S+)?)?");
 
-    private final Map<String, List<String>> parameterMap;
+    private final Map<String, List<String>> parameterValuesByName;
 
     private QueryParams(String[] queryParamKeyVaues){
-        this.parameterMap = new HashMap<>();
+        this.parameterValuesByName = new HashMap<>();
 
         for(String queryKeyValue : queryParamKeyVaues) {
-            setQueryParam(this.parameterMap, queryKeyValue);
+            setQueryParam(this.parameterValuesByName, queryKeyValue);
         }
     }
 
@@ -41,7 +41,7 @@ public class QueryParams {
 
         String key = keyAndValue[0];
         String value = keyAndValue[1];
-        this.parameterMap.computeIfAbsent(key, (k) -> new ArrayList<>()).add(value);
+        this.parameterValuesByName.computeIfAbsent(key, (k) -> new ArrayList<>()).add(value);
     }
 
     public static QueryParams parseByPath(String path) {
@@ -61,8 +61,8 @@ public class QueryParams {
     }
 
     public String[] getParameterValues(String name) {
-        return Optional.ofNullable(this.parameterMap.get(name))
-                .map(valueList -> valueList.toArray(new String[valueList.size()]))
+        return Optional.ofNullable(this.parameterValuesByName.get(name))
+                .map(values -> values.toArray(new String[values.size()]))
                 .orElse(null);
     }
 
