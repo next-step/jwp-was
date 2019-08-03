5f6bccf8342d62963364e8cb1f3446208a912f52
internet200q <internet200@qdillion.com>
2019-08-03 12:38:25 +0900

    1단계 요구 사항 반영 및 코드 리펙토링

--- src/main/java/webserver/http/RequestLine.java
+++ src/main/java/webserver/http/RequestLine.java
@@ -1,13 +1,15 @@
 package webserver.http;
 
-import java.util.HashMap;
-
 public class RequestLine {
+    public HttpRequest httpRequest;
 
-    public String url;
+    public String httpMsg;
 
-    public RequestLine(String url){
-        this.url = url;
+
+    public RequestLine(String httpMsg){
+        this.httpMsg = httpMsg;
+        httpRequest = new HttpRequest(httpMsg);
+
     }
 
     public static RequestLine parse(String url){
@@ -15,44 +17,16 @@ public class RequestLine {
     }
 
     public String getMethod(){
-        return strSplit()[0];
+        return httpRequest.getHttpMethod();
     }
 
     public String getPath(){
-        return strSplit()[1];
+        return httpRequest.getHttpPath();
     }
 
-    public String getVersion(){
-        return strSplit()[2];
+    public String getParameter(String keyStr){
+        return httpRequest.getHttpParameterValue(keyStr);
     }
 
-    public String getParam(String param) {
-        //? 기준으로 자르고, & 기준으로 한번더 잘라 파라미터를 분리 한다
-        String[] paramStr = getPath().split("\\?")[1].split("&");
 
-        HashMap<String, String> paramMap = new HashMap<>();
-        for (int i = 0; i < paramStr.length; i++) {
-            String[] keyValueStr = paramStr[i].split("=");
-            paramMap.put(keyValueStr[0], nullCheck(keyValueStr));
-        }
-
-        return paramMap.get(param);
-    }
-
-    /**
-     * URL 분리
-     * @return
-     */
-    private String[] strSplit(){
-        return url.split(" ");
-    }
-
-    /**
-     * 파라미터 value 값 분리
-     * @param keyValue
-     * @return
-     */
-    private String nullCheck(String[] keyValue){
-        return keyValue.length == 1 ? null : keyValue[1];
-    }
 }
