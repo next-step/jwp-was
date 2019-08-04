1b685eba739dee41598f2ff044312dd7fcafb1e7
Jinho Yim <jinhoyim@outlook.com>
2019-08-04 14:18:27 +0900

    test: 테스트 케이스 이름 추가

--- src/test/java/webserver/http/QueryStringTest.java
+++ src/test/java/webserver/http/QueryStringTest.java
@@ -1,5 +1,6 @@
 package webserver.http;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
@@ -8,24 +9,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class QueryStringTest {
     @Test
+    @DisplayName("키 추출")
     void should_ContainsKey() {
         QueryString qs = QueryString.parse("id=myId");
         assertThat(qs.containsKey("id")).isTrue();
     }
 
     @Test
+    @DisplayName("키 추출2")
     void should_ContainsOtherKey() {
         QueryString qs = QueryString.parse("name=myName");
         assertThat(qs.containsKey("name")).isTrue();
     }
 
     @Test
+    @DisplayName("비어있는 쿼리스트링의 키 추출")
     void should_NotContainsKey_When_ParseEmpty() {
         QueryString qs = QueryString.parse("");
         assertThat(qs.containsKey("id")).isFalse();
     }
 
     @Test
+    @DisplayName("두개의 항목을 갖는 쿼리스트링의 키 추출")
     void should_ContainsTwoKeys_When_ParseTwoKeyValues() {
         QueryString qs = QueryString.parse("id=myId&name=myName");
         assertThat(qs.containsKey("id")).isTrue();
@@ -33,6 +38,7 @@ class QueryStringTest {
     }
 
     @Test
+    @DisplayName("키 목록")
     void should_ReturnKeys() {
         QueryString qs = QueryString.parse("id=myId&name=myName");
         Set<String> keys = qs.keys();
@@ -43,6 +49,7 @@ class QueryStringTest {
     }
 
     @Test
+    @DisplayName("비어있는 키 목록")
     void should_ReturnEmptyKeys() {
         QueryString qs = QueryString.parse("");
         Set<String> keys = qs.keys();
@@ -50,18 +57,21 @@ class QueryStringTest {
     }
 
     @Test
+    @DisplayName("값 추출")
     void should_ReturnValue() {
         QueryString qs = QueryString.parse("id=myId");
         assertThat(qs.get("id")).isEqualTo("myId");
     }
 
     @Test
+    @DisplayName("값 추출2")
     void should_ReturnOtherValue() {
         QueryString qs = QueryString.parse("name=myName");
         assertThat(qs.get("name")).isEqualTo("myName");
     }
 
     @Test
+    @DisplayName("두개의 값 추출")
     void should_ReturnTwoValues() {
         QueryString qs = QueryString.parse("id=myId&name=myName");
         assertThat(qs.get("id")).isEqualTo("myId");
@@ -69,30 +79,35 @@ class QueryStringTest {
     }
 
     @Test
+    @DisplayName("입력에 키만 존재하고 값이 없을 때 키를 값으로 사용")
     void should_ReturnSameKeyValue_When_ParseOnlyKey() {
         QueryString qs = QueryString.parse("key1");
         assertThat(qs.get("key1")).isEqualTo("key1");
     }
 
     @Test
+    @DisplayName("입력에 키만 존재하고 값이 없을 때 키를 값으로 사용2")
     void should_ReturnOtherSameKeyValue_When_ParseOnlyKey() {
         QueryString qs = QueryString.parse("key2");
         assertThat(qs.get("key2")).isEqualTo("key2");
     }
 
     @Test
+    @DisplayName("입력에 없는 키를 조회")
     void should_ReturnNull_When_GetNotContained() {
         QueryString qs = QueryString.parse("id=myId");
         assertThat(qs.get("myName")).isNull();
     }
 
     @Test
+    @DisplayName("입력을 보관")
     void should_ReturnOriginalQueryString() {
         QueryString qs = QueryString.parse("id=myId&name=myName");
         assertThat(qs.origin()).isEqualTo("id=myId&name=myName");
     }
 
     @Test
+    @DisplayName("입력을 보관2")
     void should_ReturnOtherOriginalQueryString() {
         QueryString qs = QueryString.parse("id=myId");
         assertThat(qs.origin()).isEqualTo("id=myId");
--- src/test/java/webserver/http/RequestLineTest.java
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -1,53 +1,62 @@
 package webserver.http;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RequestLineTest {
     @Test
+    @DisplayName("메서드 추출")
     void parseMethod() {
         RequestLine requestLine = RequestLine.parse("GET /users HTTP/1.1");
         assertThat(requestLine.getMethod()).isEqualTo("GET");
     }
 
     @Test
+    @DisplayName("메서드 추출2")
     void parseOtherMethod() {
         RequestLine requestLine = RequestLine.parse("POST /users HTTP/1.1");
         assertThat(requestLine.getMethod()).isEqualTo("POST");
     }
 
     @Test
+    @DisplayName("경로 추출")
     void parsePath() {
         RequestLine requestLine = RequestLine.parse("GET /users HTTP/1.1");
         assertThat(requestLine.getPath()).isEqualTo("/users");
     }
 
     @Test
+    @DisplayName("경로 추출2")
     void parseOtherPath() {
         RequestLine requestLine = RequestLine.parse("GET /products HTTP/1.1");
         assertThat(requestLine.getPath()).isEqualTo("/products");
     }
 
     @Test
+    @DisplayName("HTTP 버전 추출")
     void parseHttpVersion() {
         RequestLine requestLine = RequestLine.parse("GET /users HTTP/1.1");
         assertThat(requestLine.getHttpVersion()).isEqualTo("HTTP/1.1");
     }
 
     @Test
+    @DisplayName("HTTP 버전 추출2")
     void parseOtherHttpVersion() {
         RequestLine requestLine = RequestLine.parse("GET /users HTTP/2");
         assertThat(requestLine.getHttpVersion()).isEqualTo("HTTP/2");
     }
 
     @Test
+    @DisplayName("쿼리스트링 추출")
     void parseQueryString() {
         RequestLine requestLine = RequestLine.parse("GET /users?id=myId HTTP/1.1");
         assertThat(requestLine.getQueryString()).isEqualTo(QueryString.parse("id=myId"));
     }
 
     @Test
+    @DisplayName("쿼리스트링 추출2")
     void parseOtherQueryString() {
         RequestLine requestLine = RequestLine.parse("GET /users?id=myId&name=myName HTTP/1.1");
         assertThat(requestLine.getQueryString()).isEqualTo(QueryString.parse("id=myId&name=myName"));
