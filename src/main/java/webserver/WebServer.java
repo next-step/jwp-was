9ccb11d3342dda4b4682b2ade45c135134c0dfa8
Jihan <jihanbaek805@gmail.com>
2019-08-05 23:39:04 +0900

    feat : Thread Pool 적용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,9 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,14 +12,12 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_THREAD_POOL_SIZE = 10;
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        int port = getPort(args);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -26,9 +27,19 @@ public class WebServer {
             Socket connection;
             RequestDispatcher dispatcher = new RequestDispatcher();
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection, dispatcher));
-                thread.start();
+                executorService.execute(new RequestHandler(connection, dispatcher));
             }
+
+            executorService.shutdown();
+            executorService.awaitTermination(100, TimeUnit.SECONDS);
+            logger.info("Wep Application Server stopped {} port", port);
         }
+    }
+    private static int getPort(String[] args) {
+        if (args == null || args.length == 0) {
+            return DEFAULT_PORT;
+        }
+
+        return Integer.parseInt(args[0]);
     }
 }
