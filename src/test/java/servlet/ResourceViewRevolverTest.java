3976e3c454a54ec934ae3b19b1870331e97866f9
leechang0423 <leechang0423@naver.com>
2019-08-05 17:27:48 +0900

    feat #step2 요구사항2

---
+++ src/test/java/servlet/ResourceViewRevolverTest.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ResourceViewRevolverTest {
+
+  @Test
+  @DisplayName("static파일(css , js) 파일을 제외한 리소스 탐색시에 ./templates 하위에서 찾도록 한다 ")
+  void nonStaticFileViewResolver() {
+    String path = "/index.html";
+    try {
+      byte[] body = ResourceViewRevolver.resolve(path);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  @DisplayName("static파일(css , js) 탐색시에 ./static 하위에서 찾도록 한다 ")
+  void staticFileViewResolver() {
+    String path = "/js/scripts.js";
+    try {
+      byte[] body = ResourceViewRevolver.resolve(path);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+  }
+}
