df14b7635e3f4ea3e028b81a55fbfd9a0341fc75
leechang0423 <leechang0423@naver.com>
2019-08-05 10:58:01 +0900

    feat #step2 요구사항1 http://localhost:8080/index.html 로 접속했을 때 webapp 디렉토리의 index.html 파일을 읽어 클라이언트에 응답한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,55 +1,69 @@
 package webserver;
 
+import http.RequestLine;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
+import java.net.URISyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 
 public class RequestHandler implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+  private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    public RequestHandler(Socket connectionSocket) {
-        this.connection = connectionSocket;
+  private Socket connection;
+
+  public RequestHandler(Socket connectionSocket) {
+    this.connection = connectionSocket;
+  }
+
+  public void run() {
+    logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+        connection.getPort());
+
+    try (InputStream in = connection.getInputStream(); OutputStream out = connection
+        .getOutputStream()) {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+      RequestLine requestLine = RequestLine.parse(bufferedReader.readLine());
+
+      DataOutputStream dos = new DataOutputStream(out);
+
+      byte[] body = "Hello World".getBytes();
+      if ("/index.html".equals(requestLine.getPath())) {
+        body = FileIoUtils.loadFileFromClasspath("./templates/index.html");
+      }
+      response200Header(dos, body.length);
+      responseBody(dos, body);
+    } catch (IOException e) {
+      logger.error(e.getMessage());
+    } catch (URISyntaxException e) {
+      logger.error(e.getMessage());
     }
+  }
 
-    public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+  private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    try {
+      dos.writeBytes("HTTP/1.1 200 OK \r\n");
+      dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+      dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+      dos.writeBytes("\r\n");
+    } catch (IOException e) {
+      logger.error(e.getMessage());
     }
+  }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+  private void responseBody(DataOutputStream dos, byte[] body) {
+    try {
+      dos.write(body, 0, body.length);
+      dos.flush();
+    } catch (IOException e) {
+      logger.error(e.getMessage());
     }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+  }
 }
