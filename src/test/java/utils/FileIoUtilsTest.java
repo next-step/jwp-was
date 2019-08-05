5b9c48c76f83e532659007d8ae40f3bb62c2a288
circlee <circlee7@gmail.com>
2019-08-06 01:51:52 +0900

    style : fileIoUtilsTest 메소드명 수저

--- src/test/java/utils/FileIoUtilsTest.java
+++ src/test/java/utils/FileIoUtilsTest.java
@@ -1,6 +1,7 @@
 package utils;
 
 import exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,14 +11,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class FileIoUtilsTest {
     private static final Logger log = LoggerFactory.getLogger(FileIoUtilsTest.class);
 
+    @DisplayName("FileIoUtils loadFileFromClasspath 테스트 : 리소스 존재")
     @Test
-    void loadFileFromClasspath() throws Exception {
+    void loadTest() throws Exception {
         byte[] body = FileIoUtils.loadFileFromClasspath("./templates/index.html");
         log.debug("file : {}", new String(body));
     }
 
+    @DisplayName("FileIoUtils loadFileFromClasspath 테스트 : 없는 리소스")
     @Test
-    void loadFileFromClasspathNoResource() throws Exception {
+    void loadTestException() throws Exception {
         assertThatThrownBy(() -> FileIoUtils.loadFileFromClasspath("./templates/nofile.html"))
             .isInstanceOf(ResourceNotFoundException.class);
 
