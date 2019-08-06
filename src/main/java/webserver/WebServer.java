02cfa23b2553bb0c906e83efd882cd73d60936ff
dave915 <Htaehyun@gmail.com>
2019-08-07 08:01:39 +0900

    feat ExecutorService 사용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,25 +11,27 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_THREAD_POOL = 10;
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        int port = setPort(args);
+        ExecutorService executorService = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL);
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executorService.execute(new RequestHandler(connection));
             }
         }
+    }
+
+    private static int setPort(String args[]) {
+        if (args == null || args.length == 0) {
+            return DEFAULT_PORT;
+        }
+
+        return Integer.parseInt(args[0]);
     }
 }
