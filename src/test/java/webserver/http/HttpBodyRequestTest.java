9a8426c62f61c9d17d1700484ef2d7f4613082b0
eldie <eldie@whitin.co.kr>
2019-08-06 14:04:04 +0900

    feat : post body 와 queryString 파라미터 테스트 케이스 추가

--- src/test/java/webserver/http/HttpBodyRequestTest.java
+++ src/test/java/webserver/http/HttpBodyRequestTest.java
@@ -1,13 +1,17 @@
 package webserver.http;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import webserver.resolvers.body.FormBodyResolver;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import enums.HttpMethod;
+import webserver.resolvers.body.FormBodyResolver;
 
 public class HttpBodyRequestTest {
 
@@ -22,7 +26,14 @@ public class HttpBodyRequestTest {
             "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
 
 
-
+    private static final String HTTP_QUERY_STRING_POST = "POST /user/create?id=1 HTTP/1.1\n" + 
+    		"Host: localhost:8080\n" + 
+    		"Connection: keep-alive\n" + 
+    		"Content-Length: 46\n" + 
+    		"Content-Type: application/x-www-form-urlencoded\n" + 
+    		"Accept: */*\n" + 
+    		"\n" + 
+    		"userId=javajigi&password=password&name=JaeSung";
 
 
 
@@ -40,6 +51,17 @@ public class HttpBodyRequestTest {
         }
     }
 
+    @DisplayName("Post Http HttpBodyRequest 테스트 : with QueryString parameter")
+    @Test
+    public void httpRequestParseQueryString() throws Exception {
+    	FormBodyResolver formBodyResolver = FormBodyResolver.getInstance();
+        HttpRequest httpRequest = formBodyResolver.resolve(HttpBaseRequest.parse(new ByteArrayInputStream(HTTP_QUERY_STRING_POST.getBytes())));
+        assertEquals(HttpMethod.POST, httpRequest.getMethod());
+        assertEquals("/user/create", httpRequest.getRequestURI());
+        assertEquals("keep-alive", httpRequest.getHeader("Connection"));
+        assertEquals("1", httpRequest.getParameter("id"));
+        assertEquals("javajigi", httpRequest.getParameter("userId"));
+    }
 
 
 
