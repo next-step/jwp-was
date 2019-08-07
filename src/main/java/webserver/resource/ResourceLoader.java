297f64180773cbff61f5a7835c8430bef94b3d99
윤현준 <homelus@daum.net>
2019-08-07 21:30:27 +0900

    refactor - add comment

--- src/main/java/webserver/resource/ResourceLoader.java
+++ src/main/java/webserver/resource/ResourceLoader.java
@@ -2,8 +2,14 @@ package webserver.resource;
 
 import webserver.http.ModelAndView;
 
+/**
+ * strategy interface for loading resource given file extension
+ */
 public interface ResourceLoader {
 
+    /**
+     * Whether the given file name's extension is supported by loader
+     */
     boolean support(String name);
 
     String getResource(ModelAndView mav);
