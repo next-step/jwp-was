25f268a6a2a0b1a256511e769e8b3e73f18fdbf7
ssosso <ssossohow@gmail.com>
2019-08-08 02:43:36 +0900

    [step2] feat request body

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,12 +1,14 @@
 package controller;
 
 import annotation.Controller;
+import annotation.RequestBody;
 import annotation.RequestMapping;
+import model.User;
 
 @Controller
 public class UserController {
     @RequestMapping(path = "/user/create")
-    public String createUser() {
+    public String createUser(@RequestBody User user) {
         return "/user/form";
     }
 }
