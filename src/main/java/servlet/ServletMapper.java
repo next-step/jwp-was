0e133415d8e3a3499ecf279ae876b405fed0d274
leechang0423 <leechang0423@naver.com>
2019-08-08 11:31:49 +0900

    bugfix #step3 ServletMappe생성자 호출 버그 수정

--- src/main/java/servlet/ServletMapper.java
+++ src/main/java/servlet/ServletMapper.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -9,17 +10,21 @@ public class ServletMapper {
   private Map<String, HttpServlet> servlets;
 
   public ServletMapper() {
-    new ServletMapper(getMappingInfo());
+    this(Collections.EMPTY_MAP);
   }
 
   public ServletMapper(Map<String, HttpServlet> servletMappingInfo) {
-    servlets = servletMappingInfo;
+    if (servletMappingInfo.isEmpty()) {
+      servlets = getMappingInfo();
+      return;
+    }
+    this.servlets = servletMappingInfo;
   }
 
   private Map<String, HttpServlet> getMappingInfo() {
     return Arrays.stream(ServletMappingInfo.values())
         .collect(Collectors
-            .toMap(mappingInfo -> mappingInfo.getPath(), mappingInfo -> mappingInfo.getCreator()));
+            .toMap(mappingInfo -> mappingInfo.getPath(), mappingInfo -> mappingInfo.getServlet()));
   }
 
   public HttpServlet getServlet(String path) {
