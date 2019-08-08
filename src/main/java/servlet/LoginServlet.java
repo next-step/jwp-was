d4e45a96ff1e3114cf728bfcf4d3a2a0ba122324
leechang0423 <leechang0423@naver.com>
2019-08-08 16:40:55 +0900

    refactor #step3 User객체가 password가 맞는지 판단하다록 수정 , login -> isLogin 메소드명 변경

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -3,7 +3,6 @@ package servlet;
 import db.DataBase;
 import http.HttpRequest;
 import http.HttpResponse;
-import http.Parameters;
 import java.util.Map;
 import model.User;
 
@@ -11,15 +10,15 @@ public class LoginServlet extends AbstractHttpServlet {
 
   @Override
   public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
-    if (login(httpRequest.getParameters())) {
+    if (isLogin(httpRequest.getParameters())) {
       httpResponse.addLoginCookie();
       httpResponse.forward("/index.html");
     }
     httpResponse.sendRedirect("/user/login_failed.html");
   }
 
-  private boolean login(Map<String, String> parameters) {
-    User userId = DataBase.findUserById(parameters.get("userId"));
-    return userId != null && userId.getPassword().equals(parameters.get("password"));
+  private boolean isLogin(Map<String, String> parameters) {
+    User user = DataBase.findUserById(parameters.get("userId"));
+    return user != null && user.isCorrectPassword(parameters.get("password"));
   }
 }
