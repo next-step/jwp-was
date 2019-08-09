f7ed74a6d9c8d4185a9c21e0eb8cd5610f925c04
eldie <eldie@whitin.co.kr>
2019-08-09 11:25:37 +0900

    style : 공백 컨벤션 위반 수정

--- src/main/java/constants/SessionConstants.java
+++ src/main/java/constants/SessionConstants.java
@@ -1,5 +1,5 @@
 package constants;
 
 public class SessionConstants {
-    public static final String SESSION_KEY_NAME="JWP-WAS-SESSION-ID";
+    public static final String SESSION_KEY_NAME = "JWP-WAS-SESSION-ID";
 }
