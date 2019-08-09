5386f73c2f6c0b8e9cff3cce4d4149107184b6fc
eldie <eldie@whitin.co.kr>
2019-08-09 11:27:16 +0900

    refactor : SessionConstants package 변경

--- src/main/java/webserver/constants/SessionConstants.java
+++ src/main/java/webserver/constants/SessionConstants.java
@@ -1,4 +1,4 @@
-package constants;
+package webserver.constants;
 
 public class SessionConstants {
     public static final String SESSION_KEY_NAME = "JWP-WAS-SESSION-ID";
--- src/main/java/webserver/http/HttpSessionManager.java
+++ src/main/java/webserver/http/HttpSessionManager.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-import constants.SessionConstants;
+import webserver.constants.SessionConstants;
 
 public class HttpSessionManager {
 
