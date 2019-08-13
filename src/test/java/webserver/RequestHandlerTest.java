212b954171b2c9b1c3c80982dd3e305db5eb6f73
ssosso <ssossohow@gmail.com>
2019-08-13 23:50:53 +0900

    [step2] fix file io util & request handler test

---
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,26 @@
+package webserver;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.net.Socket;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RequestHandlerTest {
+    @Test
+    void run() throws IOException {
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(new ByteArrayInputStream("GET /user/create?userid=ssosso&password=test&name=JangSoHyun HTTP/1.1".getBytes()));
+        when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+
+        RequestHandler requestHandler = new RequestHandler(socket);
+        requestHandler.run();
+
+        InputStream responseStream = new ByteArrayInputStream(((ByteArrayOutputStream) socket.getOutputStream()).toByteArray());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(responseStream));
+        assertThat(reader.readLine()).isEqualTo("HTTP/1.1 200 OK ");
+    }
+}
