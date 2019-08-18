732c93b56a118fb0d1868a11005b2837c42111e1
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-08-19 01:44:22 +0900

    # delete : 구조변경을 위해 기존파일 제거

--- src/main/java/webserver/service/WebService.java
+++
@@ -1,9 +0,0 @@
-package webserver.service;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
-
-public interface WebService {
-    public void process(OutputStream outputStream, Object...obj) throws IOException, URISyntaxException;
-}
