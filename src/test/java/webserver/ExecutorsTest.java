d218380f6fddd4766c9505120dd9ea707fc52580
javajigi <javajigi@gmail.com>
2020-05-27 14:45:15 +0900

    upgrade gradle version to 6.3

--- src/test/java/webserver/ExecutorsTest.java
+++ src/test/java/webserver/ExecutorsTest.java
@@ -2,6 +2,7 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.util.StopWatch;
 
 import java.util.concurrent.ExecutorService;
