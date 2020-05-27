c7c9f4ebd9ff67407fafb0634cbdc45d6757c3c4
javajigi <javajigi@gmail.com>
2020-05-27 15:11:09 +0900

    add spring boot dependency, RestTemplate samples

--- src/test/java/webserver/ExecutorsTest.java
+++ src/test/java/webserver/ExecutorsTest.java
@@ -2,7 +2,6 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.util.StopWatch;
 
 import java.util.concurrent.ExecutorService;
---
+++ src/test/java/webserver/HttpRequestTest.java
@@ -0,0 +1,18 @@
+package webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HttpRequestTest {
+    @Test
+    void request_resttemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        String resourceUrl = "https://edu.nextstep.camp";
+        ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/c/4YUvqn9V", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
