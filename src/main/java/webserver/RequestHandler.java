15a8e5feae0b94b296a7d4d614584b0cd8ea7737
1000141 <ninjasul@onestorecorp.com>
2020-06-01 19:20:40 +0900

    요구 사항 기본 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,13 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,14 +23,43 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
+
+            String request = getRequestString(in);
+            String [] requestLines = splitRequest(request);
+            Map<String, String> queryStrings = parseQueryString(Arrays.asList(requestLines));
+
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String getRequestString(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        return br.readLine();
+    }
+
+    String[] splitRequest(String request) {
+        return request.split(" ");
+    }
+
+    Map<String, String> parseQueryString(List<String> requestLines) {
+        if (Objects.isNull(requestLines) || requestLines.size() < 2 || !requestLines.get(1).contains("?")) {
+            return Collections.emptyMap();
+        }
+
+        int queryStringBeginIndex = requestLines.get(1).indexOf("?");
+        String queryString = requestLines.get(1).substring(queryStringBeginIndex + 1);
+        return collectToParamMap(queryString);
+    }
+
+    private Map<String, String> collectToParamMap(String queryString) {
+        return Arrays.stream(queryString.split("&"))
+                .map(param -> param.split("="))
+                .collect(toMap(entry -> entry[0], entry -> entry[1]));
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
---
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,42 @@
+package webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+class RequestHandlerTest {
+
+    private RequestHandler requestHandler = new RequestHandler(new Socket());
+
+    @Test
+    @DisplayName("파라미터가 없는 HTTP GET 요청 Parsing 테스트")
+    void testRequestLinesWithoutParameters() {
+        String[] requestLines = requestHandler.splitRequest("GET /users HTTP/1.1");
+        assertThat(requestLines).containsExactly("GET", "/users", "HTTP/1.1");
+    }
+
+    @Test
+    @DisplayName("파라미터가 있는 HTTP GET 요청 Parsing 테스트")
+    void testRequestLinesWithParameters() {
+        String[] requestLines = requestHandler.splitRequest("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1");
+        assertThat(requestLines).containsExactly("GET", "/users?userId=javajigi&password=password&name=JaeSung", "HTTP/1.1");
+    }
+
+    @Test
+    @DisplayName("QueryString Parsing 테스트")
+    void testParseQueryString() {
+        Map<String, String> queryString = requestHandler.parseQueryString(Arrays.asList("GET", "/users?userId=javajigi&password=password&name=JaeSung", "HTTP/1.1"));
+
+        assertThat(queryString).contains(
+                entry("userId", "javajigi"),
+                entry("password", "password"),
+                entry("name", "JaeSung")
+        );
+    }
+}
