33e077a2ed50039075f2e768e6b5bf4586e75787
nokchax <nokchax@gmail.com>
2020-06-03 21:41:00 +0900

    test: not enough protocol string throws exception

--- src/main/java/http/Protocol.java
+++ src/main/java/http/Protocol.java
@@ -1,14 +1,31 @@
 package http;
 
 public class Protocol {
+    public static final String PROTOCOL_SPLITTER = "/";
+
+    private final String protocol;
+    private final String version;
+
     public Protocol(final String protocolStr) {
+        String[] tokens = protocolStr.split(PROTOCOL_SPLITTER);
+
+        validate(tokens);
+
+        this.protocol = tokens[0];
+        this.version = tokens[1];
+    }
+
+    private void validate(String[] tokens) {
+        if (tokens.length < 2) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public String getProtocol() {
-        return null;
+        return protocol;
     }
 
     public String getVersion() {
-        return null;
+        return version;
     }
 }
--- src/test/java/http/ProtocolTest.java
+++ src/test/java/http/ProtocolTest.java
@@ -4,11 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("프로토콜 클래스")
 public class ProtocolTest {
 
     @Test
+    @DisplayName("프로토콜 문자열이 정상적이지 않으면 예외를 발생시킨다")
+    void parsingFail() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Protocol("HTTP"));
+    }
+
+    @Test
+    @DisplayName("프로토콜 문자열 파싱")
     void parse() {
         Protocol protocol = new Protocol("HTTP/1.1");
 
