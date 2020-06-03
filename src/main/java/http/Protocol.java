1d4c33c205991c99796644050b966240778c07b1
nokchax <nokchax@gmail.com>
2020-06-03 21:45:17 +0900

    feat: throw exception when protocol string null or empty

--- src/main/java/http/Protocol.java
+++ src/main/java/http/Protocol.java
@@ -2,21 +2,29 @@ package http;
 
 public class Protocol {
     public static final String PROTOCOL_SPLITTER = "/";
+    public static final int PROTOCOL_STRING_TOKEN_SIZE = 2;
 
     private final String protocol;
     private final String version;
 
     public Protocol(final String protocolStr) {
-        String[] tokens = protocolStr.split(PROTOCOL_SPLITTER);
+        validate(protocolStr);
 
-        validate(tokens);
+        String[] tokens = protocolStr.split(PROTOCOL_SPLITTER);
+        verify(tokens);
 
         this.protocol = tokens[0];
         this.version = tokens[1];
     }
 
-    private void validate(String[] tokens) {
-        if (tokens.length < 2) {
+    private void validate(String protocolStr) {
+        if (protocolStr == null || protocolStr.isEmpty()) {
+            throw new IllegalArgumentException("Protocol string is null or empty");
+        }
+    }
+
+    private void verify(String[] tokens) {
+        if (tokens.length < PROTOCOL_STRING_TOKEN_SIZE) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/http/ProtocolTest.java
+++ src/test/java/http/ProtocolTest.java
@@ -3,9 +3,7 @@ package http;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
