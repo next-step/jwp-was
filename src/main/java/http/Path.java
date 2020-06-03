8670cf5f098644ec542e10c1cb7b24c48d75a48b
nokchax <nokchax@gmail.com>
2020-06-03 21:57:44 +0900

    feat: path and query string test

--- src/main/java/http/Path.java
+++ src/main/java/http/Path.java
@@ -1,8 +1,20 @@
 package http;
 
+import java.util.HashMap;
+
 public class Path {
+    private final String path;
+    private final HashMap<String, String> queryString;
+
     public Path(final String pathStr) {
         String[] tokens = pathStr.split("\\?");
+
+        this.path = tokens[0];
+        this.queryString = new HashMap<>();
+
+        if (tokens.length >= 2) {
+
+        }
     }
 
     public String getPath() {
