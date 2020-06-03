55e9e1c4024b59fa82c9d14107e2c66a72af4abc
nokchax <nokchax@gmail.com>
2020-06-03 21:26:29 +0900

    test: post parsing test

--- src/test/java/http/RequestLineParserTest.java
+++ src/test/java/http/RequestLineParserTest.java
@@ -9,11 +9,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RequestLineParserTest {
 
     @Test
-    @DisplayName("파싱")
+    @DisplayName("Get 파싱")
     void parse() {
         RequestLine requestLine = RequestLineParser.parse("GET /users HTTP/1.1");
 
         assertThat(requestLine.getMethod()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+        assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
+        assertThat(requestLine.getVersion()).isEqualTo("1.1");
+    }
+
+    @Test
+    @DisplayName("Post 파싱")
+    void parsePost() {
+        RequestLine requestLine = RequestLineParser.parse("POST /users HTTP/1.1");
+
+        assertThat(requestLine.getMethod()).isEqualTo("POST");
         assertThat(requestLine.getPath()).isEqualTo("/users");
         assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
         assertThat(requestLine.getVersion()).isEqualTo("1.1");
