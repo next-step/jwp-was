22a04f85ab3cc3a923c9320ad565f9ee1ffd41f0
nokchax <nokchax@gmail.com>
2020-06-03 21:43:10 +0900

    test: throw exception when protocol string null or empty

--- src/test/java/http/ProtocolTest.java
+++ src/test/java/http/ProtocolTest.java
@@ -2,6 +2,10 @@ package http;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -14,6 +18,14 @@ public class ProtocolTest {
     void parsingFail() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Protocol("HTTP"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("프로토콜 문자열이 null이거나비어있다면 예외를 발생시킨다")
+    void parsingFailWithNullOrEmptyStr(final String protocolStr) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Protocol(protocolStr));
     }
 
     @Test
