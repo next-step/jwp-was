5cab67b47e9a96802a330dd0beceff8aeffcf8ed
chwon <chwon@miridih.com>
2020-06-03 21:56:32 +0900

    (test) 테스트가 최소 컴파일은 되도록 변경2

--- src/main/java/http/QueryString.java
+++ src/main/java/http/QueryString.java
@@ -2,6 +2,10 @@ package http;
 
 public class QueryString {
 
+    public QueryString(String raw) {
+
+    }
+
     public String getParameter(String attributeName) {
         return null;
     }
--- src/main/java/http/RequestLineParser.java
+++ src/main/java/http/RequestLineParser.java
@@ -3,7 +3,8 @@ package http;
 public class RequestLineParser {
     public static RequestLine parse(String requestLine) {
         final String[] splited = requestLine.split(" ");
+        final QueryString queryString = new QueryString(splited[1].split("\\?")[1]);
         final Protocol protocol = new Protocol(splited[2].split("/"));
-        return new RequestLine(splited[0], splited[1], protocol);
+        return new RequestLine(splited[0], splited[1], protocol, queryString);
     }
 }
