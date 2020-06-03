fd2a0052d395918ba649e67720efe51c630bef2d
chwon <chwon@miridih.com>
2020-06-03 21:39:55 +0900

    (test) post 요청 파싱 - 실패하는 테스트 추가

--- src/test/java/http/RequestLineParserTest.java
+++ src/test/java/http/RequestLineParserTest.java
@@ -7,11 +7,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RequestLineParserTest {
 
     @Test
-    void parse() {
+    void parse_get_request() {
         final RequestLine requestLine = RequestLineParser.parse("GET /users HTTP/1.1");
         assertThat(requestLine.getMethod()).isEqualTo("GET");
         assertThat(requestLine.getPath()).isEqualTo("/users");
         assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
         assertThat(requestLine.getVersion()).isEqualTo("1.1");
     }
+    @Test
+    void parse_post_request() {
+        final RequestLine requestLine = RequestLineParser.parse("POST /users HTTP/1.1");
+        assertThat(requestLine.getMethod()).isEqualTo(HttpMethod.POST);
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+        assertThat(requestLine.getProtocol()).isEqualTo(ProtocolType.HTTP);
+        assertThat(requestLine.getVersion()).isEqualTo("1.1");
+    }
+
 }
