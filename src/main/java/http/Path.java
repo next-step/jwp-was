9e4619920021bc8c6c81e3bd3cf1bfbb27ac18f4
LeeSeungHee <palmseung@gmail.com>
2020-06-04 16:46:59 +0900

    Feat (path) : add feature to create Path

--- src/main/java/http/Path.java
+++ src/main/java/http/Path.java
@@ -1,9 +1,24 @@
 package http;
 
-public class Path {
-    public Path(String path) {
+import java.util.Objects;
 
+public class Path {
+    private String path;
+
+    public Path(String path) {
+        this.path = path;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Path path1 = (Path) o;
+        return Objects.equals(path, path1.path);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(path);
+    }
 }
