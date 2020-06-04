13090a4af9000e5697c0c84bf58b5c1ac54ca7b5
kjs4395 <kjs4395@gmail.com>
2020-06-04 14:21:54 +0900

    local git username config commit test

--- src/test/java/http/PathAndQueryStringTest.java
+++ src/test/java/http/PathAndQueryStringTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PathAndQueryStringTest {
     @Test
     void createQueryString() {
-        
+         //local config 이름 설정 테스트 ㅠ
         PathAndQueryString pathAndQueryString = PathAndQuerySpliter.split("/users?userId=javajigi&password=password&name=JaeSung");
         assertThat(pathAndQueryString.getPath()).isEqualTo("/users");
         assertThat(pathAndQueryString.getQeuryString()).isEqualTo("userId=javajigi&password=password&name=JaeSung");
