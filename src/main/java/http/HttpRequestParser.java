f37d1f1b1f529bf0570e46251788788e55c7e2d7
nokchax <nokchax@gmail.com>
2020-06-04 19:06:33 +0900

    feat: parse request raw string as http request object

--- src/main/java/http/HttpRequestParser.java
+++ src/main/java/http/HttpRequestParser.java
@@ -1,10 +1,14 @@
 package http;
 
+import utils.Tokens;
+
 public class HttpRequestParser {
+    private static final String NEW_LINE = "\n";
     private HttpRequestParser() {}
 
     public static HttpRequest parse(final String rawRequestStr) {
+        Tokens tokens = new Tokens(rawRequestStr, NEW_LINE);
 
-        return null;
+        return HttpRequest.init(RequestLineParser.parse(tokens.nextToken()));
     }
 }
