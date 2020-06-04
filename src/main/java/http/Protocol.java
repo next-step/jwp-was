ec268fec12c9df35826def1f249d2dd408886530
LeeSeungHee <palmseung@gmail.com>
2020-06-04 18:30:03 +0900

    Feat (protocolAndVersion) : add feature to create ProtocolAndVersion

--- src/main/java/http/Protocol.java
+++ src/main/java/http/Protocol.java
@@ -1,5 +1,11 @@
 package http;
 
 public enum Protocol {
-    HTTP;
+    HTTP("HTTP");
+
+    private String protocol;
+
+    Protocol(String protocol) {
+        this.protocol = protocol.toUpperCase();
+    }
 }
--- src/main/java/http/ProtocolAndVersion.java
+++ src/main/java/http/ProtocolAndVersion.java
@@ -1,15 +1,33 @@
 package http;
 
+import java.util.Objects;
+
 public class ProtocolAndVersion {
     private Protocol protocol;
     private String version;
 
     public ProtocolAndVersion(String strProtocol) {
-
+        String[] split = strProtocol.split("/");
+        protocol = Protocol.valueOf(split[0]);
+        version = split[1];
     }
 
     public ProtocolAndVersion(Protocol protocol, String version) {
         this.protocol = protocol;
         this.version = version;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProtocolAndVersion that = (ProtocolAndVersion) o;
+        return protocol == that.protocol &&
+                Objects.equals(version, that.version);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(protocol, version);
     }
 }
