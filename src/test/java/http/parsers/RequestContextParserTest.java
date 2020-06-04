2b7252430a5be0402033c0d3ce5ab0fe54fd44d1
hyeyoom <neoul_chw@icloud.com>
2020-06-04 21:08:22 +0900

    test (Step2/req3: 요청 컨텍스트에서 바디까지 파싱하는지에 대한 실패 테스트 추가!)

---
+++ src/test/java/http/parsers/RequestContextParserTest.java
@@ -0,0 +1,35 @@
+package http.parsers;
+
+import http.requests.RequestContext;
+import http.types.HttpMethod;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("요청 컨텍스트를 파싱하는 클래스 테스트")
+class RequestContextParserTest {
+
+    // =_= 수제(요)청. 아 라임청에이드 먹고싶다
+    final String testRequest =
+                    "POST /user/create HTTP/1.1\r\n" +
+                    "Host: localhost:8080\r\n" +
+                    "Content-Length: 76\r\n" +
+                    "Content-Type: application/x-www-form-urlencoded\r\n" +
+                    "\r\n" +
+                    "userId=hyeyoom&password=1234abcd&name=Chiho+Won&email=neoul_chw%40icloud.com";
+
+    @DisplayName("바디까지 포함하여 파싱했는지 테스트!")
+    @Test
+    void test_request_including_body() throws Exception {
+        final InputStream input = new ByteArrayInputStream(testRequest.getBytes());
+        final RequestContext ctx = RequestContextParser.parse(input);
+        assertThat(ctx.getMethod()).isEqualTo(HttpMethod.POST);
+        assertThat(ctx.getPath()).isEqualTo("/user/create");
+        assertThat(ctx.getBody()).isEqualTo("userId=hyeyoom&password=1234abcd&name=Chiho+Won&email=neoul_chw%40icloud.com");
+    }
+}
