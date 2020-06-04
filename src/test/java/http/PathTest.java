30c3a880ab369814da6817ab8e790008c89ddcd2
nokchax <nokchax@gmail.com>
2020-06-04 11:44:51 +0900

    test: path class throw exception when path string is null

--- src/test/java/http/PathTest.java
+++ src/test/java/http/PathTest.java
@@ -2,18 +2,28 @@ package http;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("path 클래스")
 public class PathTest {
+
+    @Test
+    @DisplayName("Path가 null인 경우 예외가 발생한다.")
+    void parseException() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Path(null));
+    }
 
     @Test
     void parse() {
         Path path = new Path("/users?name1=value1&name2=value2");
 
         assertThat(path.getPath()).isEqualTo("/users");
-        assertThat(path.getQueryString("name1")).isEqualTo("value");
+        assertThat(path.getQueryString("name1")).isEqualTo("value1");
         assertThat(path.getQueryString("name2")).isEqualTo("value2");
     }
 }
