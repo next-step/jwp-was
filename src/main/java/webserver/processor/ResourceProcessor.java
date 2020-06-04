8156b83e2bde402033f475a722cb94cf656efd20
nokchax <nokchax@gmail.com>
2020-06-04 20:59:53 +0900

    feat: js, images, css, fonts url matches with resource processor

--- src/main/java/webserver/processor/ResourceProcessor.java
+++ src/main/java/webserver/processor/ResourceProcessor.java
@@ -5,11 +5,18 @@ import http.HttpResponse;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
 public class ResourceProcessor implements Processor {
+    private static final List<String> RESOURCES_URL = Arrays.asList(
+            "/js", "/images", "/fonts", "/css"
+    );
+
     @Override
-    public boolean isMatch(HttpRequest httpRequest) {
-        return false;
+    public boolean isMatch(final HttpRequest httpRequest) {
+        return RESOURCES_URL.stream()
+                .anyMatch(url -> httpRequest.getPath().startsWith(url));
     }
 
     @Override
