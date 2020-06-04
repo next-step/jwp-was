42e7b6a55423c4e8b2bb3d2d0ce92454780ee003
nokchax <nokchax@gmail.com>
2020-06-04 18:58:14 +0900

    feat: make request line parser's constructor private

--- src/main/java/http/RequestLineParser.java
+++ src/main/java/http/RequestLineParser.java
@@ -6,6 +6,8 @@ public class RequestLineParser {
     private static final int REQUIRED_TOKEN_SIZE_TO_INIT_REQUEST_LIEN = 3;
     private static final String DELIMITER = " ";
 
+    private RequestLineParser() {}
+
     public static RequestLine parse(final String requestLineStr) {
         Tokens tokens = Tokens.init(requestLineStr, DELIMITER);
         tokens.validate(REQUIRED_TOKEN_SIZE_TO_INIT_REQUEST_LIEN);
