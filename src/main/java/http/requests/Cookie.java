e717a38cf522e999b12b087086daff2e239d2925
hyeyoom <neoul_chw@icloud.com>
2020-06-05 00:40:30 +0900

    refactor (Step2/req6: 쿠키 구현 추가)

---
+++ src/main/java/http/requests/Cookie.java
@@ -0,0 +1,39 @@
+package http.requests;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Cookie {
+
+    private static final String COOKIE_STRING_DELIMITER = ";";
+    private static final String KEY_VALUE_DELIMITER = "=";
+
+    private final Map<String, String> cookieShop;
+
+    public Cookie(String valueOfHeader) {
+        final String decodedHeader = URLDecoder.decode(valueOfHeader, StandardCharsets.UTF_8);
+        this.cookieShop = parseHeader(decodedHeader);
+    }
+
+    private Map<String, String> parseHeader(String valueOfHeader) {
+        return Arrays.stream(valueOfHeader.split(COOKIE_STRING_DELIMITER))
+                .map(this::splitParameter)
+                .collect(Collectors.toMap(
+                        AbstractMap.SimpleImmutableEntry::getKey,
+                        AbstractMap.SimpleImmutableEntry::getValue)
+                );
+    }
+
+    private AbstractMap.SimpleImmutableEntry<String, String> splitParameter(String raw) {
+        final String[] keyAndValue = raw.split(KEY_VALUE_DELIMITER);
+        return new AbstractMap.SimpleImmutableEntry<>(keyAndValue[0].trim(), keyAndValue[1].trim());
+    }
+
+    public String getValue(String key) {
+        return cookieShop.get(key);
+    }
+}
