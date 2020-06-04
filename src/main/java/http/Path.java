4afb0c5944d9572aca29b410d1f246d88944252c
LeeSeungHee <palmseung@gmail.com>
2020-06-04 17:00:06 +0900

    Fix (path) : add codes

--- src/main/java/http/Path.java
+++ src/main/java/http/Path.java
@@ -9,7 +9,9 @@ public class Path {
 
     public Path(String path) {
         this.path = getPathOnly(path);
-        this.queryStrings = new QueryStrings(path);
+        if (hasQueryStrings(path)) {
+            this.queryStrings = new QueryStrings(path);
+        }
     }
 
     public String getPath() {
@@ -20,8 +22,27 @@ public class Path {
         return queryStrings.getQueryStrings();
     }
 
-    private String getPathOnly(String path){
+    private boolean hasQueryStrings(String path) {
+        String[] split = path.split("\\?");
+        return split.length == 2;
+    }
+
+    private String getPathOnly(String path) {
         String[] split = path.split("\\?");
         return split[0];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Path path1 = (Path) o;
+        return Objects.equals(path, path1.path) &&
+                Objects.equals(queryStrings, path1.queryStrings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path, queryStrings);
     }
 }
