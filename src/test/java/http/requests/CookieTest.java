094e1c1ebf797836f38cef5822b8c2deff3acbc4
hyeyoom <neoul_chw@icloud.com>
2020-06-05 00:41:59 +0900

    test (Step2/req6: 쿠키 비교 테스트 추가)

--- src/test/java/http/requests/CookieTest.java
+++ src/test/java/http/requests/CookieTest.java
@@ -3,6 +3,8 @@ package http.requests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @see <a href=https://developer.mozilla.org/ko/docs/Web/HTTP/Cookies>Cookie!</a>
  */
@@ -17,5 +19,15 @@ class CookieTest {
         assertThat(cookie.getValue("yummy_cookie")).isEqualTo("choco");
         assertThat(cookie.getValue("tasty_cookie")).isEqualTo("strawberry");     // 팩트: 딸기 쿠키 노맛
         assertThat(cookie.getValue("no_more_cookie")).isNull();
+    }
+
+    @DisplayName("헤더의 값 순서가 어떻게 되었든 내용이 같다면 쿠키도 동일해야함.")
+    @Test
+    void test_two_cookies() {
+        final String header1 = "yummy_cookie=choco; tasty_cookie=strawberry";
+        final String header2 = "tasty_cookie=strawberry; yummy_cookie=choco";
+        final Cookie cookie1 = new Cookie(header1);
+        final Cookie cookie2 = new Cookie(header2);
+        assertThat(cookie1).isEqualTo(cookie2);
     }
 }
