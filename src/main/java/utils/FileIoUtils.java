640ae72d145a5267e27854a5bb2a4f58a373a42c
LeeSeungHee <palmseung@gmail.com>
2020-06-04 19:20:46 +0900

    Feat (FileIoUtils) : add feature to loadFileFromClassPath

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -7,8 +7,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileIoUtils {
+    private static final String PREFIX_TEMPLATES = "./templates";
+
     public static byte[] loadFileFromClasspath(String filePath) throws IOException, URISyntaxException {
+        filePath = PREFIX_TEMPLATES + filePath;
         Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
+
         return Files.readAllBytes(path);
     }
 }
