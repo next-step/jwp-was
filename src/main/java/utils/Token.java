4178c5f3f7ee73dc5257ccf522cc0bbea923ff65
nokchax <nokchax@gmail.com>
2020-06-04 13:21:06 +0900

    refactor: use string util to check string is null or empty

--- src/main/java/utils/Token.java
+++ src/main/java/utils/Token.java
@@ -5,8 +5,16 @@ import java.util.StringTokenizer;
 public class Token {
     private final StringTokenizer stringTokenizer;
 
-    public Token(String string, String splitter) {
-        stringTokenizer = new StringTokenizer(string, splitter);
+    public Token(final String origin, final String delimiter) {
+        validate(origin, delimiter);
+
+        stringTokenizer = new StringTokenizer(origin, delimiter);
+    }
+
+    private void validate(String origin, String delimiter) {
+        if (StringUtil.isEmpty(origin) || StringUtil.isEmpty(delimiter)) {
+            throw new IllegalArgumentException("String or delimiter is null or empty");
+        }
     }
 
     public String nextToken() {
--- src/test/java/utils/TokenTest.java
+++ src/test/java/utils/TokenTest.java
@@ -2,11 +2,38 @@ package utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("스트링 토큰 관리 클래스")
 public class TokenTest {
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("문자열 혹은 구분자가 null 혹은 빈 문자열인 경우에는 예외를 던진다")
+    void tokenizeFailWithNull(final String origin, final String delimiter) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Token(origin, delimiter));
+    }
+
+    private static Stream<Arguments> tokenizeFailWithNull() {
+        return Stream.of(
+                Arguments.of(null, ","),
+                Arguments.of(null, ""),
+                Arguments.of("origin", null),
+                Arguments.of("origin", ""),
+                Arguments.of("", ","),
+                Arguments.of("", ""),
+                Arguments.of(null, null)
+        );
+    }
+
 
     @Test
     @DisplayName("문자열은 구분자로 토큰화")
