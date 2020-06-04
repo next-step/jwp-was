4665785d38ec8bc36a588d2145c02ba108f6ec06
liquidjoo <liquidjoo@gmail.com>
2020-06-05 00:11:38 +0900

    feat : 요구사항 1 - GET 요청

--- src/main/java/http/RequestLineParser.java
+++ src/main/java/http/RequestLineParser.java
@@ -2,12 +2,23 @@ package http;
 
 public class RequestLineParser {
 
-    public static RequestLine parse(final String line) {
-        String[] lines = line.split(" ");
-        String method = lines[0];
-        String path = lines[1];
-        String protocol = lines[2];
+    private static final String SEPARATOR = " ";
+    private static final int METHOD_INDEX = 0;
+    private static final int PATH_INDEX = 1;
+    private static final int PROTOCOL_INDEX = 2;
 
-        return new RequestLine(null, null);
+    public static RequestLine parse(final String line) {
+        String[] lines = line.split(SEPARATOR);
+        String method = lines[METHOD_INDEX];
+        String path = lines[PATH_INDEX];
+        String protocol = lines[PROTOCOL_INDEX];
+        return new RequestLine(createMethod(method, path), new Protocol(protocol));
+    }
+
+    private static RequestMethod createMethod(String method, String path) {
+        if (method.equals("GET")) {
+            return new RequestMethodGet(path);
+        }
+        throw new IllegalArgumentException("잘 못된 요청 입니다.");
     }
 }
--- src/test/java/http/RequestLineParserTest.java
+++ src/test/java/http/RequestLineParserTest.java
@@ -1,9 +1,22 @@
 package http;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class RequestLineParserTest {
 
+    @Test
+    @DisplayName("요구사항 1 - GET 요청")
+    void parseGet() {
+        RequestLine requestLine = RequestLineParser.parse("GET /users HTTP/1.1");
+        assertAll("RequestLine 객체의 책임 확인",
+                () -> assertThat(requestLine.getMethodName()).isEqualTo("GET"),
+                () -> assertThat(requestLine.getPath()).isEqualTo("/users"),
+                () -> assertThat(requestLine.getProtocolName()).isEqualTo("HTTP"),
+                () -> assertThat(requestLine.getProtocolVersion()).isEqualTo("1.1")
+        );
+    }
 }
