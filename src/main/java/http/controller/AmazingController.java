ee7329d4d68bcbfeced606b5a00e1a16c5ac1512
hyeyoom <neoul_chw@icloud.com>
2020-06-04 23:32:58 +0900

    test (Step2/req5: 컨트롤러 테스트 성공..!)

--- src/main/java/http/controller/AmazingController.java
+++ src/main/java/http/controller/AmazingController.java
@@ -1,15 +1,62 @@
 package http.controller;
 
 import http.requests.RequestContext;
+import http.responses.HttpStatus;
 import http.responses.ResponseContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.TemplateReader;
+
+import java.io.FileNotFoundException;
 
 /**
  * 그냥 컨트롤러라고 하면 밋밋해서 으메이징이라고 붙임. 다른 의미로 으미에징해질거 같지만 일단 모르는척..
  */
 public class AmazingController {
 
+    private static final Logger log = LoggerFactory.getLogger(AmazingController.class);
 
     public static ResponseContext dispatch(RequestContext requestContext) {
-        return null;
+        final String branch = buildBranchString(requestContext);
+
+        log.debug("branch: {}", branch);
+
+        switch (branch) {
+            case "[POST]/user/create":
+                return signInHandler(requestContext);
+            default:
+                return defaultHandler(requestContext);
+        }
+    }
+
+    private static ResponseContext signInHandler(RequestContext requestContext) {
+        return ResponseContext
+                .builder()
+                    .status(HttpStatus.FOUND)
+                    .addHeader("Location", "/index.html")
+                .build();
+    }
+
+    private static ResponseContext defaultHandler(RequestContext requestContext) {
+        final byte[] rawBody = convertFileToByte(requestContext.getPath());
+        return ResponseContext
+                .builder()
+                    .status(HttpStatus.OK)
+                    .addHeader("Content-Type", "text/html;charset=utf-8")
+                    .addHeader("Content-Length", String.valueOf(rawBody.length))
+                    .body(rawBody)
+                .build();
+    }
+
+    private static byte[] convertFileToByte(String path) {
+        try {
+            return TemplateReader.read(path);
+        } catch (FileNotFoundException e) {
+            return "Hello World".getBytes();
+        }
+    }
+
+    private static String buildBranchString(RequestContext ctx) {
+        return String.format("[%s]%s", ctx.getMethod(), ctx.getPath());
     }
 }
--- src/main/java/http/responses/ResponseContext.java
+++ src/main/java/http/responses/ResponseContext.java
@@ -1,4 +1,68 @@
 package http.responses;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @see <a href=https://tools.ietf.org/html/rfc2616#section-6>Response Specification</a>
+ * 요약:
+ * HTTP버전 상태코드 설명구문
+ * 응답헤더(요청 헤더처럼)
+ */
 public class ResponseContext {
+
+    private static final String DEFAULT_HTTP_VERSION = "1.1";
+
+    private final String httpVersion;
+    private final HttpStatus status;
+    private final Map<String, String> responseHeaders;
+    private final byte[] body;
+
+    // TODO: nullable 어떻게 표현할지 고민..
+    private ResponseContext(String httpVersion, HttpStatus status, Map<String, String> responseHeaders, byte[] body) {
+        this.httpVersion = httpVersion;
+        this.status = status;
+        this.responseHeaders = responseHeaders;
+        this.body = body;
+    }
+
+    public static ResponseContextBuilder builder() {
+        return new ResponseContextBuilder();
+    }
+
+    public static class ResponseContextBuilder {
+        private String version = DEFAULT_HTTP_VERSION;
+        private HttpStatus status;
+        private final Map<String, String> responseHeaders = new HashMap<>();
+        private byte[] body;
+
+        private ResponseContextBuilder() {
+        }
+
+        public ResponseContextBuilder version(String version) {
+            this.version = version;
+            return this;
+        }
+
+        public ResponseContextBuilder status(HttpStatus status) {
+            this.status = status;
+            return this;
+        }
+
+        public ResponseContextBuilder addHeader(String key, String value) {
+            this.responseHeaders.put(key, value);
+            return this;
+        }
+
+        public ResponseContextBuilder body(byte[] body) {
+            this.body = body;
+            return this;
+        }
+
+        public ResponseContext build() {
+            final String version = this.version != null ? this.version : DEFAULT_HTTP_VERSION;
+            return new ResponseContext(version, status, responseHeaders, body);
+        }
+
+    }
 }
