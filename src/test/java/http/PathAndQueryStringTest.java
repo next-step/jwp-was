c3a20116dc82aaefa3377efe6796d125bc9cd3ee
kjs4395 <kjs4395@zuminternet.com>
2020-06-04 14:09:00 +0900

    pc test

--- src/test/java/http/PathAndQueryStringTest.java
+++ src/test/java/http/PathAndQueryStringTest.java
@@ -7,10 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PathAndQueryStringTest {
     @Test
     void createQueryString() {
-        /*QueryString queryString = QueryStringParser.parser("/users?userId=javajigi&password=password&name=JaeSung");
-        assertThat(queryString)*/
-
-
+        
         PathAndQueryString pathAndQueryString = PathAndQuerySpliter.split("/users?userId=javajigi&password=password&name=JaeSung");
         assertThat(pathAndQueryString.getPath()).isEqualTo("/users");
         assertThat(pathAndQueryString.getQeuryString()).isEqualTo("userId=javajigi&password=password&name=JaeSung");
