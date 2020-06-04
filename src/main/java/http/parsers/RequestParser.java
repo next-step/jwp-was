abad74c0d7f73fe0794c250b92bd13c871f5e329
hyeyoom <neoul_chw@icloud.com>
2020-06-04 18:49:22 +0900

    feat (요청 컨텍스트 객체를 완성. 테스트 통과)

--- src/main/java/http/parsers/RequestParser.java
+++ src/main/java/http/parsers/RequestParser.java
@@ -5,7 +5,9 @@ import http.requests.RequestContext;
 import java.util.List;
 
 public class RequestParser {
+
+    // TODO: 흠.. parser 치고는 일을 너무 편하게 하는데..? 필요한 객체일까 =_=
     public static RequestContext parse(String rawRequestLine, List<String> rawRequestHeaders) {
-        return null;
+        return new RequestContext(rawRequestLine, rawRequestHeaders);
     }
 }
--- src/main/java/http/requests/RequestContext.java
+++ src/main/java/http/requests/RequestContext.java
@@ -1,4 +1,28 @@
 package http.requests;
 
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class RequestContext {
+
+    public static final String REQUEST_HEADER_DELIMITER = ":";
+    private final RequestLine requestLine;
+    private final Map<String, String> requestHeaders;
+
+    public RequestContext(String rawRequestLine, List<String> rawRequestHeaders) {
+        this.requestLine = new RequestLine(rawRequestLine);
+        this.requestHeaders = rawRequestHeaders.stream()
+                .map(this::splitRequestHeaderKeyAndValue)
+                .collect(Collectors.toMap(
+                        AbstractMap.SimpleImmutableEntry::getKey,
+                        AbstractMap.SimpleImmutableEntry::getValue)
+                );
+    }
+
+    private AbstractMap.SimpleImmutableEntry<String, String> splitRequestHeaderKeyAndValue(String header) {
+        final String[] split = header.split(REQUEST_HEADER_DELIMITER);
+        return new AbstractMap.SimpleImmutableEntry<>(split[0].trim(), split[1].trim());
+    }
 }
