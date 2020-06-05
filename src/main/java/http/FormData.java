0a91a84cfc473a10da5ef60b26312637b7adaf51
bactoria <bactoria@gmail.com>
2020-06-05 18:32:40 +0900

    FormData가 URL인코딩 된 문자열을 디코딩 하지 않고 사용하는 버그 수정

--- src/main/java/http/FormData.java
+++ src/main/java/http/FormData.java
@@ -1,19 +1,36 @@
 package http;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FormData {
     private Map<String, String> data = new HashMap<>();
 
-    public FormData(String value) {
-        for (String  v : value.split("&")) {
+    public FormData(String encodedValue) {
+
+        String value = decode(encodedValue);
+
+        for (String v : value.split("&")) {
             String[] v1 = v.split("=");
             if (v1.length != 2) {
                 throw new RuntimeException();
             }
             data.put(v1[0], v1[1]);
         }
+    }
+
+    private String decode(String encodedValue) {
+        final String EncodeType = "UTF-8";
+        String result = null;
+        try {
+            result = URLDecoder.decode(encodedValue, EncodeType);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("지원하지 않는 Encoding 타입. Type : [" + EncodeType + "]");
+        }
+        return result;
     }
 
     public String getValue(String name) {
