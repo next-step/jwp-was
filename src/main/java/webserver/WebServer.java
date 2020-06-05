2494c76d282b35bb50ba1d47ff5805ce0e5a4394
nokchax <nokchax@gmail.com>
2020-06-06 01:49:13 +0900

    refactor: code arrangement

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -11,12 +11,7 @@ public class WebServer {
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String[] args) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        int port = initPort(args);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -29,5 +24,15 @@ public class WebServer {
                 thread.start();
             }
         }
+    }
+
+    private static int initPort(String[] args) {
+        int port;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+        return port;
     }
 }
