7f61dbea732ffcb32ef95e6630e89be8f74941fa
serverwizard <serverwizard@naver.com>
2020-06-06 19:18:44 +0900

    [feature] RequestLine에 QueryStrings 추가 및 HTTPProtocol 객체로 변경

--- src/main/java/http/RequestLine.java
+++ src/main/java/http/RequestLine.java
@@ -1,18 +1,32 @@
 package http;
 
-import org.springframework.http.HttpMethod;
-
 import java.util.Objects;
 
 public class RequestLine {
+    // 학습을 위해 spring에서 제공하고 있는 HTTPMethod 를 사용하지 않고, 만들어서 사용함
     private HttpMethod method;
     private String path;
-    private Protocol protocol;
+    private QueryStrings queryStrings;
+    private HTTPProtocol protocol;
 
-    public RequestLine(String method, String path, Protocol protocol) {
+    public RequestLine(String method, String path, String protocol) {
         this.method = HttpMethod.valueOf(method);
-        this.path = path;
-        this.protocol = protocol;
+
+        String[] requestPathAndQueryString = path.split("\\?");
+        this.path = requestPathAndQueryString[0];
+        if(requestPathAndQueryString.length == 2) {
+            this.queryStrings = new QueryStrings(requestPathAndQueryString[1]);
+        }
+
+        this.protocol = new HTTPProtocol(protocol);
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public QueryStrings getQueryStrings() {
+        return queryStrings;
     }
 
     @Override
@@ -22,11 +36,12 @@ public class RequestLine {
         RequestLine that = (RequestLine) o;
         return method == that.method &&
                 Objects.equals(path, that.path) &&
+                Objects.equals(queryStrings, that.queryStrings) &&
                 Objects.equals(protocol, that.protocol);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(method, path, protocol);
+        return Objects.hash(method, path, queryStrings, protocol);
     }
 }
