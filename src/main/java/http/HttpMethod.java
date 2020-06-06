9f0e9d8394a9b8b2a6bdb62cd3b84be8e3ca665c
serverwizard <serverwizard@naver.com>
2020-06-06 19:09:00 +0900

    [feature] HttpMethod 생성 및 TC 작성

--- src/main/java/http/HttpMethod.java
+++ src/main/java/http/HttpMethod.java
@@ -3,12 +3,12 @@ package http;
 import java.util.Arrays;
 
 public enum HttpMethod {
-    GET, POST;
+    GET, POST, PUT, DELETE;
 
     public static HttpMethod findByName(String name) {
         return Arrays.stream(HttpMethod.values())
                 .filter(httpMethod -> httpMethod.name().equals(name))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(""));
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 HTTP method 이름이 존재하지 않습니다."));
     }
 }
