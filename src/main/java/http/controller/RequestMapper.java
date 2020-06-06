aafcc44c2f886e074146762e2900373e7f9b23b0
hyeyoom <neoul_chw@icloud.com>
2020-06-06 19:35:16 +0900

    feat (간단한 요청 매퍼 정의. 컨트롤러 정의는 그 다음에.)

--- src/main/java/http/controller/RequestMapper.java
+++ src/main/java/http/controller/RequestMapper.java
@@ -1,5 +1,47 @@
 package http.controller;
 
+import http.requests.HttpRequest;
+import http.types.HttpMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class RequestMapper {
-    
+
+    private static final Map<RequestBranch, Controller> CONTROLLER_MAP = new HashMap<>();
+
+    public static void addController(String path, HttpMethod method, Controller controller) {
+        final RequestBranch branch = new RequestBranch(path, method);
+        CONTROLLER_MAP.put(branch, controller);
+    }
+
+    public static Controller dispatch(HttpRequest request) {
+        return CONTROLLER_MAP.get(new RequestBranch(request.getPath(), request.getMethod()));
+    }
+
+    private static class RequestBranch {
+
+        private final String path;
+        private final HttpMethod method;
+
+        public RequestBranch(String path, HttpMethod method) {
+            this.path = path;
+            this.method = method;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            RequestBranch that = (RequestBranch) o;
+            return Objects.equals(path, that.path) &&
+                    method == that.method;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(path, method);
+        }
+    }
 }
--- src/test/java/http/controller/RequestMapperTest.java
+++ src/test/java/http/controller/RequestMapperTest.java
@@ -1,19 +1,32 @@
 package http.controller;
 
 import http.requests.HttpRequest;
+import http.types.HttpMethod;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RequestMapperTest {
 
-    private HttpRequest request;
+    private HttpRequest mockRequest;
+
+    @BeforeEach
+    void setUp() {
+        mockRequest = new HttpRequest("GET /test-path HTTP/1.1", Collections.emptyList(), "");
+    }
 
     @DisplayName("적절한 컨트롤러로 분기하는지 테스트")
     @Test
     void dispatch_request() {
-        final Controller controller = RequestMapper.dispatch(request);
-        assertThat(controller).isInstanceOf(TestController.class);
+        RequestMapper.addController("/test-path", HttpMethod.GET, new MockController());
+        final Controller controller = RequestMapper.dispatch(mockRequest);
+        assertThat(controller).isInstanceOf(MockController.class);
+    }
+
+    private class MockController implements Controller {
     }
 }
