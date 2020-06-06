e402295fb769a50a26dd47746c4de0db6dc129e3
serverwizard <serverwizard@naver.com>
2020-06-06 18:48:06 +0900

    [test] QueryString TC 케이스 추가 (해당 key를 가진 queryString이 존재하지 않는 경우)

--- src/test/java/http/QueryStringTest.java
+++ src/test/java/http/QueryStringTest.java
@@ -4,14 +4,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("QueryString 테스트")
 class QueryStringTest {
 
-    @DisplayName("해당 key를 가진 queryString이 존재하는지 확인한다.")
+    @DisplayName("해당 key를 가진 queryString의 존재 여부를 검증한다.")
     @Test
     void name() {
         QueryString queryString = new QueryString("name", "serverwizard");
-        assertThat(queryString.exists("name")).isTrue();
+
+        assertAll(
+                () -> assertThat(queryString.exists("name")).isTrue(),
+                () -> assertThat(queryString.exists("age")).isFalse()
+        );
     }
 }
