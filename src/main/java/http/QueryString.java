ab2ebea9386ce2b98755680ef6b4e0db446ab4d9
serverwizard <serverwizard@naver.com>
2020-06-06 18:43:48 +0900

    [feature] QueryString 생성 및 TC 구현

--- src/main/java/http/QueryString.java
+++ src/main/java/http/QueryString.java
@@ -1,24 +1,19 @@
 package http;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class QueryString {
-    private Map<String, String> params = new HashMap<>();
+    private final String key;
+    private final String value;
 
-    public QueryString(String queryString) {
-        String[] queryStrings = queryString.split("&");
-        Arrays.stream(queryStrings)
-                .forEach(queryString1 -> {
-                    String[] values = queryString1.split("=");
-                    String key = values[0];
-                    String value = values[1];
-                    params.put(key, value);
-                });
+    public QueryString(String key, String value) {
+        this.key = key;
+        this.value = value;
     }
 
-    public String getValue(String key) {
-        return params.get(key);
+    public boolean exists(String key) {
+        return this.key.equals(key);
+    }
+
+    public String getValue() {
+        return this.value;
     }
 }
--- src/test/java/http/QueryStringTest.java
+++ src/test/java/http/QueryStringTest.java
@@ -1,16 +1,17 @@
 package http;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class QueryStringTest {
+@DisplayName("QueryString 테스트")
+class QueryStringTest {
 
+    @DisplayName("해당 key를 가진 queryString이 존재하는지 확인한다.")
     @Test
-    void extractQueryString() {
-        QueryString queryString = new QueryString("userId=javajigi&password=password&name=JaeSung");
-        assertThat(queryString.getValue("userId")).isEqualTo("javajigi");
-        assertThat(queryString.getValue("password")).isEqualTo("password");
-        assertThat(queryString.getValue("name")).isEqualTo("JaeSung");
+    void name() {
+        QueryString queryString = new QueryString("name", "serverwizard");
+        assertThat(queryString.exists("name")).isTrue();
     }
 }
