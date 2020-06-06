ed3fd230d1a04f7d35b78c9ef713c7222d9cfa23
rimeorange <rimeorange83@gmail.com>
2020-06-07 00:07:17 +0900

    [Step 1] 요구사항 2 - POST 요청

--- src/test/java/http/RequestLineParserTest.java
+++ src/test/java/http/RequestLineParserTest.java
@@ -7,9 +7,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RequestLineParserTest {
 
     @Test
-    void parse() {
+    void parse_get() {
         RequestLine requestLine = RequestLineParser.parse("GET /users HTTP/1.1");
         assertThat(requestLine.getMapping()).isEqualTo("GET");
+        assertThat(requestLine.getPath()).isEqualTo("/users");
+        assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
+        assertThat(requestLine.getVersion()).isEqualTo("1.1");
+    }
+
+    @Test
+    void parse_post() {
+        RequestLine requestLine = RequestLineParser.parse("POST /users HTTP/1.1");
+        assertThat(requestLine.getMapping()).isEqualTo("POST");
         assertThat(requestLine.getPath()).isEqualTo("/users");
         assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
         assertThat(requestLine.getVersion()).isEqualTo("1.1");
