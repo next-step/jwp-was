ca111b25d2f850d0d2b16aa81785f77eca6a2bba
hyeyoom <neoul_chw@icloud.com>
2020-06-06 14:27:30 +0900

    refactor (HttpRequestParser 리팩터링)

--- src/main/java/utils/HttpRequestParser.java
+++ src/main/java/utils/HttpRequestParser.java
@@ -9,6 +9,7 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 요청을 파싱하는 클래스
@@ -26,12 +27,8 @@ public class HttpRequestParser {
         if (rawRequestHeaders == null || rawRequestHeaders.isEmpty()) {
             return new HttpRequestHeader(Collections.emptyMap());
         }
-        final Map<String, String> parsedHeaderMap = rawRequestHeaders.stream()
-                .map(data -> splitKeyAndValue(data, REQUEST_HEADER_DELIMITER))
-                .collect(Collectors.toMap(
-                        AbstractMap.SimpleImmutableEntry::getKey,
-                        AbstractMap.SimpleImmutableEntry::getValue)
-                );
+        final Stream<String> stream = rawRequestHeaders.stream();
+        final Map<String, String> parsedHeaderMap = streamToMap(stream, REQUEST_HEADER_DELIMITER);
         return new HttpRequestHeader(parsedHeaderMap);
     }
 
@@ -40,13 +37,8 @@ public class HttpRequestParser {
             return new FormData(Collections.emptyMap());
         }
         final String decodedPathString = URLDecoder.decode(formData, StandardCharsets.UTF_8);
-        final Map<String, String> parsedFormDataMap =
-                Arrays.stream(decodedPathString.split(FORM_DATA_PAIR_DELIMITER))
-                        .map(data -> splitKeyAndValue(data, KEY_VALUE_DELIMITER))
-                        .collect(Collectors.toMap(
-                                AbstractMap.SimpleImmutableEntry::getKey,
-                                AbstractMap.SimpleImmutableEntry::getValue)
-                        );
+        final Stream<String> stream = Arrays.stream(decodedPathString.split(FORM_DATA_PAIR_DELIMITER));
+        final Map<String, String> parsedFormDataMap = streamToMap(stream, KEY_VALUE_DELIMITER);
         return new FormData(parsedFormDataMap);
     }
 
@@ -55,12 +47,8 @@ public class HttpRequestParser {
             return new Cookie(Collections.emptyMap());
         }
         final String decodedCookie = URLDecoder.decode(cookieData, StandardCharsets.UTF_8);
-        final Map<String, String> parsedCookie = Arrays.stream(decodedCookie.split(COOKIE_DATA_PAIR_DELIMITER))
-                .map(data -> splitKeyAndValue(data, KEY_VALUE_DELIMITER))
-                .collect(Collectors.toMap(
-                        AbstractMap.SimpleImmutableEntry::getKey,
-                        AbstractMap.SimpleImmutableEntry::getValue)
-                );
+        final Stream<String> stream = Arrays.stream(decodedCookie.split(COOKIE_DATA_PAIR_DELIMITER));
+        final Map<String, String> parsedCookie = streamToMap(stream, KEY_VALUE_DELIMITER);
         return new Cookie(parsedCookie);
     }
 
@@ -70,13 +58,17 @@ public class HttpRequestParser {
         }
         final String[] pathAndQuery = pathWithQueryString.split(QUERY_STRING_DELIMITER_REGEX);
         final String decodedPathString = URLDecoder.decode(pathAndQuery[1], StandardCharsets.UTF_8);
-        final Map<String, String> parsedQueryString = Arrays.stream(decodedPathString.split(QUERY_STRING_DATA_PAIR_DELIMITER))
-                .map(data -> splitKeyAndValue(data, KEY_VALUE_DELIMITER))
+        final Stream<String> stream = Arrays.stream(decodedPathString.split(QUERY_STRING_DATA_PAIR_DELIMITER));
+        final Map<String, String> parsedQueryString = streamToMap(stream, KEY_VALUE_DELIMITER);
+        return new QueryString(parsedQueryString);
+    }
+
+    private static Map<String, String> streamToMap(Stream<String> stream, String delimiter) {
+        return stream.map(data -> splitKeyAndValue(data, delimiter))
                 .collect(Collectors.toMap(
                         AbstractMap.SimpleImmutableEntry::getKey,
                         AbstractMap.SimpleImmutableEntry::getValue)
                 );
-        return new QueryString(parsedQueryString);
     }
 
     private static AbstractMap.SimpleImmutableEntry<String, String> splitKeyAndValue(String data, String delimiter) {
