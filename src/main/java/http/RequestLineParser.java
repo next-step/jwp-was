895df17b853195f1cb712eee18b145d2525e9066
nokchax <nokchax@gmail.com>
2020-06-06 16:22:47 +0900

    feat(apply review): remove tokenizer when split exact size of string

--- src/main/java/http/RequestLineParser.java
+++ src/main/java/http/RequestLineParser.java
@@ -1,20 +1,21 @@
 package http;
 
-import utils.Tokens;
-
 public class RequestLineParser {
-    private static final int REQUIRED_TOKEN_SIZE_TO_INIT_REQUEST_LIEN = 3;
+    private static final int REQUIRED_TOKEN_SIZE_TO_INIT_REQUEST_LINE = 3;
     private static final String DELIMITER = " ";
 
     private RequestLineParser() {}
 
     public static RequestLine parse(final String requestLineStr) {
-        Tokens tokens = Tokens.init(requestLineStr, DELIMITER);
-        tokens.validate(REQUIRED_TOKEN_SIZE_TO_INIT_REQUEST_LIEN);
+        String[] tokens = requestLineStr.split(DELIMITER);
 
-        HttpMethod httpMethod = HttpMethod.of(tokens.nextToken());
-        Path path = new Path(tokens.nextToken());
-        Protocol protocol = new Protocol(tokens.nextToken());
+        if (tokens.length != REQUIRED_TOKEN_SIZE_TO_INIT_REQUEST_LINE) {
+            throw new IllegalArgumentException("Request line format is illegal");
+        }
+
+        HttpMethod httpMethod = HttpMethod.of(tokens[0]);
+        Path path = new Path(tokens[1]);
+        Protocol protocol = new Protocol(tokens[2]);
 
         return new RequestLine(httpMethod, path, protocol);
     }
