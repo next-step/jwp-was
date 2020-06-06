4bbcaa6aeeb54a5315cca7b3e1374bd332235c57
LeeSeungHee <palmseung@gmail.com>
2020-06-06 16:01:45 +0900

    Feat (handler) : add feature for work() in UserCreateHandler

--- src/test/java/webserver/UserCreateHandlerTest.java
+++ src/test/java/webserver/UserCreateHandlerTest.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import db.DataBase;
 import http.request.Request;
 import http.request.body.Body;
 import http.request.headers.Headers2;
@@ -7,6 +8,7 @@ import http.request.requestline.requestLine2.RequestLine2;
 import http.response.ContentType;
 import http.response.HttpStatus;
 import http.response.Response;
+import model.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,13 +28,16 @@ public class UserCreateHandlerTest {
         //given
         String requestLine = "POST /users HTTP/1.1";
         Headers2 headers = new Headers2(new HashMap<>());
-        Request request = new Request(new RequestLine2(requestLine), headers, new Body(""));
+        String body = "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+        Request request = new Request(new RequestLine2(requestLine), headers, new Body(body));
         UserCreateHandler handler = new UserCreateHandler("/users");
 
         //when
         Response response = handler.work(request);
 
         //then
+
+        assertThat(DataBase.findUserById("javajigi").getPassword()).isEqualTo("password");
         assertThat(response.getContentType()).isEqualTo(ContentType.HTML);
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody())
