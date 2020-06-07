9bcada8d1ecb8cdf794a19849e6e1e830f46fe90
1000141 <ninjasul@onestorecorp.com>
2020-06-07 15:46:11 +0900

    ExceptionController 수정

--- src/main/java/http/controller/ExceptionController.java
+++ src/main/java/http/controller/ExceptionController.java
@@ -1,9 +1,16 @@
 package http.controller;
 
+import http.HttpStatus;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 
 public class ExceptionController extends AbstractController {
+    private final HttpStatus httpStatus;
+
+    public ExceptionController(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) {
 
