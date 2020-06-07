8b7219ebae70f23878b7c75ac358235b4056fa0a
serverwizard <serverwizard@naver.com>
2020-06-07 12:38:16 +0900

    [feature] 동적으로 html을 생성하기 위해 핸들바 관련 코드 추가

---
+++ src/main/java/model/Users.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.List;
+
+public class Users {
+    private List<User> users;
+
+    public Users(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+}
