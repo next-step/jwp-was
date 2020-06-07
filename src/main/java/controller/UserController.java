3ad0668b075bc0d87aa1ac4494368bcbe453ddc5
serverwizard <serverwizard@naver.com>
2020-06-07 12:36:33 +0900

    [feature] 로그인 관련된 로직을 UserController로 위임

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,21 +1,35 @@
 package controller;
 
 import db.DataBase;
-import http.QueryStrings;
+import http.*;
 import model.User;
+import model.Users;
+import template.HandleBars;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 public class UserController {
 
-    public static void join(QueryStrings queryStrings) {
-        User user = new User(queryStrings.getValue("userId"), queryStrings.getValue("password"), queryStrings.getValue("name"), queryStrings.getValue("email"));
+    public static void join(HttpRequest httpRequest, HttpResponse httpResponse) {
+        QueryStrings queryStrings = httpRequest.getQueryString();
 
+        User user = new User(queryStrings.getValue("userId"), queryStrings.getValue("password"), queryStrings.getValue("name"), queryStrings.getValue("email"));
         DataBase.addUser(user);
+
+        httpResponse.response302ToIndexPage();
     }
 
-    public static boolean isLoginSuccess(QueryStrings queryStrings) {
+    public static void login(HttpRequest httpRequest, HttpResponse httpResponse) {
+        if (isLoginSuccess(httpRequest.getQueryString())) {
+            httpResponse.response302ToLoginSuccessIndexPage();
+        } else {
+            httpResponse.response302ToLoginFailedPage();
+        }
+    }
+
+    private static boolean isLoginSuccess(QueryStrings queryStrings) {
         String userId = queryStrings.getValue("userId");
         String password = queryStrings.getValue("password");
 
@@ -23,7 +37,29 @@ public class UserController {
         return savedUser.matchPassword(password);
     }
 
-    public static List<User> findAll() {
-        return new ArrayList<>(DataBase.findAll());
+    public static void findAll(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        Optional<String> maybeCookies = httpRequest.getHeaderValue(HeaderName.COOKIE);
+
+        if (maybeCookies.isPresent()) {
+            Cookies cookies = Cookies.createCookies(maybeCookies.get());
+            Optional<Cookie> maybeLoginedCookie = cookies.getValue("logined");
+
+            if (maybeLoginedCookie.isPresent()) {
+                Cookie loginedCookie = maybeLoginedCookie.get();
+                String logined = loginedCookie.getValue();
+
+                if (Boolean.parseBoolean(logined)) {
+                    Users users = new Users(new ArrayList<>(DataBase.findAll()));
+
+                    String dynamicCreatedHtml = HandleBars.getHTML(httpRequest.getPath(), users);
+                    byte[] body = dynamicCreatedHtml.getBytes();
+
+                    httpResponse.response200Header("text/html", body.length);
+                    httpResponse.responseBody(body);
+                }
+            }
+        }
+
+        httpResponse.response302ToLoginPage();
     }
 }
