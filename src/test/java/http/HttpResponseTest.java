9113fb18cef3bde6b5a7e97e2eaf3d939f3695f0
nokchax <nokchax@gmail.com>
2020-06-07 14:30:12 +0900

    chore: code arrangement

--- src/test/java/http/HttpResponseTest.java
+++ src/test/java/http/HttpResponseTest.java
@@ -3,7 +3,6 @@ package http;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
