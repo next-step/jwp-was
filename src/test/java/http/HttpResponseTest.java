1f4e57ba4d654eb9472b7adddbe0f79bfc152f57
nokchax <nokchax@gmail.com>
2020-06-07 14:30:00 +0900

    test: redirect return exception when accept null or empty str

--- src/test/java/http/HttpResponseTest.java
+++ src/test/java/http/HttpResponseTest.java
@@ -2,8 +2,12 @@ package http;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("Http response 객체에 대한 테스트")
 class HttpResponseTest {
@@ -24,4 +28,15 @@ class HttpResponseTest {
         assertThat(httpResponse.getLocation()).isEqualTo(location);
         assertThat(httpResponse.getStatusCode()).isEqualTo(StatusCode.REDIRECT);
     }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("redirect 로 null 이나 빈 문자열을 넘기는 경우 예외 발생")
+    void redirectFail(final String location) {
+        HttpResponse httpResponse = HttpResponse.init();
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> httpResponse.sendRedirect(location));
+    }
+
 }
