6afd53353e2adf78d562b3401e662dae2104d425
nokchax <nokchax@gmail.com>
2020-06-07 16:08:44 +0900

    fix: test failure code fix

--- src/test/java/http/RequestHeaderTest.java
+++ src/test/java/http/RequestHeaderTest.java
@@ -32,6 +32,6 @@ class RequestHeaderTest {
         assertThat(requestHeader.getHeader("not exist")).isNull();
         assertThat(requestHeader.getCookie("foo")).isEqualTo("bar");
         assertThat(requestHeader.getCookie("lorem")).isEqualTo("ipsum");
-        assertThat(requestHeader.getHeaders()).hasSize(11);
+        assertThat(requestHeader.getHeaders()).hasSize(12);
     }
 }
