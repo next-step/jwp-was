d3d60c6cd6a68eb3a311673ad6e66547b3fc9db8
nokchax <nokchax@gmail.com>
2020-06-07 15:18:37 +0900

    test: login controller test

---
+++ src/test/java/controller/FileLoader.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class FileLoader {
+    private static final String TEST_DIRECTORY = "./src/test/resources/";
+
+    public static InputStream load(final String path) {
+        try {
+            return new FileInputStream(new File(TEST_DIRECTORY + path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
