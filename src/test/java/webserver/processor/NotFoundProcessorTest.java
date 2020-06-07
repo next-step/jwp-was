1657a50186e7e08b462540ec83369c8c9bd49695
nokchax <nokchax@gmail.com>
2020-06-07 13:09:39 +0900

    test: after not found processors's process, http response's status code is 404 not found

--- src/test/java/webserver/processor/NotFoundProcessorTest.java
+++ src/test/java/webserver/processor/NotFoundProcessorTest.java
@@ -1,5 +1,8 @@
 package webserver.processor;
 
+import http.HttpRequest;
+import http.HttpResponse;
+import http.StatusCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import testutils.HttpRequestGenerator;
@@ -18,5 +21,16 @@ class NotFoundProcessorTest {
         assertThat(
                 notFoundProcessor.isMatch(HttpRequestGenerator.init("GET somepath HTTP/1.1"))
         ).isTrue();
+    }
+
+    @Test
+    @DisplayName("process 를 통과하면 항상 response의 statuscode 값은 404이다")
+    void process() throws IOException {
+        HttpRequest httpRequest = HttpRequestGenerator.init("GET somepath HTTP/1.1");
+        HttpResponse httpResponse = HttpResponse.init();
+
+        notFoundProcessor.process(httpRequest, httpResponse);
+
+        assertThat(httpResponse.getStatusCode()).isEqualTo(StatusCode.NOT_FOUND);
     }
 }
