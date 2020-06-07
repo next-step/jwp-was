6dc0830b7266eaf47adcdf1b8a49796b595213f8
nokchax <nokchax@gmail.com>
2020-06-07 12:51:00 +0900

    test: is match test add test case

--- src/test/java/webserver/processor/TemplateProcessorTest.java
+++ src/test/java/webserver/processor/TemplateProcessorTest.java
@@ -6,11 +6,15 @@ import http.StatusCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import testutils.HttpRequestGenerator;
 import utils.FileIoUtils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,14 +30,24 @@ class TemplateProcessorTest {
         httpResponse = HttpResponse.init();
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource
     @DisplayName(".html로 끝나는 요청이 template processor와 매치하는지")
-    void isMatch() {
-        assertThat(templateProcessor.isMatch(httpRequest)).isTrue();
+    void isMatch(final HttpRequest httpRequest, final boolean expected) {
+        assertThat(templateProcessor.isMatch(httpRequest)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> isMatch() throws IOException {
+        HttpRequest httpRequest = HttpRequestGenerator.init("GET /index.html HTTP/1.1");
+        return Stream.of(
+                Arguments.of(httpRequest, true),
+                Arguments.of(HttpRequestGenerator.init("GET /index.htm HTTP/1.1"), false)
+        );
     }
 
     @Test
     @DisplayName(".html로 끝나는 요청인데 해당하는 파일이 없는경우")
+
     void matchButNotExist() throws IOException {
         HttpRequest httpRequest = HttpRequestGenerator.init("GET /not-exist.html HTTP/1.1");
 
