c6454b636f47a0dd3d9b8ee6de16c9286bb7faf9
bactoria <bactoria@gmail.com>
2020-06-07 15:31:36 +0900

    test : Header 객체에서 값을 가져오는 테스트 코드 추가

--- src/test/java/http/HeaderTest.java
+++ src/test/java/http/HeaderTest.java
@@ -17,4 +17,19 @@ class HeaderTest {
         assertThat(header).isNotNull();
     }
 
+    @Test
+    @DisplayName("Header 객체에서 헤더 이름으로 값을 가져올 수 있다")
+    void test2() {
+        final String[] input = {"Host: localhost:8080", "Connection: keep-alive", "Origin: http://localhost:8080"};
+        final Header header = new Header(input);
+
+        final String HostValue = header.getValue("Host");
+        final String ConnectionValue = header.getValue("Connection");
+        final String OriginValue = header.getValue("Origin");
+
+        assertThat(HostValue).isEqualTo("localhost:8080");
+        assertThat(ConnectionValue).isEqualTo("keep-alive");
+        assertThat(OriginValue).isEqualTo("http://localhost:8080");
+    }
+
 }
