b57dc9078c509bb92ac3c1a5668ccefa99458856
nokchax <nokchax@gmail.com>
2020-06-07 15:26:21 +0900

    test: user create test

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -3,29 +3,22 @@ package controller;
 import db.DataBase;
 import http.HttpRequest;
 import http.HttpResponse;
+import http.StatusCode;
 import model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sun.plugin.dom.exception.InvalidAccessException;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Collection;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("유저 컨트롤러")
 class UserControllerTest {
-    private static final String RAW_REQUEST =
-            " /user/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1\n" +
-            "Host: localhost:8080\n" +
-            "Connection: keep-alive\n" +
-            "Accept: */*";
-    private static final String RAW_GET_REQUEST = "GET" + RAW_REQUEST;
-    private static final String RAW_POST_REQUEST = "POST" + RAW_REQUEST;
-
-    private static final UserController USER_CONTROLLER = new UserController();
+    private static final AbstractController USER_CONTROLLER = new UserController();
 
     @BeforeEach
     void clear() {
@@ -33,24 +26,29 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("/user/create 호출시 유저를 메모리에 저장한다.")
+    @DisplayName("/user/create 호출시 예외 발생")
     void create() throws IOException {
-        HttpRequest httpRequest = HttpRequest.readRawRequest(new ByteArrayInputStream(RAW_GET_REQUEST.getBytes()));
+        HttpRequest httpRequest = HttpRequest.readRawRequest(FileLoader.load("user-create-get.txt"));
+        HttpResponse httpResponse = HttpResponse.init();
 
         assertThatExceptionOfType(InvalidAccessException.class)
-                .isThrownBy(() -> USER_CONTROLLER.process(httpRequest, HttpResponse.init()));
+                .isThrownBy(() -> USER_CONTROLLER.process(httpRequest, httpResponse));
+
+        assertThat(httpResponse.getStatusCode()).isEqualTo(StatusCode.NOT_FOUND);
     }
 
     @Test
     @DisplayName("/user/create POST 호출시 유저를 메모리에 저장한다.")
     void createUsingPost() throws IOException {
-        HttpRequest httpRequest = HttpRequest.readRawRequest(new ByteArrayInputStream(RAW_POST_REQUEST.getBytes()));
+        HttpRequest httpRequest = HttpRequest.readRawRequest(FileLoader.load("user-create-post.txt"));
+        HttpResponse httpResponse = HttpResponse.init();
         Collection<User> users = DataBase.findAll();
 
         assertThat(users).isEmpty();
-
-        USER_CONTROLLER.process(httpRequest, HttpResponse.init());
+        USER_CONTROLLER.process(httpRequest, httpResponse);
 
         assertThat(DataBase.findAll()).hasSize(1);
+        assertThat(httpResponse.getStatusCode()).isEqualTo(StatusCode.REDIRECT);
+        assertThat(httpResponse.getLocation()).isEqualTo("/index.html");
     }
 }
