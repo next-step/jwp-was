4a10a4ef530695b7207910362513f37d738288a7
bactoria <bactoria@gmail.com>
2020-06-07 15:37:50 +0900

    feat : Header 객체에서 헤더 이름으로 값을 가져오는 기능 추가

--- src/main/java/http/Header.java
+++ src/main/java/http/Header.java
@@ -1,7 +1,23 @@
 package http;
 
-public class Header {
+import java.util.HashMap;
+import java.util.Map;
 
-    public Header(String[] input) {
+public class Header {
+    private static final String HEADER_NAME_VALUE_TOKENIZER = ":";
+
+    private Map<String, String> data = new HashMap<>();
+
+    public Header(String[] headers) {
+        for (String header : headers) {
+            String[] h = header.split(HEADER_NAME_VALUE_TOKENIZER,2);
+            String headerName = h[0].trim();
+            String headerValue = h[1].trim();
+            data.put(headerName, headerValue);
+        }
+    }
+
+    public String getValue(String headerName) {
+        return data.get(headerName);
     }
 }
