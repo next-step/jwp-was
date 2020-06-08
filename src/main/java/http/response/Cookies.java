5d6e2ab3946774320f625b824379cd0031c2378b
LeeSeungHee <palmseung@gmail.com>
2020-06-08 17:39:30 +0900

    Test (cookies) : add test to create Cookies

---
+++ src/main/java/http/response/Cookies.java
@@ -0,0 +1,12 @@
+package http.response;
+
+import java.util.List;
+
+public class Cookies {
+    public Cookies(List<Cookie> cookies) {
+    }
+
+    public int getSize() {
+        return 0;
+    }
+}
---
+++ src/test/java/http/response/CookiesTest.java
@@ -0,0 +1,29 @@
+package http.response;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CookiesTest {
+    @DisplayName("Cookies 생성")
+    @Test
+    void create() {
+        //given
+        Cookie cookie = new Cookie("JSESSIONID", "abc1234", "/", true);
+        Cookie cookie2 = new Cookie("JSESSIONID2", "abc12345", "/index", false);
+
+        //when
+        Cookies cookies = new Cookies(Arrays.asList(cookie, cookie2));
+
+        //then
+        assertThat(cookies.getSize()).isEqualTo(2);
+        assertThat(cookies.toString())
+                .isEqualTo("JSESSIONID=abc1234; Path=/; HttpOnly"
+                        +"&"
+                        +"JSESSIONID2=abc12345; Path=/index; ");
+    }
+}
