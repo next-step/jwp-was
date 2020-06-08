e3486794481e4c8d6f3e2134cd4d25bb13560be7
nokchax <nokchax@gmail.com>
2020-06-08 21:07:27 +0900

    feat: when session id is null or empty string then of static factory method throw exception

--- src/main/java/session/DefaultHttpSession.java
+++ src/main/java/session/DefaultHttpSession.java
@@ -1,5 +1,7 @@
 package session;
 
+import utils.StringUtil;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,10 +12,18 @@ public class DefaultHttpSession implements HttpSession {
     private DefaultHttpSession() {}
 
     public static DefaultHttpSession of(final String sessionId) {
+        validate(sessionId);
+
         DefaultHttpSession httpSession = new DefaultHttpSession();
         httpSession.sessionId = sessionId;
 
         return httpSession;
+    }
+
+    private static void validate(final String sessionId) {
+        if (StringUtil.isEmpty(sessionId)) {
+            throw new IllegalArgumentException("Session id can't be a null or empty string");
+        }
     }
 
     @Override
