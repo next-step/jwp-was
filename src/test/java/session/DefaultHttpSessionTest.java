0e59854c9245b95fd10bd2dd4e4d8a92895b41f2
nokchax <nokchax@gmail.com>
2020-06-08 21:06:04 +0900

    test: when session id is null or empty string then of static factory method throw exception

--- src/test/java/session/DefaultHttpSessionTest.java
+++ src/test/java/session/DefaultHttpSessionTest.java
@@ -2,8 +2,11 @@ package session;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("세션 attribute 를 관리하는 세션 객체")
 class DefaultHttpSessionTest {
@@ -14,6 +17,14 @@ class DefaultHttpSessionTest {
         String sessionId = "new id";
 
         assertThatCode(() -> DefaultHttpSession.of(sessionId)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("세션 키값이 null 이거나 빈 문자열일 경우에는 예외가 발생한다.")
+    void ofException(final String sessionId) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> DefaultHttpSession.of(sessionId));
     }
 
 }
