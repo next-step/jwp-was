125480796eef1fb1711a8d84e14ea81a4043835a
nokchax <nokchax@gmail.com>
2020-06-08 10:17:00 +0900

    test: parse query test

--- src/main/java/http/request/Parameters.java
+++ src/main/java/http/request/Parameters.java
@@ -1,7 +1,20 @@
 package http.request;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Parameters {
+    private Map<String, String> parameters;
+
+    private Parameters() {
+        this.parameters = new HashMap<>();
+    }
+
     public static Parameters newInstance() {
         return new Parameters();
+    }
+
+    public void parse(final String query) {
+
     }
 }
--- src/test/java/http/request/ParametersTest.java
+++ src/test/java/http/request/ParametersTest.java
@@ -10,7 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParametersTest {
 
     @Test
+    @DisplayName("정적 팩토리 메서드로 생성")
     void newInstance() {
         assertThatCode(Parameters::newInstance).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("쿼리 스트링, 혹은 body를 받아서 파라미터에 추가")
+    void parse() {
+        String query = "name1=value1&name2=value2";
+
+        Parameters parameters = Parameters.newInstance();
+        parameters.parse(query);
     }
 }
