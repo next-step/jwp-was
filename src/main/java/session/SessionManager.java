60487cabecb9ce8e55c68e8270c13783d3fe11da
nokchax <nokchax@gmail.com>
2020-06-08 20:44:54 +0900

    test: when client access first time then session manager make new session key and session and add it to response

---
+++ src/main/java/session/SessionManager.java
@@ -0,0 +1,10 @@
+package session;
+
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+
+public class SessionManager {
+    public void loadSession(final HttpRequest httpRequest, final HttpResponse httpResponse) {
+
+    }
+}
---
+++ src/test/java/session/SessionManagerTest.java
@@ -0,0 +1,37 @@
+package session;
+
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import testutils.HttpRequestGenerator;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("모든 request 에 대해서 세션을 셍상히고 로드하는 작업을 함")
+class SessionManagerTest {
+    private SessionManager sessionManager;
+
+    @BeforeEach
+    void setEnv() {
+        sessionManager = new SessionManager();
+    }
+
+    @Test
+    @DisplayName("세션 메니저는 http request 와 http response 를 파라미터로 받아 세션 키를 생성 및 응답값에 저장한다.")
+    void loadSession() throws IOException {
+        HttpRequest httpRequest = HttpRequestGenerator.init("GET /test HTTP/1.1");
+        HttpResponse httpResponse = HttpResponse.init();
+
+        Map<String, String> cookies = httpResponse.getCookies();
+
+        assertThat(cookies).hasSize(0);
+        sessionManager.loadSession(httpRequest, httpResponse);
+        assertThat(cookies).hasSize(1);
+        assertThat(cookies.get("suid")).isNotNull();
+    }
+}
