bcdadc4eefc8e83581f33ba79d7f4743002c9ec1
bactoria <bactoria@gmail.com>
2020-06-09 17:41:01 +0900

    test : 테스트 코드 추가

--- src/test/java/http/request/FormDataTest.java
+++ src/test/java/http/request/FormDataTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
@@ -31,6 +34,20 @@ class FormDataTest {
         assertThat(password).isEqualTo("password1");
         assertThat(name).isEqualTo("name1");
         assertThat(email).isEqualTo("aa1@aa.a");
+    }
+
+    @Test
+    @DisplayName("URL인코딩된 formData을 넣은 경우 가져올 때에는 URL디코딩되어 나온다")
+    void getDecoded() throws UnsupportedEncodingException {
+        final String key = "password";
+        final String value = "p&a&s&==s";
+        final String valueURLEncoded = URLEncoder.encode(value, "UTF-8");
+        final String input = "userId=a&" + key + "=" + valueURLEncoded;
+        final FormData formData = new FormData(input);
+
+        final String result = formData.getValue(key);
+
+        assertThat(result).isEqualTo(value);
     }
 
     @ParameterizedTest
