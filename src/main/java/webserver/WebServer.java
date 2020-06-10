e19763140625ebd8b06830089be0f50ca92ac5db
hyeyoom <neoul_chw@icloud.com>
2020-06-11 01:09:14 +0900

    feat (요구사항1 - 스레드 풀 사용하도록 수정)

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -5,14 +5,27 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
+    private static final int INIT_SIZE_OF_THREAD_POOL = 10;
+    private static final int SIZE_OF_THREAD_POOL = 250;
+    private static final int KEEP_ALIVE_TIME = 30;
+    private static final int WORK_QUEUE_CAPACITY = 100;
+    private static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(
+            INIT_SIZE_OF_THREAD_POOL,
+            SIZE_OF_THREAD_POOL, KEEP_ALIVE_TIME, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(WORK_QUEUE_CAPACITY)
+    );
+
     public static void main(String[] args) throws Exception {
         final int port = (args == null || args.length == 0) ? DEFAULT_PORT : Integer.parseInt(args[0]);
-        
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -20,8 +33,7 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                THREAD_POOL_EXECUTOR.execute(new RequestHandler(connection));
             }
         }
     }
