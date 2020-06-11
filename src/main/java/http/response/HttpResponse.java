c3603380b744ae675b8a0cb4b00aa7312947fa71
liquidjoo <liquidjoo@gmail.com>
2020-06-11 15:11:40 +0900

    reafactor(ResponseHeader) : Response Header 중복 제거 및 책임 분리

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -2,63 +2,60 @@ package http.response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
 
 public class HttpResponse {
 
     private static final String BASIC_PROTOCOL = "HTTP/1.1";
-    private static final String BASIC_CONTENT_KEY = "Content-Type:";
     private static final String BASIC_REQUEST = "%s %s \r\n";
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
 
     private final DataOutputStream dataOutputStream;
+    private final ResponseHeader responseHeader;
 
     public HttpResponse(final DataOutputStream dataOutputStream) {
         this.dataOutputStream = dataOutputStream;
+        this.responseHeader = new ResponseHeader();
     }
 
-    public void responseHeaderByCss() {
+    private void applyHeader() throws IOException {
+        for (final Map.Entry<String, String> entry : responseHeader.getHeader().entrySet()) {
+            dataOutputStream.writeBytes(entry.getKey() + ": " + entry.getValue() + "\r\n");
+        }
+    }
+
+    public void addHeader(String key, String value) {
+        responseHeader.addHeader(key, value);
+    }
+
+    public void forward(String path) {
         try {
             responseOK();
-            dataOutputStream.writeBytes("Host: localhost:8080 \r\n");
-            dataOutputStream.writeBytes("Accept: text/css,*/*;q=0.1 \r\n");
-            dataOutputStream.writeBytes("Connection: keep-alive \r\n");
-            dataOutputStream.writeBytes("\r\n");
+            responseHeader.addHeader("Host", "localhost:8080");
+            responseHeader.addHeader("Accept", "text/css,*/*;q=0.1");
+            responseHeader.addHeader("Connection", "keep-alive");
+            applyHeader();
+            responseBody(readFile("./static/" + path));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void response302Header(String location) {
-        try {
-            responseFound();
-            dataOutputStream.writeBytes("Location: " + location + " \r\n");
-            dataOutputStream.writeBytes("\r\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void forwardBody(String path) {
+
     }
 
-    public void responseHeaderByLoginSuccess() {
+    public void sendRedirect(String path) {
         try {
             responseFound();
-            responseContentTypeHtml();
-            dataOutputStream.writeBytes("Set-Cookie: logined=true; Path=/ \r\n");
-            dataOutputStream.writeBytes("Location: /index.html \r\n");
-            dataOutputStream.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void response302HeaderByLoginFail(String location) {
-        try {
-            responseFound();
-            dataOutputStream.writeBytes("Set-Cookie: logined=false; Path=/ \r\n");
-            dataOutputStream.writeBytes("Location: " + location + " \r\n");
-            dataOutputStream.writeBytes("\r\n");
+            responseHeader.addHeader("Content-Type", ResponseContentType.HTML.toString());
+            responseHeader.addHeader("Location", path);
+            applyHeader();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -67,9 +64,18 @@ public class HttpResponse {
     public void response200Header(int lengthOfBodyContent) {
         try {
             responseOK();
-            responseContentTypeHtml();
-            dataOutputStream.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dataOutputStream.writeBytes("\r\n");
+            responseHeader.addHeader("Content-Type", ResponseContentType.HTML.toString());
+            responseHeader.addHeader("Content-Length", String.valueOf(lengthOfBodyContent));
+            applyHeader();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void responseBody(byte[] body) {
+        try {
+            dataOutputStream.write(body, 0, body.length);
+            dataOutputStream.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -83,16 +89,15 @@ public class HttpResponse {
         dataOutputStream.writeBytes(String.format(BASIC_REQUEST, BASIC_PROTOCOL, ResponseHttpStatus.Found.toString()));
     }
 
-    private void responseContentTypeHtml() throws IOException {
-        dataOutputStream.writeBytes(String.format(BASIC_REQUEST, BASIC_CONTENT_KEY, ResponseContentType.HTML.toString()));
+    private byte[] readFile(String filePath) {
+        try {
+            return viewByUserForm(filePath);
+        } catch (IOException | URISyntaxException | NullPointerException e) {
+            throw new IllegalArgumentException("Not Found Path");
+        }
     }
 
-    public void responseBody(byte[] body) {
-        try {
-            dataOutputStream.write(body, 0, body.length);
-            dataOutputStream.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+    private byte[] viewByUserForm(String path) throws IOException, URISyntaxException {
+        return FileIoUtils.loadFileFromClasspath(path);
     }
 }
