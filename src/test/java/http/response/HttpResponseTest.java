e54302eb6300e97ef8ee78e1ac98f710487abc96
bactoria <bactoria@gmail.com>
2020-06-12 14:09:46 +0900

    test : 리팩토링에 따른 테스트 코드 변경

--- src/test/java/http/response/HttpResponseTest.java
+++ src/test/java/http/response/HttpResponseTest.java
@@ -1,5 +1,6 @@
 package http.response;
 
+import http.common.ContentType;
 import http.common.HeaderFieldName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,29 +26,30 @@ class HttpResponseTest {
     }
 
     @Test
-    @DisplayName("response200HTML 메서드 호출 시 상태코드는 302가 된다")
+    @DisplayName("response200 메서드 호출 시 상태코드는 302가 된다")
     void response200HtmlStatusCodeTest() {
         final StatusCode statusCode = StatusCode.OK;
         final int expected = statusCode.getCode();
         final byte[] body = new byte[0];
         final HttpResponse httpResponse = new HttpResponse();
 
-        httpResponse.response200HTML(body);
+        httpResponse.response200(ContentType.TEXT_CSS_UTF_8, body);
         int result = httpResponse.getStatusCode();
 
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    @DisplayName("response200HTML 메서드 호출 시 정상적인 Content-Type 헤더가 추가된다")
+    @DisplayName("response200 메서드 호출 시 정상적인 Content-Type 헤더가 추가된다")
     void response200HtmlContentTypeTest() {
-        final HeaderFieldName contentType = HeaderFieldName.CONTENT_TYPE;
-        final String expected = "text/html;charset=utf-8";
+        final HeaderFieldName contentTypeName = HeaderFieldName.CONTENT_TYPE;
+        final ContentType contentTypeValue = ContentType.TEXT_HTML_UTF_8;
+        final String expected = contentTypeValue.getValue();
         final byte[] body = new byte[0];
         final HttpResponse httpResponse = new HttpResponse();
 
-        httpResponse.response200HTML(body);
-        String result = httpResponse.getHeader(contentType);
+        httpResponse.response200(contentTypeValue, body);
+        String result = httpResponse.getHeader(contentTypeName);
 
         assertThat(result).isEqualTo(expected);
     }
@@ -61,53 +63,7 @@ class HttpResponseTest {
         final String expected = String.valueOf(body.length);
         final HttpResponse httpResponse = new HttpResponse();
 
-        httpResponse.response200HTML(body);
-        String result = httpResponse.getHeader(contentLength);
-
-        assertThat(result).isEqualTo(expected);
-    }
-
-
-    @Test
-    @DisplayName("response200CSS 메서드 호출 시 상태코드는 302가 된다")
-    void response200CssStatusCodeTest() {
-        final StatusCode statusCode = StatusCode.OK;
-        final int expected = statusCode.getCode();
-        final byte[] body = new byte[0];
-        final HttpResponse httpResponse = new HttpResponse();
-
-        httpResponse.response200CSS(body);
-        int result = httpResponse.getStatusCode();
-
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("response200CSS 메서드 호출 시 정상적인 Content-Type 헤더가 추가된다")
-    void response200CssContentTypeTest() {
-        final HeaderFieldName contentType = HeaderFieldName.CONTENT_TYPE;
-        final String expected = "text/css;charset=utf-8";
-        final byte[] body = new byte[0];
-        final HttpResponse httpResponse = new HttpResponse();
-
-        httpResponse.response200CSS(body);
-        String result = httpResponse.getHeader(contentType);
-
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("response200CSS 메서드 호출 시 정상적인 Content-Length 헤더가 추가된다")
-    void response200CssContentLengthTest() {
-        final HeaderFieldName contentLength = HeaderFieldName.CONTENT_LENGTH;
-        String css = "body {\n" +
-                "  background-color:#e0e0e0;\n" +
-                "}";
-        final byte[] body = css.getBytes();
-        final String expected = String.valueOf(body.length);
-        final HttpResponse httpResponse = new HttpResponse();
-
-        httpResponse.response200CSS(body);
+        httpResponse.response200(ContentType.TEXT_HTML_UTF_8, body);
         String result = httpResponse.getHeader(contentLength);
 
         assertThat(result).isEqualTo(expected);
