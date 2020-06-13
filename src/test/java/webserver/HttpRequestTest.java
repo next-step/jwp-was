bac8407f0505bd48a5575047c23a810caf0c887c
bactoria <bactoria@gmail.com>
2020-06-14 03:14:42 +0900

    test : InputStream 형태의 Http Request Message 를 HttpRequest로 파싱하는 테스트 코드 추가

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,18 +1,29 @@
 package webserver;
 
+import http.request.HttpMethod;
+import http.request.HttpRequest;
+import http.request.parser.RequestReader;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpRequestTest {
+    private String testDirectory = "./src/test/resources/";
+
     @Test
-    void request_resttemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        String resourceUrl = "https://edu.nextstep.camp";
-        ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/c/4YUvqn9V", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    @DisplayName("Http Request Message를 정상적으로 파싱할 수 있다")
+    public void request_GET() throws Exception {
+        final InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        final HttpRequest request = RequestReader.read(in);
+
+        assertEquals(HttpMethod.GET, request.getMethod());
+        assertEquals("/user/create", request.getPath());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("javajigi", request.getParameter("userId"));
     }
 }
