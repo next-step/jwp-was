7d29e66d5311c1239e8eaa5fb11fc9d4c8b8c339
bactoria <bactoria@gmail.com>
2020-06-13 16:11:05 +0900

    feat : HttpSession 에서 attribute를 생성하고 가져오는 기능 추가

--- src/main/java/http/common/HttpSession.java
+++ src/main/java/http/common/HttpSession.java
@@ -1,17 +1,31 @@
 package http.common;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class HttpSession {
 
     private String id;
+    private Map<String, Object> attributes;
 
     public HttpSession() {
         UUID uuid = UUID.randomUUID();
         this.id = uuid.toString();
+        this.attributes = new HashMap<>();
     }
 
     public String getId() {
         return id;
     }
+
+    public void setAttribute(String key, Object value) {
+        attributes.put(key, value);
+    }
+
+    public Object getAttribute(String key) {
+        return attributes.get(key);
+    }
+
 }
