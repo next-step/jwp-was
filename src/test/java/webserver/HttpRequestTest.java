b4c66c3f315d2512858e1115f8d4048f3630542f
liquidjoo <liquidjoo@gmail.com>
2020-06-19 02:18:06 +0900

    feat : 요구사항 2 - 쓰레드풀 < 요청카운트 일 때 반응

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,13 +1,37 @@
 package webserver;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestTest.class);
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService es = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 25; i++) {
+            es.execute(() -> {
+                RestTemplate restTemplate = new RestTemplate();
+                String resourceUrl = "https://edu.nextstep.camp";
+                ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/c/4YUvqn9V", String.class);
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            });
+        }
+        es.shutdown();
+        es.awaitTermination(100, TimeUnit.SECONDS);
+    }
+
     @Test
     void request_resttemplate() {
         RestTemplate restTemplate = new RestTemplate();
