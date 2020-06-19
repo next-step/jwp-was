a76e954de229800eba863e5ceafba131efece195
박우빈 (Park WooBean) <pwb0127@gmail.com>
2020-06-19 21:22:38 +0900

    [Step5] Thread Pool 적용 (#188)
    * feature: 쓰레드풀 적용
    
    * test: ThreadPool 수보다 많은 요청을 동시에 보내보는 테스트

--- src/test/java/webserver/ExecutorsTest.java
+++ src/test/java/webserver/ExecutorsTest.java
@@ -2,20 +2,30 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ExecutorsTest {
+
     private static final Logger logger = LoggerFactory.getLogger(ExecutorsTest.class);
 
     private static AtomicInteger counter = new AtomicInteger(0);
+    private static RestTemplate restTemplate = new RestTemplate();
 
     public static void main(String[] args) throws Exception {
-        ExecutorService es = Executors.newFixedThreadPool(100);
+        ExecutorService es = Executors.newFixedThreadPool(10);
+
+        String requestUri = "http://localhost:8080/index.html";
 
         StopWatch sw = new StopWatch();
         sw.start();
@@ -23,8 +33,9 @@ public class ExecutorsTest {
             es.execute(() -> {
                 int idx = counter.addAndGet(1);
                 try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
+                    ResponseEntity<String> responseEntity = restTemplate.getForEntity(new URI(requestUri), String.class);
+                    assertThat(responseEntity.getStatusCode().is2xxSuccessful()).isTrue();
+                } catch (URISyntaxException e) {
                     e.printStackTrace();
                 }
                 logger.info("Thread {}", idx);
