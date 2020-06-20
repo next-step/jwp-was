42db63165f5c692c479384832831173b9bcc0e95
wongeun.song <wongeun.song@navercorp.com>
2020-06-20 16:01:22 +0900

    [step5] - threadpoolexecutor 적

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,14 +1,21 @@
 package webserver;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int THREAD_CORE_POOL_SIZE = 250;
+    private static final int THREAD_MAX_POOL_SIZE = 250;
+    private static final int KEEP_ALIVE_TIMEOUT = 30;
+    private static final int QUEUE_SIZE = 100;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -25,8 +32,11 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                        THREAD_CORE_POOL_SIZE, THREAD_MAX_POOL_SIZE,
+                        KEEP_ALIVE_TIMEOUT, TimeUnit.SECONDS,
+                        new LinkedBlockingQueue<>(QUEUE_SIZE));
+                threadPoolExecutor.execute(new Thread(new RequestHandler(connection)));
             }
         }
     }
--- src/test/java/webserver/ExecutorsTest.java
+++ src/test/java/webserver/ExecutorsTest.java
@@ -4,15 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class ExecutorsTest {
     private static final Logger logger = LoggerFactory.getLogger(ExecutorsTest.class);
 
     private static AtomicInteger counter = new AtomicInteger(0);
+    private static AtomicInteger counter2 = new AtomicInteger(0);
 
     public static void main(String[] args) throws Exception {
         ExecutorService es = Executors.newFixedThreadPool(100);
@@ -31,10 +31,22 @@ public class ExecutorsTest {
             });
         }
         sw.stop();
-
         es.shutdown();
         es.awaitTermination(100, TimeUnit.SECONDS);
         logger.info("Total Elaspsed: {}", sw.getTotalTimeSeconds());
+
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 100, 1, TimeUnit.MICROSECONDS, new LinkedBlockingQueue<>(5));
+        ThreadPoolExecutor threadPoolExecutor2 = new ThreadPoolExecutor(10, 100, 1, TimeUnit.MICROSECONDS, new SynchronousQueue<>());
+        IntStream.range(0, 30).forEach(a -> threadPoolExecutor.execute(() -> {
+            int i = counter2.addAndGet(1);
+            logger.info(Thread.currentThread().toString() + "===" + i);
+        }));
+
+        IntStream.range(0, 30).forEach(a -> threadPoolExecutor2.execute(() -> {
+            int i = counter2.addAndGet(1);
+            logger.info(Thread.currentThread().toString() + "===" + i);
+        }));
     }
 }
 
