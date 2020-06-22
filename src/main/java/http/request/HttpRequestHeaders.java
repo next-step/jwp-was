86656c19755fafa2b09cd98fcaa2b310eccd2855
jinwoo <jwlee@qualson.com>
2020-06-22 23:57:53 +0900

    Refactor(HttpsRequestHeaders, HttpResponseHeaders) : extends Headers class and delete functions for headers

--- src/main/java/http/request/HttpRequestHeaders.java
+++ src/main/java/http/request/HttpRequestHeaders.java
@@ -1,32 +1,11 @@
 package http.request;
 
-import com.google.common.collect.Maps;
+import http.Headers;
 import http.HttpCookies;
 
-import java.util.Collection;
-import java.util.Map;
-
-public class HttpRequestHeaders {
-    private static final String HEADER_DELIMITER = ": ";
+public class HttpRequestHeaders extends Headers {
     private static final String CONTENT_LENGTH = "Content-Length";
     private static final String COOKIE = "Cookie";
-    private final Map<String, String> values = Maps.newHashMap();
-
-    public HttpRequestHeaders() {
-    }
-
-    public void addHeader(final String header) {
-        final String[] keyAndValue = header.trim().split(HEADER_DELIMITER);
-        values.put(keyAndValue[0], keyAndValue[1]);
-    }
-
-    public void addHeader(final String key, final String value) {
-        values.put(key, value);
-    }
-
-    public String getHeader(final String key) {
-        return values.getOrDefault(key, null);
-    }
 
     public int getContentLength() {
         final String value = getHeader(CONTENT_LENGTH);
@@ -35,9 +14,5 @@ public class HttpRequestHeaders {
 
     public HttpCookies getCookies() {
         return HttpCookies.from(getHeader(COOKIE));
-    }
-
-    public Collection<String> getHeaderNames() {
-        return values.keySet();
     }
 }
--- src/main/java/http/response/HttpResponseHeaders.java
+++ src/main/java/http/response/HttpResponseHeaders.java
@@ -1,17 +1,14 @@
 package http.response;
 
-import com.google.common.collect.Maps;
+import http.Headers;
 import http.HttpCookies;
 
 import java.util.Collection;
-import java.util.Map;
 
-public class HttpResponseHeaders {
-    private final Map<String, String> headers;
+public class HttpResponseHeaders extends Headers {
     private final HttpCookies cookies;
 
     public HttpResponseHeaders() {
-        this.headers = Maps.newHashMap();
         this.cookies = HttpCookies.create();
     }
 
@@ -19,20 +16,8 @@ public class HttpResponseHeaders {
         cookies.addCookie(key, value);
     }
 
-    public void addHeader(final String key, final String value) {
-        headers.put(key, value);
-    }
-
-    public String getHeader(final String key) {
-        return headers.getOrDefault(key, null);
-    }
-
     public String getCookie(final String key) {
         return cookies.getValue(key);
-    }
-
-    public Collection<String> getHeaderNames() {
-        return headers.keySet();
     }
 
     public Collection<String> getCookieNames() {
