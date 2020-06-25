28924326b4e601706b988545b7198e54e8b2f433
SujungRim <remember2889@gmail.com>
2020-06-25 23:43:14 +0900

    test(HttpRequestTest): 동시에 서버의 스레드풀 수보다 많은 요청을 발생시키는 테스트 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -13,18 +13,24 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
-    private static final int THREAD_POOL_SIZE = 250;
-    private static final int QUEUE_SIZE = 100;
+    private static final int DEFAULT_THREAD_POOL_SIZE = 250;
+    private static final int DEFAULT_QUEUE_SIZE = 100;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
+        int threadPoolSize = 0;
+        int queueSize = 0;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
+            threadPoolSize = DEFAULT_THREAD_POOL_SIZE;
+            queueSize = DEFAULT_QUEUE_SIZE;
         } else {
             port = Integer.parseInt(args[0]);
+            threadPoolSize = Integer.parseInt(args[1]);
+            queueSize = Integer.parseInt(args[2]);
         }
 
-        ExecutorService threadPool = ThreadPoolFactory.newFixedThreadPoolWithQueueSize(THREAD_POOL_SIZE, QUEUE_SIZE);
+        ExecutorService threadPool = ThreadPoolFactory.newFixedThreadPoolWithQueueSize(threadPoolSize, queueSize);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,18 +1,61 @@
 package webserver;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestTest.class);
+
+    void setUpServer(String port, String poolSize, String queueSize) {
+        Thread thread = new Thread(() -> {
+            try {
+                WebServer.main(new String[]{port, poolSize, queueSize});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+
+    @DisplayName("한정된 스레드풀에서 요청을 처리할 수 없으면 RejectedExecutionException 발생")
     @Test
-    void request_resttemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        String resourceUrl = "https://edu.nextstep.camp";
-        ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/c/4YUvqn9V", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    void test_requests_exceeding_threadpoolSize() throws Exception {
+        // given
+        String port = "8080";
+        String poolSize = "5";
+        String queueSize = "10";
+        setUpServer(port, poolSize, queueSize);
+
+        int nThreads = 20;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+        // when
+        IntStream.range(0, nThreads).forEach(n -> {
+            executorService.execute(() -> {
+                RestTemplate restTemplate = new RestTemplate();
+                String resourceUrl = "http://localhost:" + port;
+                ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/index.html", String.class);
+
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            });
+        });
+        // then
+        executorService.shutdown();
+        executorService.awaitTermination(200, TimeUnit.SECONDS);
     }
 }
