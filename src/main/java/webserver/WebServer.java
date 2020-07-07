2c321e4dbc438b7bc3abd47041a727841d5deaba
jwee0330 <jwee0330@gmail.com>
2020-07-08 08:44:16 +0900

    feat: thread pool 생성한다

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
@@ -26,11 +27,16 @@ public class WebServer {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
+            // 쓰레드 풀을 생성한다.
+            BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(100);
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                    250, 250, 3000, TimeUnit.MILLISECONDS, workQueue);
+
+
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executor.execute(new RequestHandler(connection));
             }
         }
     }
