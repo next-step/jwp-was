35bbb8f2248a2a25f5f704c61d580cab6c52a4a0
devson <34268173+ivvve@users.noreply.github.com>
2022-06-10 00:09:31 +0900

    1단계 - TDD 실습 (#206)

---
+++ src/main/java/webserver/RequestLine.java
@@ -0,0 +1,59 @@
+package webserver;
+
+public class RequestLine {
+    /**
+     * HTTP 요청의 Request-Line을 파싱한다.
+     *
+     * @see <a href="https://datatracker.ietf.org/doc/html/rfc2616#section-5.1">RFC 2616 - 5.1 Request-Line</a>
+     */
+    public static RequestLine parse(final String httpRequestRequestLine) {
+        final String[] tokens = httpRequestRequestLine.split(" ");
+
+        final HttpMethod method = HttpMethod.valueOf(tokens[0]);
+
+        final String[] pathAndQueryString = tokens[1].split("\\?");
+        final String path = pathAndQueryString[0];
+        final String queryString = (1 < pathAndQueryString.length) ? pathAndQueryString[1] : "";
+
+        final String[] protocolAndVersion = tokens[2].split("/");
+        final String protocol = protocolAndVersion[0];
+        final String version = protocolAndVersion[1];
+
+        return new RequestLine(method, path, queryString, protocol, version);
+    }
+
+    private RequestLine(final HttpMethod method, final String path, final String queryString,
+                        final String protocol, final String version) {
+        this.method = method;
+        this.path = path;
+        this.queryString = queryString;
+        this.protocol = protocol;
+        this.version = version;
+    }
+
+    private final HttpMethod method;
+    private final String path;
+    private final String queryString;
+    private final String protocol;
+    private final String version;
+
+    public HttpMethod getMethod() {
+        return this.method;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public String getQueryString() {
+        return this.queryString;
+    }
+
+    public String getProtocol() {
+        return this.protocol;
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+}
