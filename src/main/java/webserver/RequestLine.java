6a7f03b71461e82285093119b726c8f77833b39e
Pyro <rhwjddhks@gmail.com>
2022-06-09 17:55:33 +0900

    feat: RequestLine 을 파싱해주는 객체 구현

---
+++ src/main/java/webserver/RequestLine.java
@@ -0,0 +1,47 @@
+package webserver;
+
+import exception.IllegalRequestLineException;
+
+public class RequestLine {
+    private final HttpMethod method;
+    private final Uri uri;
+    private final Protocol protocol;
+
+    public RequestLine(HttpMethod method, Uri uri, Protocol protocol) {
+        this.method = method;
+        this.uri = uri;
+        this.protocol = protocol;
+    }
+
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return uri.getPath();
+    }
+
+    public String getQuery(String key) {
+        return uri.getQuery(key);
+    }
+
+    public Protocol getProtocol() {
+        return protocol;
+    }
+
+    public static RequestLine from(String requestLine) {
+        if (requestLine == null) {
+            throw new IllegalRequestLineException("NULL");
+        }
+        String[] requestInfo = requestLine.trim().split("\\s+");
+        int expectedLength = 3;
+        if (requestInfo.length != expectedLength) {
+            throw new IllegalRequestLineException(requestLine);
+        }
+        return new RequestLine(
+                HttpMethod.from(requestInfo[0]),
+                Uri.from(requestInfo[1]),
+                Protocol.from(requestInfo[2])
+        );
+    }
+}
---
+++ src/test/java/webserver/RequestLineTest.java
@@ -0,0 +1,42 @@
+package webserver;
+
+import exception.IllegalRequestLineException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class RequestLineTest {
+
+    @DisplayName("RequestLine 을 파싱해서, method, path, query string, protocol 정보를 가져올 수 있다.")
+    @Test
+    void from() {
+        RequestLine requestLine = RequestLine.from("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1");
+        assertAll(
+                () -> assertThat(requestLine.getMethod()).isEqualTo(HttpMethod.GET),
+                () -> assertThat(requestLine.getPath()).isEqualTo("/users"),
+                () -> assertThat(requestLine.getQuery("userId")).isEqualTo("javajigi"),
+                () -> assertThat(requestLine.getQuery("password")).isEqualTo("password"),
+                () -> assertThat(requestLine.getQuery("name")).isEqualTo("JaeSung"),
+                () -> assertThat(requestLine.getProtocol()).isEqualTo(Protocol.HTTP_1_1)
+        );
+    }
+
+    @DisplayName("request line 이 null 일 경우, IllegalRequestLineException 이 발생한다.")
+    @Test
+    void emptyRequestLine() {
+        assertThatThrownBy(
+                () -> RequestLine.from(null)
+        ).isInstanceOf(IllegalRequestLineException.class);
+    }
+
+    @DisplayName("부적절한 request line 일 경우, IllegalRequestLineException 이 발생한다.")
+    @Test
+    void illegalRequestLine() {
+        assertThatThrownBy(
+                () -> RequestLine.from("GET /users")
+        ).isInstanceOf(IllegalRequestLineException.class);
+    }
+}
