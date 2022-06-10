b6f2f7f088aa6f0fbd96bee44ea74cb648fc5c48
sjgwon <sjgwon@woowahan.com>
2022-06-10 09:10:55 +0900

    feat: HttpMethod enum 추가

---
+++ src/test/java/webserver/HttpMethodTest.java
@@ -0,0 +1,15 @@
+package webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class HttpMethodTest {
+
+    @DisplayName("유효하지 않는 HTTP 메소드일 경우 예외를 반환한다")
+    @Test
+    public void invalidValueOf() {
+        assertThatThrownBy(() -> HttpMethod.valueOf("PUT")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
