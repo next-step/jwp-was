fd9e0f404206abf9880c72b3e6b47d9d5c5c87d0
sjgwon <sjgwon@woowahan.com>
2022-06-10 19:51:31 +0900

    feat: QueryString 객체 추가

---
+++ src/test/java/webserver/QueryStringTest.java
@@ -0,0 +1,41 @@
+package webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class QueryStringTest {
+
+    @DisplayName("QueryString 객체를 생성할 수 있다")
+    @Test
+    public void create() {
+        String queryString = "userId=javajigi&password=password&name=JaeSung";
+
+        QueryString actual = QueryString.from(queryString);
+
+        assertThat(actual.get("userId")).isEqualTo("javajigi");
+        assertThat(actual.get("password")).isEqualTo("password");
+        assertThat(actual.get("name")).isEqualTo("JaeSung");
+    }
+
+    @DisplayName("예외 반환 검증")
+    @ParameterizedTest
+    @ValueSource(strings = {"=&=", "&password=password", "=", " "})
+    public void invalid(String input) {
+        assertThatThrownBy(() -> QueryString.from(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("key값 조회 시 결과가 없으면 예외를 반환한다")
+    @Test
+    public void invalidGet() {
+        String input = "userId=javajigi&password=password&name=JaeSung";
+
+        QueryString actual= QueryString.from(input);
+
+        assertThatThrownBy(() -> actual.get("userName")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
