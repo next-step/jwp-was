a78022583ac564d5d8329b12500e4f283728b3bf
dongchul <sorkehdcjf@gmail.com>
2022-06-12 13:19:32 +0900

    feat: QueryString 파싱

--- src/main/java/utils/RequestLine.java
+++ src/main/java/utils/RequestLine.java
@@ -6,13 +6,16 @@ public class RequestLine {
 
     private String path;
 
+    private String queryString;
+
     private String protocol;
 
     private String version;
 
-    public RequestLine(String method, String path, String protocol, String version) {
+    public RequestLine(String method, String path, String queryString, String protocol, String version) {
         this.method = method;
         this.path = path;
+        this.queryString = queryString;
         this.protocol = protocol;
         this.version = version;
     }
@@ -21,11 +24,12 @@ public class RequestLine {
         String[] tokens = readLine.split(" ");
 
         String method = tokens[0];
-        String path = tokens[1];
+        String path = tokens[1].split("\\?")[0] == null ? tokens[1] : tokens[1].split("\\?")[0];
+        String queryString = tokens[1].split("\\?").length > 1 ? tokens[1].split("\\?")[1] : null;
         String protocol = tokens[2].split("/")[0];
         String version = tokens[2].split("/")[1];
 
-        return new RequestLine(method, path, protocol, version);
+        return new RequestLine(method, path, queryString, protocol, version);
     }
 
     public String getMethod() {
@@ -42,5 +46,9 @@ public class RequestLine {
 
     public String getVersion() {
         return version;
+    }
+
+    public String getQueryString() {
+        return queryString;
     }
 }
--- src/test/java/utils/RequestLineTest.java
+++ src/test/java/utils/RequestLineTest.java
@@ -43,4 +43,24 @@ class RequestLineTest {
                 () -> assertThat(version).isEqualTo("1.1")
         );
     }
+
+    @DisplayName("QueryString 파싱")
+    @Test
+    void parseQueryStringRequestLine() {
+        RequestLine requestLine = RequestLine.parse("GET /users?userId=dean&password=password&name=Dongchul HTTP/1.1");
+
+        String method = requestLine.getMethod();
+        String path = requestLine.getPath();
+        String queryString = requestLine.getQueryString();
+        String protocol = requestLine.getProtocol();
+        String version = requestLine.getVersion();
+
+        assertAll(
+                () -> assertThat(method).isEqualTo("GET"),
+                () -> assertThat(path).isEqualTo("/users"),
+                () -> assertThat(queryString).isEqualTo("userId=dean&password=password&name=Dongchul"),
+                () -> assertThat(protocol).isEqualTo("HTTP"),
+                () -> assertThat(version).isEqualTo("1.1")
+        );
+    }
 }
