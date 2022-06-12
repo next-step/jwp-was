929534ed7590575531dabf57e17d97fbf8fe368c
magyeon <magyeon@woowahan.com>
2022-06-12 22:40:09 +0900

    feat: HttpRequestParser parse 메서드 구현

--- src/main/java/utils/http/HttpRequestParser.java
+++ src/main/java/utils/http/HttpRequestParser.java
@@ -1,7 +1,28 @@
 package utils.http;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class HttpRequestParser {
+    private static final Pattern REQUEST_LINE_PATTERN = Pattern.compile("(.+) (.+) (.+)/(.+)");
+
+    private static final int GROUP_METHOD = 1;
+    private static final int GROUP_PATH = 2;
+    private static final int GROUP_PROTOCOL = 3;
+    private static final int GROUP_VERSION = 4;
+
     public static HttpRequest parse(String requestLine) {
-        return null;
+        Matcher matcher = REQUEST_LINE_PATTERN.matcher(requestLine);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Http Request 형식에 맞지 않습니다 : " + requestLine);
+        }
+
+        String method = matcher.group(GROUP_METHOD);
+        String path = matcher.group(GROUP_PATH);
+        String protocol = matcher.group(GROUP_PROTOCOL);
+        String version = matcher.group(GROUP_VERSION);
+
+        return new HttpRequest(method, path, protocol, version);
     }
 }
