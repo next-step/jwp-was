39afcd6e0d96bd839a80a3f0e0a70f59fab296b6
chyee <chyee@coupang.com>
2022-06-25 23:43:35 +0900

    test add test for threadpool

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,17 +1,55 @@
 package webserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class HttpRequestTest {
+
     @Test
-    void request_resttemplate() {
+    void request_success() throws InterruptedException {
+        //given
+        int count = 250;
+        CountDownLatch countDownLatch = new CountDownLatch(count);
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        ExecutorService executorService = Executors.newFixedThreadPool(count);
+
+        //when
+        IntStream.range(0, count).forEach(i ->
+                executorService.execute(() -> {
+                    restTemplate.getForEntity("http://localhost:8080/index.html", String.class);
+                    countDownLatch.countDown();
+                }));
+        countDownLatch.await(5, TimeUnit.SECONDS);
+
+        //then
+        assertThat(countDownLatch.getCount()).isEqualTo(0);
+    }
+
+    @Test
+    void request_fail() throws InterruptedException {
+        //given
+        int count = 500;
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+        RestTemplate restTemplate = new RestTemplate();
+        ExecutorService executorService = Executors.newFixedThreadPool(count);
+
+        //when
+        IntStream.range(0, count).forEach(i ->
+                executorService.execute(() -> {
+                    restTemplate.getForEntity("http://localhost:8080/index.html", String.class);
+                    countDownLatch.countDown();
+                }));
+        countDownLatch.await(10, TimeUnit.SECONDS);
+
+        //then
+        assertThat(countDownLatch.getCount()).isNotEqualTo(0);
     }
 }
