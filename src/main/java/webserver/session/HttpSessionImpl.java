46f73d22876828aea3a9bb4e983e5e2bf6b5369f
Pyro <rhwjddhks@gmail.com>
2022-06-29 18:35:19 +0900

    feat: HttpSession 인터페이스를 implmenets 하는 클래스를 선언

---
+++ src/main/java/webserver/session/HttpSessionImpl.java
@@ -0,0 +1,100 @@
+package webserver.session;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
+
+public class HttpSessionImpl implements HttpSession {
+
+    private final UUID uuid = UUID.randomUUID();
+    private final Map<String, Object> session = new HashMap<>();
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return null;
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+
+    }
+
+    @Override
+    public void invalidate() {
+
+    }
+
+    @Override
+    public long getCreationTime() {
+        return 0;
+    }
+
+    @Override
+    public long getLastAccessedTime() {
+        return 0;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    @Override
+    public void setMaxInactiveInterval(int interval) {
+
+    }
+
+    @Override
+    public int getMaxInactiveInterval() {
+        return 0;
+    }
+
+    @Override
+    public HttpSessionContext getSessionContext() {
+        return null;
+    }
+
+    @Override
+    public Object getValue(String name) {
+        return null;
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return null;
+    }
+
+    @Override
+    public String[] getValueNames() {
+        return new String[0];
+    }
+
+    @Override
+    public void putValue(String name, Object value) {
+
+    }
+
+    @Override
+    public void removeValue(String name) {
+
+    }
+
+    @Override
+    public boolean isNew() {
+        return false;
+    }
+}
