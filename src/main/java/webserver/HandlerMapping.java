26594176d9c507c994778321b900a3868302b79b
dongchul <sorkehdcjf@gmail.com>
2022-07-04 23:00:46 +0900

    feat: file extension 중복 제거

--- src/main/java/webserver/HandlerMapping.java
+++ src/main/java/webserver/HandlerMapping.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import utils.ResourceUtils;
 import webserver.http.controller.Controller;
 import webserver.http.controller.LoginController;
 import webserver.http.controller.NotFoundController;
@@ -8,12 +9,12 @@ import webserver.http.controller.UserCreateController;
 import webserver.http.controller.UserListController;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class HandlerMapping {
 
-    private static final List<String> applyFileExtension = List.of(".html", ".css", ".ico", ".js");
+    private static final Set<String> applyFileExtension = ResourceUtils.resourcePath.keySet();
 
     private static final Controller NOT_FOUND = new NotFoundController();
 
