da7dbd1b2fa25e885090cd43d6a62430a8a5fd5b
devson <sonyc5720@gmail.com>
2022-07-06 00:32:00 +0900

    refactor: 요청 마다 Thread를 생성하지 않고 Pool로부터 Thread를 사용하도록 변경

--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class WebApplicationServer {
     private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
@@ -18,6 +18,8 @@ public class WebApplicationServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ServerExecutorService serverExecutorService = new ServerExecutorService();
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -25,8 +27,7 @@ public class WebApplicationServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                serverExecutorService.execute(new RequestHandler(connection));
             }
         }
     }
