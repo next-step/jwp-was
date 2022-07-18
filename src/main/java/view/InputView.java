0aabe6f6ff19962e304c8f9d75d7b9d8daddb57d
황정현 <jhhwang@wemakeprice.com>
2022-07-18 17:17:12 +0900

    [feature] 힌트 구현 및 HTTPMETOD ENUM

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,2 +1,4 @@
-package view;public class InputView {
+package view;
+
+public class InputView {
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,19 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.IOUtils;
+import view.ResultView;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+
+    private ResultView resultView = new ResultView();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -24,6 +25,7 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            resultView.printRequestLine(IOUtils.getFirstLine(in));
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
