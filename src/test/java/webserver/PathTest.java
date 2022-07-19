be0dd66a750c39ad1fa4663f23508acf09a404fe
YONGJU <odetsuo@gmail.com>
2022-07-20 08:56:07 +0900

    feat: Path 분리

---
+++ src/test/java/webserver/PathTest.java
@@ -0,0 +1,47 @@
+package webserver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PathTest {
+
+
+    @DisplayName("빈 문자열은 파싱할 수 없다")
+    @ParameterizedTest(name = "#{index}: [{arguments}]")
+    @NullAndEmptySource
+    @ValueSource(strings = " ")
+    void empty_strings_cannot_be_parsed(String emptyPath) {
+        assertThatThrownBy(() -> Path.from(emptyPath))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("QueryString이 없는 경우 파싱")
+    @Test
+    void has_no_query_string() {
+        final Path path = Path.from("/users");
+
+        assertAll(
+            () -> assertThat(path.getLocation()).isEqualTo("/users"),
+            () -> assertThat(path.getQueryString()).isEmpty()
+        );
+    }
+
+    @DisplayName("QueryString이 있는 경우 파싱")
+    @Test
+    void has_query_string() {
+        final Path path = Path.from("/users?userId=1234");
+
+        assertAll(
+            () -> assertThat(path.getLocation()).isEqualTo("/users"),
+            () -> assertThat(path.getQueryString()).isEqualTo("userId=1234")
+        );
+    }
+
+}
