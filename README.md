af30fe106b631b22f8d5c3d8bea6919cb4976552
jason <ck96117@gmail.com>
2022-07-20 02:20:45 +0900

    ✨ feat: RequestLine을 파싱하는 parsing() 메소드 구현

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 - [x] Request Line은 RFC7230 (https://www.rfc-editor.org/rfc/rfc7230) -> 3.1.1. Request Line 의 정의에 따라 String 타입의 `method SP request-target SP HTTP-version CRLF` 의 구조를 가진다.
 - [x] method는 Enum으로 구현하여야 하며 형태는 RFC7231 (https://www.rfc-editor.org/rfc/rfc7231#section-4) 의 정의에 따라  
   `GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE`의 형태를 가진다.
-- [ ] HttpParser는 RequestLine을 method, path(request-target), protocaol, version으로 분리 할 수 있다.
+- [x] HttpParser는 RequestLine을 method, path(request-target), protocaol, version으로 분리 할 수 있다.
 - [ ] Request Line 객체를 만든다.
 - Request Line 객체는 method, path(request-target), protocaol, version을 필드로 가진다.
 - [ ] Query String이 포함된 형태의 Request Line이 들어오면 쿼리 스트링을 파싱 할 수 있다. (Map 형태로 파싱할예정)
--- src/main/java/utils/RequestLineParser.java
+++ src/main/java/utils/RequestLineParser.java
@@ -1,12 +1,49 @@
 package utils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class RequestLineParser {
-    private static final Pattern REQUEST_LINE = Pattern.compile(
-            "[A-Z]* {1}\\S* {1}[A-Z]*\\/[0-9|.]+");
+    private static final Pattern REQUEST_LINE = Pattern.compile("[A-Z]* {1}\\S* {1}[A-Z]*\\/[0-9|.]+");
+    public static final String REQUEST_LINE_SEPARATOR = " ";
+    public static final Integer METHOD_INDEX = 0;
+    public static final Integer PATH_INDEX = 1;
+    public static final Integer PROTOCOL_WITH_VERSION_INDEX = 2;
+    public static final Integer PROTOCOL_INDEX = 0;
+    public static final Integer PROTOCOL_VERSION_INDEX = 1;
+    public static final String VERSION_SEPARATOR = "/";
 
     static boolean isRequestLinePattern(String requestLine) {
         return REQUEST_LINE.matcher(requestLine).matches();
+    }
+
+    public static Map<String, String> parsing(String requestLine) {
+        validateCanParsing(requestLine);
+
+        String[] split = requestLine.split(REQUEST_LINE_SEPARATOR);
+
+        HashMap<String, String> parsedRequest = new HashMap<>();
+        parsedRequest.put("method", split[METHOD_INDEX]);
+        parsedRequest.put("path", split[PATH_INDEX]);
+        parsedRequest.putAll(parsingProtocol(split[PROTOCOL_WITH_VERSION_INDEX]));
+
+        return parsedRequest;
+    }
+
+    private static void validateCanParsing(String requestLine) throws IllegalArgumentException {
+        if (!isRequestLinePattern(requestLine)) {
+            throw new IllegalArgumentException("Request Line이 올바른 형식을 가지고 있지 않습니다.");
+        }
+    }
+
+    private static Map<String, String> parsingProtocol(String protocol) {
+        String[] split = protocol.split(VERSION_SEPARATOR);
+
+        HashMap<String, String> parsedProtocol = new HashMap<>();
+        parsedProtocol.put("protocol", split[PROTOCOL_INDEX]);
+        parsedProtocol.put("protocolVersion", split[PROTOCOL_VERSION_INDEX]);
+
+        return parsedProtocol;
     }
 }
