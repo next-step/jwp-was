72a45ece437dcb474089c5697c50aecb47274efb
rheew <47098896+rheew@users.noreply.github.com>
2022-07-19 22:55:24 +0900

    feat: requestHandler 에서 파싱 기능 추가 - IOUtils 에 readLine 데이터 가저오는 기능 추가

--- src/main/java/utils/IOUtils.java
+++ src/main/java/utils/IOUtils.java
@@ -1,7 +1,14 @@
 package utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IOUtils {
     /**
@@ -12,9 +19,30 @@ public class IOUtils {
      * @return
      * @throws IOException
      */
+    private static final Logger logger = LoggerFactory.getLogger(IOUtils.class);
+
     public static String readData(BufferedReader br, int contentLength) throws IOException {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
+    }
+
+    public static String readRequestData(InputStream is) {
+        List<String> data = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line = br.readLine();
+
+            while (line != null && !line.isEmpty()) {
+                data.add(line);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+
+        return data.get(0);
     }
 }
