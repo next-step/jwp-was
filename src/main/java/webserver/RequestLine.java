fd53ea3873538875e70d25521a121237d2878023
rheew <47098896+rheew@users.noreply.github.com>
2022-07-19 22:22:27 +0900

    refactor: Protocol 과 Version을 같은 클래스로 관리

--- src/main/java/webserver/RequestLine.java
+++ src/main/java/webserver/RequestLine.java
@@ -9,21 +9,16 @@ public class RequestLine {
     public static final int METHOD_INDEX = 0;
     public static final int PATH_INDEX = 1;
     public static final int PROTOCOL_AND_VERSION_INDEX = 2;
-    public static final int PROTOCOL_INDEX = 0;
-    public static final int VERSION_INDEX = 1;
     public static final String REGEX_BLANK = " ";
-    public static final String REGEX_SLASH = "/";
 
     private HttpMethodType method;
     private RequestPath request;
-    private String protocol;
-    private String version;
+    private Protocol protocol;
 
     public RequestLine(String requestLine) {
         this.method = parsingMethod(requestLine);
-        this.request = parsingPath(requestLine);
+        this.request = parsingRequestPath(requestLine);
         this.protocol = parsingProtocol(requestLine);
-        this.version = parsingVersion(requestLine);
     }
 
     private HttpMethodType parsingMethod(String requestLine) {
@@ -31,18 +26,12 @@ public class RequestLine {
                 requestLine.split(REGEX_BLANK)[METHOD_INDEX]);
     }
 
-    private RequestPath parsingPath(String requestLine) {
+    private RequestPath parsingRequestPath(String requestLine) {
         return new RequestPath(requestLine.split(REGEX_BLANK)[PATH_INDEX]);
     }
 
-    private String parsingProtocol(String requestLine) {
-        return requestLine.split(REGEX_BLANK)[PROTOCOL_AND_VERSION_INDEX]
-                .split(REGEX_SLASH)[PROTOCOL_INDEX];
-    }
-
-    private String parsingVersion(String requestLine) {
-        return requestLine.split(REGEX_BLANK)[PROTOCOL_AND_VERSION_INDEX]
-                .split(REGEX_SLASH)[VERSION_INDEX];
+    private Protocol parsingProtocol(String requestLine) {
+        return new Protocol(requestLine.split(REGEX_BLANK)[PROTOCOL_AND_VERSION_INDEX]);
     }
 
     public HttpMethodType getMethod() {
@@ -58,10 +47,10 @@ public class RequestLine {
     }
 
     public String getProtocol() {
-        return protocol;
+        return protocol.getProtocol();
     }
 
     public String getVersion() {
-        return version;
+        return protocol.getVersion();
     }
 }
