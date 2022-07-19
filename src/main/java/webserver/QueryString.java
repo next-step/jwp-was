400ff8214844ffae9c7174cef9bdfdaa3c143e16
ohminho <ohminho@woowahan.com>
2022-07-19 23:39:11 +0900

    refactor - Path 의 쿼리스트링을 저장하는 QueryString 객체 생성 - 매직넘버 상수로 수정

---
+++ src/main/java/webserver/QueryString.java
@@ -0,0 +1,39 @@
+package webserver;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class QueryString {
+
+    private static final String ENTRY_DELIMITER = "&";
+
+    private static final String KEY_VALUE_DELIMITER = "=";
+
+    private static final int KEY_IDX = 0;
+
+    private static final int VALUE_IDX = 1;
+
+    private final Map<String, String> valuesByKey;
+
+    private QueryString(Map<String, String> valuesByKey) {
+        this.valuesByKey = Collections.unmodifiableMap(valuesByKey);
+    }
+
+    static QueryString parseOf(String queryString) {
+        if (queryString == null || queryString.isEmpty()) {
+            return new QueryString(Collections.emptyMap());
+        }
+
+        return new QueryString(
+                Stream.of(queryString.split(ENTRY_DELIMITER))
+                        .map(entries -> entries.split(KEY_VALUE_DELIMITER))
+                        .collect(Collectors.toMap(entry -> entry[KEY_IDX], entry -> entry[VALUE_IDX]))
+        );
+    }
+
+    String getValue(String key) {
+        return valuesByKey.get(key);
+    }
+}
--- src/test/java/webserver/RequestPathTest.java
+++ src/test/java/webserver/RequestPathTest.java
@@ -18,24 +18,19 @@ class RequestPathTest {
                 .hasMessageContaining("유효한 RequestPath 가 아님");
     }
 
-    @DisplayName("RequestPath 를 파싱해서 쿼리스트링을 알 수 있다.")
-    @CsvSource(
-            value = {
-                    "/users:''",
-                    "/users?userId=javajigi:userId=javajigi",
-                    "/users?userId=javajigi&password=password:userId=javajigi&password=password",
-            },
-            delimiter = ':')
-    @ParameterizedTest
-    void parseQueryStringTest(String path, String queryString) {
+    @DisplayName("RequestPath 를 파싱해서 쿼리스트링의 key 에 대한 value 를 알 수 있다.")
+    @Test
+    void parseQueryStringTest() {
         // given
-        RequestPath requestPath = new RequestPath(path);
+        RequestPath requestPath = new RequestPath("/users?userId=javajigi&password=password");
 
         // when
-        String actualQueryString = requestPath.getQueryString();
+        QueryString queryString = requestPath.parseQueryString();
 
         // then
-        assertThat(actualQueryString).isEqualTo(queryString);
+        assertThat(queryString.getValue("userId")).isEqualTo("javajigi");
+        assertThat(queryString.getValue("password")).isEqualTo("password");
+        assertThat(queryString.getValue("password2")).isNull();
     }
 
 }
