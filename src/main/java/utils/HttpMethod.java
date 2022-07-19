f79189ed548a019be4edcec17eb955fedd46ddbc
Hwangs <43225288+HWANGJEONGHYEON1@users.noreply.github.com>
2022-07-19 10:23:24 +0900

    [1단계 - TDD 실습] 리뷰 요청드립니다. (#238)

---
+++ src/main/java/utils/HttpMethod.java
@@ -0,0 +1,23 @@
+package utils;
+
+import exception.NotExistHttpMethodException;
+
+import java.util.Arrays;
+
+public enum HttpMethod {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    PATCH("PATCH");
+
+    String name;
+
+    HttpMethod(String name) {
+        this.name = name;
+    }
+
+    public static boolean checkHttpMethod(String requestMethod) {
+        return Arrays.stream(HttpMethod.values())
+                .anyMatch(method -> requestMethod.equals(method.name));
+    }
+}
