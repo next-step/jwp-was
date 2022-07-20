091b6f0ea6e5f6c944bf70a6862dc29fa62bd4be
younlll <youn.lee96@gmail.com>
2022-07-20 22:05:58 +0900

    chore: 컨벤션 적용

--- src/test/java/webserver/RequestLineTest.java
+++ src/test/java/webserver/RequestLineTest.java
@@ -1,34 +1,33 @@
 package webserver;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RequestLineTest {
+	private RequestLine requestLine = new RequestLine();
 
-    private RequestLine requestLine = new RequestLine();
-    
-    @DisplayName("GET 요청에 대한 RequestLine을 통해 정상적 파싱되었다.")
-    @Test
-    void parsingGetRequest() {
-        // given
-        String method = "GET";
-        String path = "/users";
-        String protocol = "HTTP";
-        String version = "1.1";
-        String request = "GET /users HTTP/1.1";
+	@DisplayName("GET 요청에 대한 RequestLine을 통해 정상적 파싱되었다.")
+	@Test
+	void parsingGetRequest() {
+		// given
+		String method = "GET";
+		String path = "/users";
+		String protocol = "HTTP";
+		String version = "1.1";
+		String request = "GET /users HTTP/1.1";
 
-        // when
-        RequestLine result = requestLine.parse(request);
-        
-        // then
-        assertAll(
-                () -> assertThat(result.getMethod()).isEqualTo(method),
-                () -> assertThat(result.getPath()).isEqualTo(path),
-                () -> assertThat(result.getProtocol()).isEqualTo(protocol),
-                () -> assertThat(result.getVersion()).isEqualTo(version)
-        );
-    }
+		// when
+		RequestLine result = requestLine.parse(request);
+
+		// then
+		assertAll(
+			() -> assertThat(result.getMethod()).isEqualTo(method),
+			() -> assertThat(result.getPath()).isEqualTo(path),
+			() -> assertThat(result.getProtocol()).isEqualTo(protocol),
+			() -> assertThat(result.getVersion()).isEqualTo(version)
+		);
+	}
 }
