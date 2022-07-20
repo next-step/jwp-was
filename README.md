a28d45c06ba8162b59da3ff7f38abf215130c0aa
joelyou-nsuslab <joel.you@nsuslab.com>
2022-07-20 17:49:20 +0900

    feature: HTTP POST 요청에 대한 RequestLine 파싱.
    * 2단계요구사항 작성
    * HttpMethod POST 추가
    * Testcode 작성 및 Testcode 메소드명 변경

--- README.md
+++ README.md
@@ -22,3 +22,13 @@
     * path는 /users
     * protocol은 HTTP
     * version은 1.1
+
+### 요구사항 2 - POST 요청
+
+* HTTP POST 요청에 대한 RequestLine을 파싱한다.
+* 파싱하는 로직 구현을 TDD로 구현한다.
+* 예를 들어 "POST /users HTTP/1.1"을 파싱하면 다음과 같은 결과를 얻을 수 있어야 한다.
+    * method는 POST
+    * path는 /users
+    * protocol은 HTTP
+    * version은 1.1
--- src/test/java/webserver/RequestLineTest.java
+++ src/test/java/webserver/RequestLineTest.java
@@ -7,11 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RequestLineTest {
 
     @Test
-    void GET_요청_테스트() {
+    void requestLineFromGetRequest() {
         String request = "GET /users HTTP/1.1";
         RequestLine requestLine = new RequestLine(request);
 
         assertThat(requestLine.getHttpMethod()).isEqualTo(HttpMethod.GET);
+        assertThat(requestLine.getRequestPath()).isEqualTo("/users");
+        assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
+        assertThat(requestLine.getVersion()).isEqualTo("1.1");
+    }
+
+    @Test
+    void requestLineFromPostRequest() {
+        String request = "POST /users HTTP/1.1";
+        RequestLine requestLine = new RequestLine(request);
+
+        assertThat(requestLine.getHttpMethod()).isEqualTo(HttpMethod.POST);
         assertThat(requestLine.getRequestPath()).isEqualTo("/users");
         assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
         assertThat(requestLine.getVersion()).isEqualTo("1.1");
