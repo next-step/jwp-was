0c6b35f6a3f8142474b3cd7f38957ccdc177c905
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-07-20 10:53:03 +0900

    feat: Query String 파싱 기능 구현

---
+++ src/main/java/http/request/Path.java
@@ -0,0 +1,40 @@
+package http.request;
+
+public class Path {
+
+    private static final String QUERY_PARAMETER_DELIMITER = "\\?";
+    private static final int PATH_ONLY_INDEX = 0;
+    private static final int QUERYSTRING_INDEX = 1;
+    private static final int QUERY_STRING_EMPTY = 1;
+
+    private String path;
+    private QueryParameters queryParameters;
+
+    private Path(String path) {
+        this.path = path;
+    }
+    private Path(String path, QueryParameters queryParameters) {
+        this.path = path;
+        this.queryParameters = queryParameters;
+    }
+
+    public static Path from(String pathWithParam) {
+        String[] pathWithParams = pathWithParam.split(QUERY_PARAMETER_DELIMITER);
+        if (queryStringExist(pathWithParams)) {
+            return new Path(pathWithParams[PATH_ONLY_INDEX], QueryParameters.from(pathWithParams[QUERYSTRING_INDEX]));
+        }
+        return new Path(pathWithParams[PATH_ONLY_INDEX]);
+    }
+
+    private static boolean queryStringExist(String[] pathWithParams) {
+        return pathWithParams.length != QUERY_STRING_EMPTY;
+    }
+
+    public String getPathWithOutParam() {
+        return path;
+    }
+
+    public QueryParameters getQueryParameters() {
+        return queryParameters;
+    }
+}
---
+++ src/test/java/http/request/QueryParametersTest.java
@@ -0,0 +1,30 @@
+package http.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueryParametersTest {
+
+    @DisplayName("QueryParameters 객체 생성 성공")
+    @Test
+    void createQueryParameters() {
+        String queryString = "userId=javajigi&password=password&name=JaeSung";
+        QueryParameters queryParameters = QueryParameters.from(queryString);
+        assertAll(
+                () -> assertThat(queryParameters.get("userId")).isEqualTo("javajigi"),
+                () -> assertThat(queryParameters.get("password")).isEqualTo("password"),
+                () -> assertThat(queryParameters.get("name")).isEqualTo("JaeSung")
+        );
+    }
+
+    @DisplayName("Key값이 중복될 경우 마지막에 들어온 value로 대체")
+    @Test
+    void duplicatedKey() {
+        String queryString = "userId=javajigi&userId=headf1rst";
+        QueryParameters queryParameters = QueryParameters.from(queryString);
+        assertThat(queryParameters.get("userId")).isEqualTo("headf1rst");
+    }
+}
