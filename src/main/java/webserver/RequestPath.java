66903775732193ad6b5379d0e530f7e13fa2d3f7
rheew <47098896+rheew@users.noreply.github.com>
2022-07-20 12:54:54 +0900

    style: 오타 수정

--- src/main/java/webserver/RequestPath.java
+++ src/main/java/webserver/RequestPath.java
@@ -6,7 +6,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class RequestPath {
 
     public static final String REGEX_QUESTION_MARK = "\\?";
-    public static final int PATH_INDEXT = 0;
+    public static final int PATH_INDEX = 0;
 
     private String path;
     private MultiValueMap<String, String> params;
@@ -14,7 +14,7 @@ public class RequestPath {
     public RequestPath(String request) {
         final String[] requests = request.split(REGEX_QUESTION_MARK);
 
-        this.path = requests[PATH_INDEXT];
+        this.path = requests[PATH_INDEX];
 
         if (requests.length < 1) {
             return;
