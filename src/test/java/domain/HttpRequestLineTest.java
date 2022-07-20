7dfeb764e3e1efcfd2bf04c9996bbc0f5bec1657
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-20 18:44:24 +0900

    test: Http Request Line 테스트 코드 추가

---
+++ src/test/java/domain/HttpRequestLineTest.java
@@ -0,0 +1,51 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HttpRequestLineTest {
+
+	@Test
+	@DisplayName("지원하지 않는 메서드 파싱")
+	public void parseNotSupportedMethod() {
+		// given
+		String line = "PUT /users HTTP/1.1";
+		// when & then
+		Assertions.assertThrows(IllegalArgumentException.class, () -> HttpRequestLine.of(line));
+	}
+
+	@Test
+	@DisplayName("GET 메서드 파싱")
+	public void parseGetMethod() {
+		// given
+		String line = "GET /users HTTP/1.1";
+
+		// when
+		HttpRequestLine requestLine = HttpRequestLine.of(line);
+
+		// then
+		assertThat(requestLine.getMethod()).isEqualTo("GET");
+		assertThat(requestLine.getPath()).isEqualTo("/users");
+		assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
+		assertThat(requestLine.getVersion()).isEqualTo("1.1");
+	}
+
+	@Test
+	@DisplayName("POST 메서드 파싱")
+	public void parsePostMethod() {
+		// given
+		String line = "POST /users HTTP/1.1";
+
+		// when
+		HttpRequestLine requestLine = HttpRequestLine.of(line);
+
+		// then
+		assertThat(requestLine.getMethod()).isEqualTo("POST");
+		assertThat(requestLine.getPath()).isEqualTo("/users");
+		assertThat(requestLine.getProtocol()).isEqualTo("HTTP");
+		assertThat(requestLine.getVersion()).isEqualTo("1.1");
+	}
+}
