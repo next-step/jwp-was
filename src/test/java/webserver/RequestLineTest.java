2bac3927bc71b1f289bd2755d9da65bcb93c79e2
younlll <youn.lee96@gmail.com>
2022-07-20 22:55:25 +0900

    test: POST 요청에 대한 정상 테스트

--- src/test/java/webserver/RequestLineTest.java
+++ src/test/java/webserver/RequestLineTest.java
@@ -68,4 +68,26 @@ public class RequestLineTest {
 		assertThatThrownBy(() -> requestLine.parse(request))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
+
+	@DisplayName("POST 요청에 대한 RequestLine을 통해 정상적 파싱되었다.")
+	@Test
+	void parsingPostRequest() {
+		// given
+		String method = "POST";
+		String path = "/users";
+		String protocol = "HTTP";
+		String version = "1.1";
+		String request = "POST /users HTTP/1.1";
+
+		// when
+		RequestLine result = requestLine.parse(request);
+
+		// then
+		assertAll(
+			() -> assertThat(result.getMethod()).isEqualTo(method),
+			() -> assertThat(result.getPath()).isEqualTo(path),
+			() -> assertThat(result.getProtocol()).isEqualTo(protocol),
+			() -> assertThat(result.getVersion()).isEqualTo(version)
+		);
+	}
 }
