f1e086c4a126da918c09ed78055f9a659465281c
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-20 19:23:12 +0900

    test: Query Parameter 테스트 코드 추가

---
+++ src/test/java/domain/QueryParameterTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class QueryParameterTest {
+	@Test
+	public void parseQueryString() {
+		// given
+		String query = "userId=javajigi&password=password&name=JaeSung";
+
+		// when
+		QueryParameter queryParameter = QueryParameter.of(query);
+
+		// then
+		assertThat(queryParameter.getValue("userId")).isEqualTo("javajigi");
+		assertThat(queryParameter.getValue("password")).isEqualTo("password");
+		assertThat(queryParameter.getValue("name")).isEqualTo("JaeSung");
+	}
+}
