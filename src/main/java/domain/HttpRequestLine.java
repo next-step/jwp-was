69efde75f3dac4dcfd25739c08ba70b344837787
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-20 19:20:01 +0900

    feat: Query String Parsing 기능 구현

--- src/main/java/domain/HttpRequestLine.java
+++ src/main/java/domain/HttpRequestLine.java
@@ -1,5 +1,9 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class HttpRequestLine {
 	private static final String MESSAGE_INVALID_REQUEST_LINE = "유효하지 않은 Request Line 입니다.";
 	private static final String REQUEST_LINE_DELIMITER = " ";
@@ -11,11 +15,15 @@ public class HttpRequestLine {
 	private HttpMethod httpMethod;
 	private String httpPath;
 	private HttpProtocol httpProtocol;
+	private Map<String, String> parameters;
 
 	public HttpRequestLine(String method, String path, String protocol) {
 		this.httpMethod = HttpMethod.valueOf(method);
-		this.httpPath = path;
+		this.httpPath = path.split("\\?")[0];
 		this.httpProtocol = HttpProtocol.of(protocol);
+		this.parameters = Arrays.stream(path.split("\\?")[1].split("&"))
+								.map(s -> (s.split("=")))
+								.collect(Collectors.toMap(s -> s[0], s -> s[1]));
 	}
 
 	public static HttpRequestLine of(String line) {
@@ -40,5 +48,9 @@ public class HttpRequestLine {
 
 	public String getVersion() {
 		return httpProtocol.getVersion();
+	}
+
+	public Map<String, String> getParameters() {
+		return parameters;
 	}
 }
