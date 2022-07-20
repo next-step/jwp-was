8315b5012e57ca24253f348e94d68f862244fd6c
YONGJU <odetsuo@gmail.com>
2022-07-20 09:31:23 +0900

    refactor: Protocol 적용

--- src/main/java/webserver/RequestLine.java
+++ src/main/java/webserver/RequestLine.java
@@ -6,23 +6,17 @@ public class RequestLine {
 
     private static final int INDEX_OF_METHOD = 0;
     private static final int INDEX_OF_PATH = 1;
-    private static final int INDEX_OF_PROTOCOL_AND_VERSION = 2;
-    private static final int INDEX_OF_PROTOCOL = 0;
-    private static final int INDEX_OF_VERSION = 1;
+    private static final int INDEX_OF_PROTOCOL = 2;
     private static final String DELIMITER = " ";
-    private static final String PROTOCOL_DELIMITER = "/";
-
 
     private final HttpMethod method;
     private final Path path;
-    private final String protocol;
-    private final String version;
+    private final Protocol protocol;
 
-    private RequestLine(final HttpMethod method, final Path path, final String protocol, final String version) {
+    private RequestLine(final HttpMethod method, final Path path, final Protocol protocol) {
         this.method = method;
         this.path = path;
         this.protocol = protocol;
-        this.version = version;
     }
 
     public static RequestLine parse(final String requestLine) {
@@ -32,12 +26,10 @@ public class RequestLine {
 
         final String[] tokens = requestLine.split(DELIMITER);
         final HttpMethod method = HttpMethod.of(tokens[INDEX_OF_METHOD]);
-        final String path = tokens[INDEX_OF_PATH];
-        final String[] protocolAndVersion = tokens[INDEX_OF_PROTOCOL_AND_VERSION].split(PROTOCOL_DELIMITER);
-        final String protocol = protocolAndVersion[INDEX_OF_PROTOCOL];
-        final String version = protocolAndVersion[INDEX_OF_VERSION];
+        final Path path = Path.from(tokens[INDEX_OF_PATH]);
+        final Protocol protocol = Protocol.parse(tokens[INDEX_OF_PROTOCOL]);
 
-        return new RequestLine(method, Path.from(path), protocol, version);
+        return new RequestLine(method, path, protocol);
     }
 
     public HttpMethod getMethod() {
@@ -49,11 +41,11 @@ public class RequestLine {
     }
 
     public String getProtocol() {
-        return protocol;
+        return protocol.getType();
     }
 
     public String getVersion() {
-        return version;
+        return protocol.getVersion();
     }
 
     public String getQueryString() {
