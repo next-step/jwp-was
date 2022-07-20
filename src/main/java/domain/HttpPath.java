8a76254ea7a81025a681897c804341f1a41d8b14
Jeongsu Yang <mycat83@gmail.com>
2022-07-21 00:15:49 +0900

    feat(webserver): Query String 처리 로직 추가

--- src/main/java/domain/HttpPath.java
+++ src/main/java/domain/HttpPath.java
@@ -1,12 +1,34 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class HttpPath {
     private final String path;
+    private final List<Map<String, String>> queryStrings = new ArrayList<>();
 
-    public HttpPath(String path) {
-        this.path = path;
+    public HttpPath(String pathSpec) {
+        final String[] splitPathSpec = pathSpec.split("\\?");
+        this.path = splitPathSpec[0];
+        if (splitPathSpec.length == 2) {
+            setQueryStrings(splitPathSpec[1]);
+        }
+    }
+
+    private void setQueryStrings(String queryStringSpec) {
+        final String[] splitQueryStringSpecs = queryStringSpec.split("&");
+        for (String splitQueryStringSpec : splitQueryStringSpecs) {
+            addQueryString(splitQueryStringSpec);
+        }
+    }
+
+    private void addQueryString(String splitQueryStringSpec) {
+        final String[] items = splitQueryStringSpec.split("=");
+        if (items.length == 2) {
+            queryStrings.add(Map.of(items[0], items[1]));
+        }
     }
 
     @Override
@@ -14,11 +36,11 @@ public class HttpPath {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         HttpPath httpPath = (HttpPath) o;
-        return Objects.equals(path, httpPath.path);
+        return Objects.equals(path, httpPath.path) && Objects.equals(queryStrings, httpPath.queryStrings);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(path);
+        return Objects.hash(path, queryStrings);
     }
 }
--- src/test/java/domain/RequestLineParserTest.java
+++ src/test/java/domain/RequestLineParserTest.java
@@ -7,9 +7,9 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("RequestLineParser 단위 테스트")
 public class RequestLineParserTest {
+    @DisplayName("GET RequestLine을 파싱하여 HttpRequest를 얻는다.")
     @Test
-    @DisplayName("RequestLine을 파싱하여 HttpRequest를 얻는다.")
-    void parse() {
+    void parseForGet() {
         // given
         RequestLineParser requestLineParser = new RequestLineParser();
 
@@ -19,5 +19,33 @@ public class RequestLineParserTest {
         // then
         assertThat(httpRequest).isEqualTo(
                 new HttpRequest(new HttpMethod("GET"), new HttpPath("/users"), new HttpProtocol("HTTP/1.1")));
+    }
+
+    @DisplayName("POST RequestLine을 파싱하여 HttpRequest를 얻는다.")
+    @Test
+    void parseForPost() {
+        // given
+        RequestLineParser requestLineParser = new RequestLineParser();
+
+        // when
+        HttpRequest httpRequest = requestLineParser.parse("POST /users HTTP/1.1");
+
+        // then
+        assertThat(httpRequest).isEqualTo(
+                new HttpRequest(new HttpMethod("POST"), new HttpPath("/users"), new HttpProtocol("HTTP/1.1")));
+    }
+
+    @DisplayName("Query String이 있는 RequestLine을 파싱하여 HttpRequest를 얻는다.")
+    @Test
+    void parseWithQueryString() {
+        // given
+        RequestLineParser requestLineParser = new RequestLineParser();
+
+        // when
+        HttpRequest httpRequest = requestLineParser.parse("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1");
+
+        // then
+        assertThat(httpRequest).isEqualTo(
+                new HttpRequest(new HttpMethod("GET"), new HttpPath("/users?userId=javajigi&password=password&name=JaeSung"), new HttpProtocol("HTTP/1.1")));
     }
 }
