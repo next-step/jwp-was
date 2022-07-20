cd68da7566e80e9d620d22f2a44838466d4878ef
kkwan0226 <kkwan0226@gmail.com>
2022-07-20 21:48:09 +0900

    :sparkles: feat: HTTPMethod 구현

---
+++ src/main/java/webserver/HttpMethod.java
@@ -0,0 +1,24 @@
+package webserver;
+
+import java.util.Arrays;
+
+public enum HttpMethod {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    PATCH("PATCH"),
+    DELETE("DELETE");
+
+    private final String method;
+
+    HttpMethod(String method) {
+        this.method = method;
+    }
+
+    public static HttpMethod from(String method) {
+        return Arrays.stream(values())
+                .filter(value -> value.method.equals(method))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("올바르지 못한 HTTP METHOD 입니다."));
+    }
+}
