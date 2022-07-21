56e6fd385f35009b7334a78ea40417b9eff5c2d9
Shin seongsu <tlstjdtn321@naver.com>
2022-07-22 01:33:08 +0900

    refactor: HttpPath, HttpProtocol 객체로 비슷한 값들 묶어놓기

--- src/main/java/model/RequestLine.java
+++ src/main/java/model/RequestLine.java
@@ -6,64 +6,48 @@ import java.util.Map;
 
 public class RequestLine {
     public static final String DELIMITER = " ";
+    public static final int HTTP_METHOD_INDEX = 0;
+    public static final int HTTP_PATH_INDEX = 1;
+    public static final int HTTP_PROTOCOL_INDEX = 2;
 
-    private HttpMethod method;
-    private String path;
-    private QueryString queryString;
-    private String protocol;
-    private String version;
+    private HttpMethod httpMethod;
+    private HttpPath httpPath;
+    private HttpProtocol httpProtocol;
 
-    public RequestLine() { }
-
-    public RequestLine(HttpMethod method, String path, QueryString queryString, String protocol, String version) {
-        this.method = method;
-        this.path = path;
-        this.queryString = queryString;
-        this.protocol = protocol;
-        this.version = version;
+    public RequestLine(HttpMethod httpMethod, HttpPath httpPath, HttpProtocol httpProtocol) {
+        this.httpMethod = httpMethod;
+        this.httpPath = httpPath;
+        this.httpProtocol = httpProtocol;
     }
 
-    public HttpMethod getMethod() {
-        return method;
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
     }
 
-    public String getPath() {
-        return path;
+    public HttpPath getHttpPath() {
+        return httpPath;
     }
 
-    public QueryString getQueryString() {
-        return queryString;
-    }
-
-    public String getProtocol() {
-        return protocol;
-    }
-
-    public String getVersion() {
-        return version;
+    public HttpProtocol getHttpProtocol() {
+        return httpProtocol;
     }
 
     public static RequestLine from(String line) {
-        System.out.println(line);
         String[] attributes = line.split(DELIMITER);
-        HttpPath httpPath = HttpPath.Instance(attributes[1]);
-        HttpProtocol httpProtocol = HttpProtocol.Instance(attributes[2]);
 
         return new RequestLine(
-                HttpMethod.of(attributes[0]),
-                httpPath.getPath(),
-                httpPath.getQueryString(),
-                httpProtocol.getProtocol(),
-                httpProtocol.getVersion()
+                HttpMethod.of(attributes[HTTP_METHOD_INDEX]),
+                HttpPath.Instance(attributes[HTTP_PATH_INDEX]),
+                HttpProtocol.Instance(attributes[HTTP_PROTOCOL_INDEX])
         );
     }
 
     @Override
     public String toString() {
-        return "method=" + method +
-                ", path='" + path + '\'' +
-                ", queryString=" + queryString +
-                ", protocol='" + protocol + '\'' +
-                ", version='" + version + "'";
+        return "RequestLine{" +
+            "httpMethod=" + httpMethod +
+            ", httpPath=" + httpPath +
+            ", httpProtocol=" + httpProtocol +
+            '}';
     }
 }
