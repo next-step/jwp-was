e6dc8a98068b7fb447ecb769f0c13684546ba9c3
etff <wish2rich@gmail.com>
2022-07-21 21:55:49 +0900

    feat: param parser 추가

--- src/main/java/webserver/RequestLineParser.java
+++ src/main/java/webserver/RequestLineParser.java
@@ -3,53 +3,48 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
 public class RequestLineParser {
     private static final Logger logger = LoggerFactory.getLogger(RequestLineParser.class);
     private static final String INVALID_HTTP_REQUEST = "잘못된 HTTP 요청 ";
     private static final String DELIMITER = " ";
-    private static final int METHOD_INDEX = 0;
-    public static final int PROTOCOL_INDEX = 0;
-    public static final int PATH_INDEX = 1;
-    public static final int VERSION_INDEX = 1;
     public static final int PROTOCOL_INFORMATION_SIZE = 2;
     private static final int MINIMUM_REQUEST_LINE_TOKEN = 3;
-    public static final String PROTOCOL_DELIMITER = "/";
-    private String[] items;
-
+    private HttpMethod httpMethod;
+    private Protocol protocol;
 
     public RequestLineParser(String line) {
         if (line == null) {
             throw new IllegalArgumentException(INVALID_HTTP_REQUEST + line);
         }
-        this.items = line.split(DELIMITER);
-        if (items.length < MINIMUM_REQUEST_LINE_TOKEN) {
+        String[] item = line.split(DELIMITER);
+
+        if (item.length < MINIMUM_REQUEST_LINE_TOKEN) {
             throw new IllegalArgumentException(INVALID_HTTP_REQUEST + line);
         }
+
+        this.httpMethod = new HttpMethod(item);
+        this.protocol = new Protocol(item[PROTOCOL_INFORMATION_SIZE]);
     }
 
     public String getMethod() {
-        return items[METHOD_INDEX];
+        return httpMethod.getMethod();
     }
 
     public String getPath() {
-        return items[PATH_INDEX];
+        return httpMethod.getPath();
     }
 
     public String getProtocol() {
-        return getProtocolInformation(PROTOCOL_INDEX);
-    }
-
-    private String getProtocolInformation(int index) {
-        final String item = items[PROTOCOL_INFORMATION_SIZE];
-        final String[] protocolInformation = item.split(PROTOCOL_DELIMITER);
-        if (protocolInformation.length != PROTOCOL_INFORMATION_SIZE) {
-            logger.error(INVALID_HTTP_REQUEST);
-            throw new IllegalArgumentException(INVALID_HTTP_REQUEST);
-        }
-        return protocolInformation[index];
+        return protocol.getProtocol();
     }
 
     public Double getVersion() {
-        return Double.valueOf(getProtocolInformation(VERSION_INDEX));
+        return protocol.getVersion();
+    }
+
+    public Map<String, String> getParams() {
+        return httpMethod.getParams();
     }
 }
--- src/test/java/webserver/RequestLineParserTest.java
+++ src/test/java/webserver/RequestLineParserTest.java
@@ -29,4 +29,15 @@ class RequestLineParserTest {
         assertThat(parser.getProtocol()).isEqualTo("HTTP");
         assertThat(parser.getVersion()).isEqualTo(1.1);
     }
+
+    @DisplayName("GET param 요청을 받고 파싱된 값을 리턴한다.")
+    @Test
+    void get_request_with_param() {
+        RequestLineParser parser = new RequestLineParser("GET /user/create?userId=javajigi&password=pass HTTP/1.1");
+
+        assertThat(parser.getMethod()).isEqualTo("GET");
+        assertThat(parser.getPath()).isEqualTo("/user/create");
+        assertThat(parser.getProtocol()).isEqualTo("HTTP");
+        assertThat(parser.getVersion()).isEqualTo(1.1);
+    }
 }
