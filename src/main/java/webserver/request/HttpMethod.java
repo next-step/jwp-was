cec0eac48d8978cbd5580bb69726ad8b7d4197b2
sang5c <skah321@gmail.com>
2022-07-21 17:38:53 +0900

    feat(HttpMethod): 문자열을 받아 HttpMethod 반환한다

---
+++ src/main/java/webserver/request/HttpMethod.java
@@ -0,0 +1,6 @@
+package webserver.request;
+
+public enum HttpMethod {
+    GET,
+    POST
+}
---
+++ src/test/java/webserver/request/HttpMethodTest.java
@@ -0,0 +1,26 @@
+package webserver.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class HttpMethodTest {
+
+    @DisplayName("valueOf 메소드 테스트")
+    @ParameterizedTest(name = "\"{0}\" 문자열을 받으면 HttpMethod {1}을 반환한다.")
+    @CsvSource({"GET, GET", "POST, POST"})
+    void valueOf(String source, HttpMethod expected) {
+        assertThat(HttpMethod.valueOf(source)).isEqualTo(expected);
+    }
+
+    @DisplayName("잘못된 문자열을 전달하면 IllegalArgumentException이 발생한다.")
+    @Test
+    void valueOfThrowException() {
+        assertThatThrownBy(() -> HttpMethod.valueOf("SEND"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
