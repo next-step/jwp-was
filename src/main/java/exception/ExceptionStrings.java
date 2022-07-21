fa79410e6bd754ef5c3c3d99297415ba2c946064
itdar <thesockerr@gmail.com>
2022-07-21 22:16:36 +0900

    refactor: 프로토콜의 매직넘버, 매직리터럴 상수화

--- src/main/java/exception/ExceptionStrings.java
+++ src/main/java/exception/ExceptionStrings.java
@@ -3,6 +3,6 @@ package exception;
 public final class ExceptionStrings {
 
     public static final String INVALID_REQUEST_LINE = "Invalid requestLine string";
-
     public static final String INVALID_REQUEST_PATH = "Invalid requestLine path";
+    public static final String INVALID_PROTOCOL_VERSION_STRING = "Invalid protocol and version string";
 }
--- src/main/java/webserver/enums/RequestProtocol.java
+++ src/main/java/webserver/enums/RequestProtocol.java
@@ -1,7 +1,14 @@
 package webserver.enums;
 
+import static exception.ExceptionStrings.INVALID_PROTOCOL_VERSION_STRING;
+
 public enum RequestProtocol {
     HTTP_1_1("HTTP", "1.1");
+
+    private static final String PROTOCOL_DELIMITER = "/";
+    private static final String VERSION_DOT = "\\.";
+    private static final int PROTOCOL_COMPONENT_COUNT = 2;
+    private static final String UNDER_BAR = "_";
 
     private final String protocol;
     private final String version;
@@ -20,13 +27,13 @@ public enum RequestProtocol {
     }
 
     public static RequestProtocol of(String protocolString) {
-        if (protocolString.split("/").length < 2) {
-            throw new IllegalArgumentException("Invalid protocol and version string");
+        if (protocolString.split(PROTOCOL_DELIMITER).length < PROTOCOL_COMPONENT_COUNT) {
+            throw new IllegalArgumentException(INVALID_PROTOCOL_VERSION_STRING);
         }
 
         return RequestProtocol.valueOf(protocolString
-            .replaceAll("/", "_")
-            .replaceAll("\\.", "_")
+            .replaceAll(PROTOCOL_DELIMITER, UNDER_BAR)
+            .replaceAll(VERSION_DOT, UNDER_BAR)
         );
     }
 
