16f602b0a59113a056f0d62070d07fbe3845834d
Shin seongsu <tlstjdtn321@naver.com>
2022-07-22 01:31:21 +0900

    refactor: HTTP METHOD 허용되지 않은 값이 왔을 경우 예외 처리

--- src/main/java/constant/HttpMethod.java
+++ src/main/java/constant/HttpMethod.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 public enum HttpMethod {
     GET,
     POST,
-
-    UNKNOWN;
+    PATCH,
+    DELETE,
+    PUT;
 
     public static HttpMethod of(String httpmethod) {
         return Arrays.stream(values())
                 .filter(method -> method.name().equals(httpmethod))
                 .findFirst()
-                .orElse(UNKNOWN);
+                .orElseThrow(() -> new RuntimeException(Message.NOT_ALLOW_HTTP_METHOD.getValue()));
     }
 
 }
