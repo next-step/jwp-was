59ff90050b70c202fcfd5dc97a47a4f33c39e95a
itdar <thesockerr@gmail.com>
2022-07-23 01:21:38 +0900

    feat: 기능요구사항 1 구현
    - http://localhost:8080/index.html 로 접속했을 때 webapp 디렉토리의 index.html 파일을 읽어 클라이언트에 응답한다.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ---
 
 ## 2단계. HTTP 웹 서버 구현
-- [ ] 기능 요구사항 1
+- [x] 기능 요구사항 1
 > http://localhost:8080/index.html 로 접속했을 때 webapp 디렉토리의 index.html 파일을 읽어 클라이언트에 응답한다.
 - HTTP Request Header 예
 ```
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,23 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
+import webserver.request.HttpRequest;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String PATH_TEMPLATES = "./templates";
 
     private Socket connection;
 
@@ -19,18 +26,39 @@ public class RequestHandler implements Runnable {
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        logger.debug("Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        try (InputStream inputStream = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            HttpRequest httpRequest = parseRequest(inputStream);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            byte[] body = FileIoUtils.loadFileFromClasspath(PATH_TEMPLATES + httpRequest.getPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
+        } catch (IOException | URISyntaxException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private HttpRequest parseRequest(InputStream is) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String line;
+            line = br.readLine();
+            HttpRequest httpRequest = new HttpRequest(line);
+            logger.debug("RequestLine: " + line);
+            while (!line.equals("")) {
+                line = br.readLine();
+                logger.debug("Headers: " + line);
+            }
+
+            return httpRequest;
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+
+        throw new IllegalStateException();
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
