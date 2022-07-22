b41c944490f9c9d2ac50b07dc9e3b726d412017a
mintjordy <mintjordy@woowahan.com>
2022-07-22 23:56:15 +0900

    refactor: mockito를 사용하던 테스트에서 mock 의존 제거

--- src/test/java/webserver/http/request/parser/RequestLineParserTest.java
+++ src/test/java/webserver/http/request/parser/RequestLineParserTest.java
@@ -1,34 +1,30 @@
 package webserver.http.request.parser;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import webserver.http.request.Method;
 import webserver.http.request.Protocol;
 import webserver.http.request.RequestLine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static webserver.http.request.parser.fixture.URIFixture.fixtureWithQueryParameters;
 
-@ExtendWith(MockitoExtension.class)
 class RequestLineParserTest {
-
-    @InjectMocks
     private RequestLineParser requestLineParser;
 
-    @Mock
-    private MethodParser methodParser;
-    @Mock
-    private URIParser uriParser;
-    @Mock
-    private ProtocolParser protocolParser;
+    @BeforeEach
+    void setUp() {
+        MethodParser methodParser = new MethodParser();
+        URIParser uriParser = new URIParser(
+                new KeyValuePairParser(),
+                new QueryParametersParser(new KeyValuePairParser())
+        );
+        ProtocolParser protocolParser = new ProtocolParser();
+
+        requestLineParser = new RequestLineParser(methodParser, uriParser, protocolParser);
+    }
 
     @DisplayName("문자열이 white space 값을 기준으로 3개의 문자열로 구분이 되지 않는 경우 예외발생")
     @Test
@@ -43,9 +39,6 @@ class RequestLineParserTest {
     @Test
     void parse() {
         String requestLineMessage = "GET /uri?name=jordy HTTP/1.1";
-        when(methodParser.parse(any())).thenReturn(Method.GET);
-        when(uriParser.parse(any())).thenReturn(fixtureWithQueryParameters("/uri", "name", "jordy"));
-        when(protocolParser.parse(any())).thenReturn(new Protocol("HTTP", "1.1"));
 
         RequestLine actual = requestLineParser.parse(requestLineMessage);
 
@@ -55,8 +48,5 @@ class RequestLineParserTest {
                                 fixtureWithQueryParameters("/uri", "name", "jordy"),
                                 new Protocol("HTTP", "1.1")
                         ));
-        verify(methodParser).parse("GET");
-        verify(uriParser).parse("/uri?name=jordy");
-        verify(protocolParser).parse("HTTP/1.1");
     }
 }
