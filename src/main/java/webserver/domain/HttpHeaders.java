02ff84a1313574f94ca701750b5e21dafd94bd16
catsbi <killfine1988@gmail.com>
2022-07-23 13:48:31 +0900

    feat: HttpHeaders 테스트 케이스 추가 및 로직 개선

--- src/main/java/webserver/domain/HttpHeaders.java
+++ src/main/java/webserver/domain/HttpHeaders.java
@@ -9,6 +9,8 @@ public class HttpHeaders {
     public static final String DELIMITER = ":";
     public static final int KEY_POINT = 0;
     public static final int VALUE_POINT = 1;
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String DEFAULT_CONTENT_TYPE = "text/html";
 
     private final Map<String, String> headers = new HashMap<>();
 
@@ -21,13 +23,35 @@ public class HttpHeaders {
         return httpHeaders;
     }
 
-    private void add(String headerStr) {
+    public static HttpHeaders defaultResponseHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(CONTENT_TYPE, DEFAULT_CONTENT_TYPE);
+
+        return httpHeaders;
+    }
+
+    public void add(String headerStr) {
         String[] headerInfo = headerStr.split(DELIMITER);
+
+        if (headerInfo.length != 2) {
+            throw new IllegalArgumentException("유효한 속성 문자열이 아닙니다. value:"+headerStr);
+        }
 
         headers.put(headerInfo[KEY_POINT].trim(), headerInfo[VALUE_POINT].trim());
     }
 
+    public void add(String key, String value) {
+        headers.put(key, value);
+    }
+
     public Map<String, String> getHeaders() {
         return Collections.unmodifiableMap(headers);
+    }
+
+    public String getAttribute(String key) {
+        if (!headers.containsKey(key)) {
+            throw new IllegalArgumentException();
+        }
+        return headers.get(key);
     }
 }
