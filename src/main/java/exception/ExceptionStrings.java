c9be22d5060872496dff751152154d54033c4082
Gin <thesockerrr@gmail.com>
2022-07-23 12:17:30 +0900

    [Step1] TDD 실습 리뷰 요청드립니다. (#257)
    * doc: 요구사항 내용 추가
    
    * feat: Step 1 구현
    
    * refactor: Request 파싱 관련 리팩토링
    
    - HttpRequest -> RequestLine(StartLine) 구조로 변경
    - RequestProtocol enum 추가 (프로토콜의 종류, 버전 통합 관리)
    
    * refactor: 동철님 리뷰사항 수정 커밋 #1
    
    - 매직넘버 제거
    - Collections.emptyMap() 적용
    - 빠진 테스트 추가 및 테스트 의미에 맞게 설명 수정
    
    * refactor: 프로토콜의 매직넘버, 매직리터럴 상수화
    
    * test: 빠진 테스트케이스 추가
    
    - Path, QueryStrings 관련 테스트 추가

---
+++ src/main/java/exception/ExceptionStrings.java
@@ -0,0 +1,8 @@
+package exception;
+
+public final class ExceptionStrings {
+
+    public static final String INVALID_REQUEST_LINE = "Invalid requestLine string";
+    public static final String INVALID_REQUEST_PATH = "Invalid requestLine path";
+    public static final String INVALID_PROTOCOL_VERSION_STRING = "Invalid protocol and version string";
+}
---
+++ src/main/java/webserver/enums/HttpMethod.java
@@ -0,0 +1,6 @@
+package webserver.enums;
+
+public enum HttpMethod {
+    GET,
+    POST
+}
