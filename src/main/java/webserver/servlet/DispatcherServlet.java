f66aa730ccdb2b7cb14d9f93736e0853d36d991b
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-24 05:00:01 +0900

    feat: Dispatcher Servlet 코드 추가 - 요청에 따른 컨트롤러 매핑 기능

---
+++ src/main/java/webserver/servlet/DispatcherServlet.java
@@ -0,0 +1,30 @@
+package webserver.servlet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import webserver.http.request.HttpRequest;
+import webserver.controller.Controller;
+import webserver.controller.IndexController;
+import webserver.http.response.HttpResponse;
+
+public class DispatcherServlet implements Servlet {
+
+	private Map<String, Controller> controllerMap = new HashMap<>();
+
+	public DispatcherServlet() {
+		controllerMap.put("/", new IndexController());
+		controllerMap.put("/index.html", new IndexController());
+	}
+
+	@Override
+	public HttpResponse service(HttpRequest httpRequest) {
+		// Path 정보 추출
+		String uri = httpRequest.getPath();
+		String path = uri.substring(uri.lastIndexOf("/"));
+
+		Controller controller = controllerMap.get(path);
+
+		return controller.process(httpRequest);
+	}
+}
