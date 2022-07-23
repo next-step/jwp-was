e0798a9a67b6e8f2fdc57a3ec1fb625ca6d51601
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-24 04:57:07 +0900

    feat: ContentType 코드 구현

---
+++ src/main/java/webserver/http/ContentType.java
@@ -0,0 +1,32 @@
+package webserver.http;
+
+import java.util.Arrays;
+
+public enum ContentType {
+	JS(".js", "text/javascript"),
+	CSS(".css", "text/css"),
+	SVG(".svg", "image/svg+xml"),
+	HTML(".html", "text/html;charset=utf-8");
+
+	private static final String NOT_SUPPORTED_CONTENT_TYPE = "지원하지 않는 Content Type 입니다.";
+
+	private final String extension;
+	private final String mime;
+
+	ContentType(String extension, String mime) {
+		this.extension = extension;
+		this.mime = mime;
+	}
+
+	public static ContentType findContentType(String uri) {
+		return Arrays.stream(ContentType.values())
+					 .filter(contentType -> uri.endsWith(contentType.extension))
+					 .findAny()
+					 .orElseThrow(() -> new IllegalArgumentException(NOT_SUPPORTED_CONTENT_TYPE));
+	}
+
+	public String getMime() {
+		return mime;
+	}
+
+}
