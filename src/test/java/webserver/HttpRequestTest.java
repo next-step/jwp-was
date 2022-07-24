7bd6b6120a6b0883efdd81830a29b72206247a2f
YONGJU <odetsuo@gmail.com>
2022-07-24 20:10:50 +0900

    test: 로그인 하지 않은 상태에서 사용자 목록 조회
    로그인 페이지로 이동한다

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -3,7 +3,6 @@ package webserver;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
