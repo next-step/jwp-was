b84cd943cb413c11bbe0eee3c833632639eae11d
이정수 <lsecret@naver.com>
2022-07-24 11:47:25 +0900

    feat: 요구사항 2. POST 요청 파싱 테스트 추가

--- src/test/java/webserver/http/RequestLineTest.java
+++ src/test/java/webserver/http/RequestLineTest.java
@@ -9,47 +9,44 @@ import static webserver.http.RequestTestConstant.*;
 
 public class RequestLineTest {
 
-    private RequestLine requestLine;
+    private RequestLine getRequestLine;
 
     @BeforeEach
     void setup() {
-        requestLine = new RequestLine(GET_REQUEST);
+        getRequestLine = new RequestLine(GET_REQUEST);
     }
 
     @Test
     @DisplayName("RequestLine을 파싱하여 Method를 가져온다.")
     void parsingRequestLineAndGetMethod() {
         // then
-        assertThat(requestLine.method()).isEqualTo(GET_METHOD);
+        assertThat(getRequestLine.method()).isEqualTo(GET_METHOD);
     }
 
     @Test
     @DisplayName("RequestLine을 파싱하여 path를 가져온다.")
     void parsingRequestLineAndGetPath() {
         // then
-        assertThat(requestLine.path()).isEqualTo(PATH);
+        assertThat(getRequestLine.path()).isEqualTo(PATH);
     }
 
     @Test
     @DisplayName("RequestLine을 파싱하여 protocol을 가져온다.")
     void parsingRequestLineAndGetProtocol() {
-        // when
-        RequestLine requestLine = new RequestLine(GET_REQUEST);
-
         // then
-        assertThat(requestLine.protocol()).isEqualTo(PROTOCOL);
+        assertThat(getRequestLine.protocol()).isEqualTo(PROTOCOL);
     }
 
     @Test
     @DisplayName("RequestLine을 파싱하여 version을 가져온다.")
     void parsingRequestLineAndGetVersion() {
         // then
-        assertThat(requestLine.version()).isEqualTo(VERSION);
+        assertThat(getRequestLine.version()).isEqualTo(VERSION);
     }
 
     @Test
-    @DisplayName("RequestLine을 파싱한 결과 정상 확인")
-    void parsingRequestLine() {
+    @DisplayName("GET RequestLine을 파싱한 결과 정상 확인")
+    void parsingGetRequestLine() {
         // given
         Protocol protocol = new Protocol(GET_REQUEST.split(REQUEST_SPLIT_SYMBOL)[2]);
 
@@ -61,4 +58,16 @@ public class RequestLineTest {
     }
 
 
+    @Test
+    @DisplayName("POST RequestLine을 파싱한 결과 정상 확인")
+    void parsingPostRequestLine() {
+        // given
+        Protocol protocol = new Protocol(POST_REQUEST.split(REQUEST_SPLIT_SYMBOL)[2]);
+
+        // when
+        RequestLine requestLine = new RequestLine(POST_REQUEST);
+
+        // then
+        assertThat(requestLine).isEqualTo(new RequestLine(POST_METHOD, PATH, protocol));
+    }
 }
--- src/test/java/webserver/http/RequestTestConstant.java
+++ src/test/java/webserver/http/RequestTestConstant.java
@@ -2,11 +2,13 @@ package webserver.http;
 
 public class RequestTestConstant {
     static final String GET_METHOD = "GET";
+    static final String POST_METHOD = "POST";
     static final String PATH = "/users";
     static final String PROTOCOL = "HTTP";
     static final String VERSION = "1.1";
     static final String PROTOCOL_VERSION = "HTTP/1.1";
     static final String GET_REQUEST = "GET /users HTTP/1.1";
+    static final String POST_REQUEST = "POST /users HTTP/1.1";
 
     static final String REQUEST_SPLIT_SYMBOL = " ";
 
