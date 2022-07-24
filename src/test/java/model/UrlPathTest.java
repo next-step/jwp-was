d2e5ce17fca512ce4893169a9dea3f9d79646811
fistkim101 <fistkim101@gmail.com>
2022-07-24 16:34:16 +0900

    test : UrlPath 내 path내 파일 확장자 포함 여부 판단 로직 검증 및 파일이름 파싱로직 검증

---
+++ src/test/java/model/UrlPathTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UrlPathTest {
+
+    @DisplayName("확장자 파싱 검증")
+    @Test
+    void hasExtensionTest() {
+        UrlPath noFileUrlPath = new UrlPath("/domain/user");
+        Assertions.assertThat(noFileUrlPath.hasExtension()).isFalse();
+
+        UrlPath wrongFileUrlPath = new UrlPath("/index.htl");
+        Assertions.assertThat(wrongFileUrlPath.hasExtension()).isFalse();
+
+        UrlPath fileUrlPath1 = new UrlPath("/domain/index.html");
+        Assertions.assertThat(fileUrlPath1.hasExtension()).isTrue();
+
+        UrlPath fileUrlPath2 = new UrlPath("/index.html");
+        Assertions.assertThat(fileUrlPath2.hasExtension()).isTrue();
+    }
+
+    @DisplayName("파일 이름 파싱 검증")
+    @Test
+    void getFileNameTest() {
+        UrlPath hasFileUrlPath = new UrlPath("/index.html");
+        String fileName = hasFileUrlPath.getFileName();
+        Assertions.assertThat(fileName).isEqualTo("index.html");
+    }
+}
