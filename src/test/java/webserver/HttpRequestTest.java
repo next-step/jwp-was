507b0ed03f12690c046320e9585b2df195b13049
YONGJU <odetsuo@gmail.com>
2022-07-24 18:34:03 +0900

    test: 로그인 실패 테스트

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,17 +1,62 @@
 package webserver;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+class HttpRequestTest {
 
-public class HttpRequestTest {
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() {
+        restTemplate = new RestTemplate();
+    }
+
     @Test
     void request_resttemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/index.html", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @DisplayName("로그인 실패")
+    @Test
+    void login_failed() {
+        회원가입_요청("admin", "pass");
+
+        ResponseEntity<String> response = 로그인_요청("admin", "password");
+
+        org.junit.jupiter.api.Assertions.assertAll(
+            () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND),
+            () -> assertThat(response.getHeaders().get("Location")).containsExactly("/user/login_failed.html"),
+            () -> assertThat(response.getHeaders().get("Set-Cookie")).containsExactly("logined=false; Path=/")
+        );
+
+    }
+
+    private ResponseEntity<String> 로그인_요청(final String userId, final String password) {
+        Map<String, String> loginParams = Map.of(
+            "userId", userId,
+            "password", password
+        );
+
+        return restTemplate.postForEntity("http://localhost:8080/user/login", loginParams, String.class);
+    }
+
+    private void 회원가입_요청(final String userId, final String password) {
+        Map<String, String> createUserParams = Map.of(
+            "userId", userId,
+            "password", password,
+            "name", "관리자",
+            "email", "admin@admin.com"
+        );
+
+        restTemplate.postForEntity("http://localhost:8080/user/create", createUserParams, String.class);
     }
 }
