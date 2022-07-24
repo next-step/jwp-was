4d7dd09c4beed35434eb9af749a9fe344cb1d540
Jeongsu Yang <mycat83@gmail.com>
2022-07-24 23:04:03 +0900

    refactor: 테스트 메서드명 변경

--- src/test/java/domain/HttpPathTest.java
+++ src/test/java/domain/HttpPathTest.java
@@ -1,11 +1,9 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("HttpPath 단위 테스트")
@@ -17,7 +15,7 @@ class HttpPathTest {
             "/users?userId=javajigi=JaeSung",
             "/users?userId=javajigi?name=JaeSung"
     })
-    void initException(String path) {
+    void createException(String path) {
         assertThatThrownBy(() -> new HttpPath(path))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(HttpPath.VALIDATION_MESSAGE);
--- src/test/java/domain/RequestLineTest.java
+++ src/test/java/domain/RequestLineTest.java
@@ -1,6 +1,5 @@
 package domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,9 +9,9 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("RequestLineParser 단위 테스트")
 public class RequestLineTest {
-    @DisplayName("GET RequestLine을 파싱하여 HttpRequest를 얻는다.")
+    @DisplayName("GET RequestLine을 생성한다.")
     @Test
-    void parseForGet() {
+    void createForGet() {
         // when
         RequestLine requestLine = new RequestLine("GET /users HTTP/1.1");
 
@@ -21,9 +20,9 @@ public class RequestLineTest {
                 new RequestLine(HttpMethod.GET, new HttpPath("/users"), new HttpProtocol("HTTP/1.1")));
     }
 
-    @DisplayName("POST RequestLine을 파싱하여 HttpRequest를 얻는다.")
+    @DisplayName("POST RequestLine을 생성한다.")
     @Test
-    void parseForPost() {
+    void createForPost() {
         // when
         RequestLine requestLine = new RequestLine("POST /users HTTP/1.1");
 
@@ -32,9 +31,9 @@ public class RequestLineTest {
                 new RequestLine(HttpMethod.POST, new HttpPath("/users"), new HttpProtocol("HTTP/1.1")));
     }
 
-    @DisplayName("Query String이 있는 RequestLine을 파싱하여 HttpRequest를 얻는다.")
+    @DisplayName("Query String이 있는 RequestLine을 생성한다.")
     @Test
-    void parseWithQueryString() {
+    void createWithQueryString() {
         // when
         RequestLine requestLine = new RequestLine("GET /users?userId=javajigi&password=password&name=JaeSung HTTP/1.1");
 
@@ -43,13 +42,13 @@ public class RequestLineTest {
                 new RequestLine(HttpMethod.GET, new HttpPath("/users?userId=javajigi&password=password&name=JaeSung"), new HttpProtocol("HTTP/1.1")));
     }
 
-    @DisplayName("HTTP 요청 형식이 맞지 않으면 예외를 발생한다.")
+    @DisplayName("Request Line 형식이 맞지 않으면 예외를 발생한다.")
     @ParameterizedTest(name = "{displayName} - {arguments}")
     @ValueSource(strings = {
             "GET /users",
             "GET /users HTTP/1.1 HTTP/1.1",
     })
-    void parseException(String requestLine) {
+    void createException(String requestLine) {
         assertThatThrownBy(() -> new RequestLine(requestLine))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(RequestLine.VALIDATION_MESSAGE);
