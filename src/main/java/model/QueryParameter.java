af4878562a2e8cb7c0cfb36a43061d75b692715e
fistkim101 <fistkim101@gmail.com>
2022-07-24 12:37:19 +0900

    feat : QueryParameter 없을시 빈 Map 생기지 않도록 로직 분리

--- src/main/java/model/QueryParameter.java
+++ src/main/java/model/QueryParameter.java
@@ -4,10 +4,12 @@ import java.util.Map;
 
 public class QueryParameter {
 
-    private final Map<String, String> parameters;
+    private Map<String, String> parameters;
 
     public QueryParameter(Map<String, String> parameters) {
-        this.parameters = parameters;
+        if (!parameters.isEmpty()) {
+            this.parameters = parameters;
+        }
     }
 
     public Map<String, String> getParameters() {
