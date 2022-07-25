d7859fe50d15a88b56c20ff0a88da1fe9218f9d8
KimYongKwon <dydrnjs518@gmail.com>
2022-07-25 21:53:18 +0900

    feat - 기능 요구사항 1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,16 +2,21 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.URISyntaxException;
 
 import com.google.common.base.Charsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.IOUtils;
 import webserver.request.domain.request.RequestLine;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+
+//    private HttpRequest httpRequest = new HttpRequest();
+    private RequestLine requestLine;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -23,7 +28,7 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            showRequest(in);
+            //showRequest(in);
 
 //            BufferedReader br = new BufferedReader(new InputStreamReader(in, Charsets.UTF_8));
 //            String line = br.readLine();
@@ -32,12 +37,16 @@ public class RequestHandler implements Runnable {
 //                line = br.readLine();
 //                logger.debug("header : {}", line);
 //            }
-            
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, Charsets.UTF_8));
+            RequestLine requestLine = RequestLine.parse(br.readLine());
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            String path = IOUtils.loadFileFromClasspath(requestLine.parsePath());
+            byte[] body = path.getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
@@ -46,12 +55,14 @@ public class RequestHandler implements Runnable {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(is, Charsets.UTF_8));
             String line = br.readLine();
-            RequestLine requestLine = RequestLine.create(line);
+            RequestLine requestLine = RequestLine.parse(line);
+//            httpRequest.addProperty(line);
             logger.debug(requestLine.toString());
 
             StringBuilder stringBuilder = new StringBuilder();
             while(!line.equals("")) {
                 line = br.readLine();
+//                httpRequest.addProperty(line);
                 stringBuilder.append(line + "\n");
             }
 
