cec476af53ea1115e28253f1d74e57f13dd14933
rheew <47098896+rheew@users.noreply.github.com>
2022-07-26 14:09:48 +0900

    feat: http://localhost:8080/index.html 로 접속했을 때 webapp 디렉토리의 index.html 파일을 읽어 클라이언트에 응답한다.

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -5,10 +5,23 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileIoUtils {
+
+    private static final String classPath = "./templates";
+    private static final Set<String> files = new HashSet<>(Arrays.asList("/index.html"));
+
     public static byte[] loadFileFromClasspath(String filePath) throws IOException, URISyntaxException {
-        Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
+//        if (!files.contains(filePath)) {
+//            return "Hello World".getBytes();
+//        }
+        Path path = Paths.get(FileIoUtils.class.getClassLoader()
+                .getResource(classPath + filePath)
+                .toURI());
+
         return Files.readAllBytes(path);
     }
 }
