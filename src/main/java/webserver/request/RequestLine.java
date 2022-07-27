4499b1408d6ecb2d6ee9b32aa51ea37ea9b51150
황정현 <jhhwang@wemakeprice.com>
2022-07-27 16:23:35 +0900

    refactor 요청 클래스 변경

--- src/main/java/webserver/request/RequestLine.java
+++ src/main/java/webserver/request/RequestLine.java
@@ -5,7 +5,7 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.HttpMethod;
-import utils.RequestUtils;
+import utils.IOUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,28 +19,24 @@ public class RequestLine {
     private String protocol;
     private String version;
 
-    private RequestLine() {
+    public RequestLine(String uri, HttpMethod method, String protocol, String version) {
+        this.uri = uri;
+        this.method = method;
+        this.protocol = protocol;
+        this.version = version;
     }
 
-    private static class InnerInstanceRequestLine {
-        private static final RequestLine instance = new RequestLine();
-    }
-
-    public static RequestLine getInstance() {
-        return InnerInstanceRequestLine.instance;
-    }
-
-    public RequestLine parsing(BufferedReader br) throws IOException {
+    public static RequestLine parsing(BufferedReader br) throws IOException {
         String firstLine = br.readLine();
         String[] requestDataArray = firstLine.split(" ");
         String method = requestDataArray[0];
-        String path = requestDataArray[1];
+        String uri = requestDataArray[1];
         if (!HttpMethod.checkHttpMethod(method)) {
             throw new NotExistHttpMethodException(NOT_EXIST_HTTP_METHOD);
         }
 
         String[] protocolAndVersion = requestDataArray[2].split("/");
-        return setting(HttpMethod.valueOf(method), path, protocolAndVersion[0], protocolAndVersion[1]);
+        return new RequestLine(uri, HttpMethod.valueOf(method), protocolAndVersion[0], protocolAndVersion[1]);
     }
 
     public String getQueryParam() {
@@ -56,16 +52,8 @@ public class RequestLine {
         String[] queryStringArr = this.uri.split("\\?");
         String queryString = queryStringArr[1];
         logger.debug("queryParam {}", queryString);
-        User user = RequestUtils.convertToUser(queryString);
+        User user = User.convertToUser(IOUtils.changeStringToMap(queryString));
         return user;
-    }
-
-    private RequestLine setting(HttpMethod method, String path, String protocol, String version) {
-        this.method = method;
-        this.uri = path;
-        this.protocol = protocol;
-        this.version = version;
-        return this;
     }
 
     public String getUri() {
