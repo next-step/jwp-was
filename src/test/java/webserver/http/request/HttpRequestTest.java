01c4b89838d474ab6df1193a23ca79f11e1733cf
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-27 11:41:16 +0900

    test: HttpRequest 테스트 코드 추가

---
+++ src/test/java/webserver/http/request/HttpRequestTest.java
@@ -0,0 +1,34 @@
+package webserver.http.request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+public class HttpRequestTest {
+	private final String testDirectory = "./src/test/resources/";
+
+	@Test
+	public void request_GET() throws Exception {
+		InputStream in = new FileInputStream(testDirectory + "http/Http_GET.txt");
+		HttpRequest request = HttpRequestParser.parse(in);
+
+		assertThat(request.getMethod().toString()).isEqualTo("GET");
+		assertThat(request.getPath()).isEqualTo("/user/create");
+		assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
+		assertThat(request.getParameter("userId")).isEqualTo("javajigi");
+	}
+
+	@Test
+	public void request_POST() throws Exception {
+		InputStream in = new FileInputStream(testDirectory + "http/Http_POST.txt");
+		HttpRequest request = HttpRequestParser.parse(in);
+
+		assertThat(request.getMethod().toString()).isEqualTo("POST");
+		assertThat(request.getPath()).isEqualTo("/user/create");
+		assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
+		assertThat(request.getParameter("userId")).isEqualTo("javajigi");
+	}
+}
