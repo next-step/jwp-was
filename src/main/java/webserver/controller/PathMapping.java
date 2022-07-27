7981472d04c27ab725c6a845d279a0f29a254a7e
황정현 <jhhwang@wemakeprice.com>
2022-07-27 16:22:58 +0900

    feat url mapping 역할 클래스 추가

--- src/main/java/webserver/controller/PathMapping.java
+++ src/main/java/webserver/controller/PathMapping.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 
-public enum RequestApiPath {
+public enum PathMapping {
     CREATE("/user/create", (request, response) -> {
         new CreateUserController().service(request, response);
     }),
@@ -16,23 +16,32 @@ public enum RequestApiPath {
     }),
     LOGIN("/user/login", (request, response) -> {
         new LoginController().service(request, response);
-    });
+    }),
+    NONE("none", ((request, response) -> {
+        new NotFoundController().service(request, response);
+    }));
 
     String path;
     BiConsumer<HttpRequest, HttpResponse> consumer;
 
-    RequestApiPath(String path, BiConsumer<HttpRequest, HttpResponse> consumer) {
+    PathMapping(String path, BiConsumer<HttpRequest, HttpResponse> consumer) {
         this.path = path;
         this.consumer = consumer;
     }
 
-    public static Optional<RequestApiPath> findMappingUrl(HttpRequest request) {
-        return Arrays.stream(RequestApiPath.values())
+    public static PathMapping findMappingUrl(HttpRequest request) {
+        return Arrays.stream(PathMapping.values())
                 .filter(value -> value.path.startsWith(request.getRequestPath()))
-                .findAny();
+                .findAny()
+                .orElse(PathMapping.NONE);
     }
 
-    public void findPath(HttpRequest request, HttpResponse response) {
+    public void mappingController(HttpRequest request, HttpResponse response) {
         this.consumer.accept(request, response);
+    }
+
+    public static boolean isExist(String requestPath) {
+        return Arrays.stream(PathMapping.values())
+                .anyMatch(value -> value.path.equals(requestPath));
     }
 }
