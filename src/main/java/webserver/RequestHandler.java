77da94dff3c23774efb2dcf35de55d89379c67f7
황정현 <jhhwang@wemakeprice.com>
2022-07-27 16:21:19 +0900

    refactor response 관렬 코드 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,20 +2,14 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-
-import com.github.jknack.handlebars.internal.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import webserver.request.Request;
-import webserver.request.RequestApiPath;
-import webserver.response.FileResponse;
-import webserver.response.Response;
+import webserver.request.HttpRequest;
+import webserver.response.HttpResponse;
 import utils.IOUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    public static final String REDIRECT = "redirect:";
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -28,47 +22,12 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             DataOutputStream dos = new DataOutputStream(out);
-            Request request = IOUtils.convertRequest(in);
+            HttpRequest request = IOUtils.convertRequest(in);
             logger.debug("requestLine : {}", request.getRequestLine());
-            Response response = getResponse(request);
-            response.sendStatus(dos);
-
+            HttpResponse response = new HttpResponse();
+            response.start(request, dos);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
-    }
-
-    private Response getResponse(Request request) {
-        String requestPath = request.getRequestPath();
-        logger.debug("webserver.request Path : {}", requestPath);
-        Response response = new Response();
-        return FileResponse.getFileResponse(requestPath)
-                .orElse(getApiResponse(request, response));
-    }
-
-    private Response getApiResponse(Request request, Response response) {
-
-        String path = RequestApiPath.getViewName(request, response);
-        return getResponse(request, response, path);
-    }
-
-    private Response getResponse(Request request, Response response, String path) {
-        if (path.startsWith(REDIRECT)) {
-            return redirectResponse(response, path);
-        }
-
-        try {
-            response.makeStatus(HttpStatus.OK);
-            response.addBody(ViewResolver.mapping(response, path).getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return response;
-    }
-
-    private Response redirectResponse(Response response, String path) {
-        response.makeStatus(HttpStatus.FOUND);
-        response.makeLocationPath(path.replace(REDIRECT, StringUtils.EMPTY));
-        return response;
     }
 }
