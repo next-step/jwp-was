093d529ff11755d86ba454b11dbc7deab53b27b2
seondongpyo <seondongpyo@gmail.com>
2022-07-27 21:18:01 +0900

    feat: 세션으로 데이터를 추가 또는 조회하는 기능 추가

--- src/main/java/webserver/http/HttpSession.java
+++ src/main/java/webserver/http/HttpSession.java
@@ -1,8 +1,12 @@
 package webserver.http;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpSession {
 
     private final String id;
+    private final Map<String, Object> attributes = new HashMap<>();
 
     public HttpSession(String id) {
         this.id = id;
@@ -10,5 +14,13 @@ public class HttpSession {
 
     public String getId() {
         return id;
+    }
+
+    public void setAttribute(String name, Object value) {
+        attributes.put(name, value);
+    }
+
+    public Object getAttribute(String name) {
+        return attributes.get(name);
     }
 }
--- src/test/java/webserver/http/HttpSessionTest.java
+++ src/test/java/webserver/http/HttpSessionTest.java
@@ -16,4 +16,13 @@ class HttpSessionTest {
         HttpSession session = new HttpSession(id);
         assertThat(session.getId()).isEqualTo(id);
     }
+
+    @DisplayName("세션에 데이터를 추가하거나, 세션에서 데이터를 조회할 수 있다.")
+    @Test
+    void attribute() {
+        String id = UUID.randomUUID().toString();
+        HttpSession session = new HttpSession(id);
+        session.setAttribute("name", "홍길동");
+        assertThat(session.getAttribute("name")).isEqualTo("홍길동");
+    }
 }
