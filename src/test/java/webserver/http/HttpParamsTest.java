f872c7c2acb9b6edf897c089e5d4416acd90b3cf
sang5c <skah321@gmail.com>
2022-07-28 23:56:03 +0900

    test: DisplayName 추가

--- src/test/java/webserver/http/HttpParamsTest.java
+++ src/test/java/webserver/http/HttpParamsTest.java
@@ -1,5 +1,6 @@
 package webserver.http;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,11 +8,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class HttpParamsTest {
 
+    @DisplayName("문자열을 받아 HttpParam을 생성한다")
     @Test
     void create() {
         assertThat(new HttpParams("key=value&key2=value2")).isNotNull();
     }
 
+    @DisplayName("잘못된 형식의 문자열의 경우 Exception이 발생한다")
     @Test
     void invalidQuerystring() {
         assertThatThrownBy(() -> new HttpParams("key=valuekey2=value2"))
