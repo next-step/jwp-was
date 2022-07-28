76d57075ab3852f00d0f42b1031b8627500e5880
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-29 01:13:22 +0900

    refactor: Cookies 객체 적용

--- src/main/java/webserver/controller/UserLoginController.java
+++ src/main/java/webserver/controller/UserLoginController.java
@@ -2,6 +2,7 @@ package webserver.controller;
 
 import webserver.exception.InvalidPasswordException;
 import webserver.exception.NotFoundUserException;
+import webserver.http.HttpCookies;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
 import webserver.service.UserService;
@@ -28,10 +29,10 @@ public class UserLoginController extends AbstractController {
 		try {
 			userService.login(httpRequest.getHttpBody());
 
-			httpResponse.addHeader("SET-COOKIE", "logined=true; Path=/");
+			httpResponse.addHeader("SET-COOKIE", HttpCookies.of("logined=true; Path=/").getFlatCookies());
 			httpResponse.sendRedirect("/index.html");
 		} catch (InvalidPasswordException | NotFoundUserException e) {
-			httpResponse.addHeader("SET-COOKIE", "logined=false; Path=/");
+			httpResponse.addHeader("SET-COOKIE", HttpCookies.of("logined=false; Path=/").getFlatCookies());
 			httpResponse.sendRedirect("/user/login_failed.html");
 		}
 	}
