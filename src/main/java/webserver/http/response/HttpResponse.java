7e4690482141a4353b3d066441434237069e2f44
donghui.lee <donghui.lee@nhnsoft.com>
2022-07-29 00:47:59 +0900

    refactor: ResponseBody 객체 제거

--- src/main/java/webserver/http/response/HttpResponse.java
+++ src/main/java/webserver/http/response/HttpResponse.java
@@ -1,5 +1,6 @@
 package webserver.http.response;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
@@ -9,11 +10,11 @@ import webserver.http.request.HttpProtocol;
 public class HttpResponse {
 	private final String TEMPLATES_ROOT_PATH = "./templates";
 	private final String STATIC_ROOT_PATH = "./static";
+	private final String NEW_LINE = "\r\n";
 
 	private final OutputStream outputStream;
 	private final HttpResponseHeaders headers;
 	private final HttpStatusLine statusLine;
-	private HttpResponseBody responseBody;
 
 	public HttpResponse(HttpProtocol httpProtocol, OutputStream outputStream) {
 		this.outputStream = outputStream;
@@ -32,22 +33,10 @@ public class HttpResponse {
 		headers.addAttribute(attribute, value);
 	}
 
-	private void write(HttpStatus status) {
-		try {
-			statusLine.write(status, outputStream);
-			headers.write(outputStream);
-			responseBody.write(outputStream);
-			outputStream.flush();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	public void forward(String uri) {
 		Resource resource = Resource.of(getRootPath(uri));
 		response200Header(resource.getContentType(), resource.getContentLength());
-		responseBody = new HttpResponseBody(resource);
-		write(HttpStatus.OK);
+		write(HttpStatus.OK, resource.getResource());
 	}
 
 	public void sendRedirect(String uri) {
@@ -56,13 +45,42 @@ public class HttpResponse {
 	}
 
 	public void forwardBody(ContentType contentType, String body) {
-		response200Header(contentType, body.getBytes(StandardCharsets.UTF_8).length);
-		responseBody = new HttpResponseBody(body);
-		write(HttpStatus.OK);
+		byte[] responseBody = body.getBytes(StandardCharsets.UTF_8);
+		response200Header(contentType, responseBody.length);
+		write(HttpStatus.OK, responseBody);
 	}
 
 	public void response200Header(ContentType contentType, int contentLength) {
 		headers.addContentType(contentType);
 		headers.addContentLength(contentLength);
+	}
+
+	private void write(HttpStatus status, byte[] responseBody) {
+		try {
+			writeHeader(status);
+			writeBody(responseBody);
+			outputStream.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void write(HttpStatus status) {
+		try {
+			writeHeader(status);
+			outputStream.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void writeHeader(HttpStatus status) throws IOException {
+		statusLine.write(status, outputStream);
+		headers.write(outputStream);
+	}
+
+	private void writeBody(byte[] responseBody) throws IOException {
+		outputStream.write(NEW_LINE.getBytes(StandardCharsets.UTF_8));
+		outputStream.write(responseBody);
 	}
 }
