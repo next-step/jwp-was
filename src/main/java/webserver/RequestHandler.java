94eae2e4752a01eec0b91d132662ea485e2d3b57
wjdxor <wjdxortaek@gmail.com>
2022-07-28 16:33:28 +0900

    feat: 기능 요구사항 6 - 로그인 상태 확인 후 list출력(handlebars template 활용)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,8 +4,14 @@ import java.io.*;
 import java.net.Socket;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
 import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
@@ -34,11 +40,15 @@ public class RequestHandler implements Runnable {
             String url = RLParser.getUri().getPath();
             // Request Header
             int contentLength = 0;
+            boolean logined = false;
             while (!line.equals("")) {
                 line = br.readLine();
                 logger.debug("header: {}", line);
                 if (line.contains("Content-Length")){
                     contentLength = getContentLength(line);
+                }
+                if (line.contains("Cookie")) {
+                    logined = isLogin(line);
                 }
             }
 
@@ -56,7 +66,6 @@ public class RequestHandler implements Runnable {
                 Map<String, String> params = QSParser.getQueryParameters();
                 User user = DataBase.findUserById(params.get("userId"));
                 DataOutputStream dos = new DataOutputStream(out);
-                response302Header(dos, "/index.html");
                 if (user != null) {
                     if (user.getPassword().equals(params.get("password"))) {
                         response302LoginHeader(dos, "logined=true", "/index.html");
@@ -66,6 +75,19 @@ public class RequestHandler implements Runnable {
                 } else {
                     response302LoginHeader(dos,"logined=false","/user/login_failed.html");
                 }
+            } else if ("/user/list".equals(url)) {
+                if (!logined) {
+                    DataOutputStream dos = new DataOutputStream(out);
+                    byte[] body = FileIoUtils.loadFileFromClasspath("./templates/user/login.html");
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                }
+                Collection<User> users = DataBase.findAll();
+                String userList = HandleBarTemplateLoader.load("user/list",users);
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = userList.getBytes(StandardCharsets.UTF_8);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             } else {
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] body = FileIoUtils.loadFileFromClasspath("./templates" + url);
@@ -75,6 +97,24 @@ public class RequestHandler implements Runnable {
         } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private boolean isLogin(String line) {
+        String cookieString = line.split(":")[1].trim();
+        if (cookieString.isEmpty()) {
+            return false;
+        }
+        Map<String, String> cookies = new HashMap<>();
+        for (String cookie : cookieString.split(";")) {
+            String cookieName = cookie.split("=")[0];
+            String cookieValue = cookie.split("=")[1];
+            cookies.put(cookieName, cookieValue);
+        }
+        String loginedValue = cookies.get("logined");
+        if (loginedValue == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(loginedValue);
     }
 
     private int getContentLength(String line) {
