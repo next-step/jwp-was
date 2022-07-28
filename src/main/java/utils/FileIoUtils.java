0987fe7d1e29046df2c12e562a777389880eb236
HyungjuJang <dacapolife87@gmail.com>
2022-07-28 22:04:00 +0900

    feat : 리소스를 찾을수 없을때 처리 추가

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -1,14 +1,21 @@
 package utils;
 
+import webserver.http.exception.NotFoundException;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileIoUtils {
-    public static byte[] loadFileFromClasspath(String filePath) throws IOException, URISyntaxException {
-        Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
+    public static byte[] loadFileFromClasspath(String filePath) throws URISyntaxException, IOException {
+        URL resource = FileIoUtils.class.getClassLoader().getResource(filePath);
+        if (resource == null) {
+            throw new NotFoundException("페이지를 찾을수 없습니다.");
+        }
+        Path path = Paths.get(resource.toURI());
         return Files.readAllBytes(path);
     }
 }
