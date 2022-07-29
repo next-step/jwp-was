1ef2619f7ce8b9860da20b2d540f69cc2125eb32
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-07-29 23:41:25 +0900

    feat: 예외처리 객체 생성

---
+++ src/main/java/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package exception;
+
+import webserver.response.HttpStatusCode;
+
+public class NotFoundException extends RuntimeException {
+
+    private final String message = "해당 값을 찾지 못했습니다.";
+
+    public NotFoundException(HttpStatusCode statusCode) {
+        super(statusCode.toString());
+    }
+}
