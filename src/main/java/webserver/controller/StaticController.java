f47877af9605d70616d4389ede7309882ff7ac09
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-07-30 01:20:34 +0900

    feat: style sheet 처리 Controller 구현

---
+++ src/main/java/webserver/controller/StaticController.java
@@ -0,0 +1,39 @@
+package webserver.controller;
+
+import http.request.Protocol;
+import utils.FileIoUtils;
+import webserver.HttpHeader;
+import webserver.request.HttpRequest;
+import webserver.response.HttpResponse;
+import webserver.response.HttpStatusCode;
+import webserver.response.StatusLine;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collections;
+
+public class StaticController implements Controller {
+
+    private final static String STATIC_PATH = "./static";
+    private final static String CONTENT_TYPE = "Content-Type";
+
+    @Override
+    public HttpResponse service(HttpRequest request) throws IOException, URISyntaxException {
+        return HttpResponse.of(
+                StatusLine.of(Protocol.from("HTTP/1.1"), HttpStatusCode.OK),
+                HttpHeader.from(Collections.singletonMap(
+                        CONTENT_TYPE, String.format("text/%s;charset=utf-8", fileExtension(request))
+                )),
+                FileIoUtils.loadFileFromClasspath(staticPath(request))
+        );
+    }
+
+    private String fileExtension(HttpRequest request) {
+        String path = request.getPath();
+        return path.substring(request.getPath().lastIndexOf('.') + 1);
+    }
+
+    private String staticPath(HttpRequest request) {
+        return STATIC_PATH + request.getPath();
+    }
+}
