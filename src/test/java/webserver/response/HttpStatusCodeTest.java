7a136d4b55a319b47137891f30968c29b7c0d604
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-07-30 01:20:54 +0900

    feat: Http 상태 코드 열거

---
+++ src/test/java/webserver/response/HttpStatusCodeTest.java
@@ -0,0 +1,20 @@
+package webserver.response;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("Http 응답 코드 테스트")
+class HttpStatusCodeTest {
+
+    private final static String REGEX = "\\d+ [a-zA-Z]+";
+
+    @DisplayName("응답 코드는 숫자, 문자 형태")
+    @ParameterizedTest
+    @EnumSource(HttpStatusCode.class)
+    void code(HttpStatusCode statusCode) {
+        assertThat(statusCode.toString()).containsPattern(REGEX);
+    }
+}
