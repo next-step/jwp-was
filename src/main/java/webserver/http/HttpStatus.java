0446b42cd46966b222ac717d21d218b7d8b7ff3c
HyungjuJang <dacapolife87@gmail.com>
2022-07-30 13:04:43 +0900

    refactor : 오버라이드 어노테이션 추가

--- src/main/java/webserver/http/HttpStatus.java
+++ src/main/java/webserver/http/HttpStatus.java
@@ -16,6 +16,7 @@ public enum HttpStatus {
         this.statusMessage = statusMessage;
     }
 
+    @Override
     public String toString() {
         return statusCode + " " + statusMessage;
     }
