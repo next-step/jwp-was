0bf60001fa4fd41a676229f6a1c28932b859945c
mintjordy <mintjordy@woowahan.com>
2022-08-01 01:29:58 +0900

    feat: 요청 처리중 예외 발생시, 400 || 500 에러 응답 반환

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import webserver.http.domain.controller.RequestProcessor;
+import webserver.http.domain.exception.BadRequestException;
 import webserver.http.domain.exception.NullRequestException;
 import webserver.http.domain.request.Request;
 import webserver.http.domain.response.Response;
+import webserver.http.domain.response.StatusCode;
 import webserver.http.view.request.RequestReader;
 import webserver.http.view.response.ResponseWriter;
 
@@ -42,15 +44,33 @@ public class RequestHandler implements Runnable {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charsets.UTF_8));
              DataOutputStream dos = new DataOutputStream(connection.getOutputStream())
         ) {
-            Request request = requestReader.read(bufferedReader);
-            logger.info("[request] = {}", request);
-
-            Response response = requestProcessor.process(request);
+            Response response = processRequest(bufferedReader);
             responseWriter.write(dos, response);
         } catch (NullRequestException e) {
             logger.warn(e.getMessage(), e);
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
+    }
+
+    private Response processRequest(BufferedReader bufferedReader) throws IOException {
+        try {
+            Request request = requestReader.read(bufferedReader);
+            logger.info("[request] = {}", request);
+            return requestProcessor.process(request);
+        } catch (BadRequestException e) {
+            logger.warn("[bad request] = {}", e.getMessage(), e);
+            return getResponse(StatusCode.BAD_REQUEST, "잘못된 요청입니다. ;(");
+        } catch (RuntimeException e) {
+            logger.error("[internal error] - 요청값 처리중 에러 발생 = {}", e.getMessage(), e);
+            return getResponse(StatusCode.INTERNAL_ERROR, "서버 내부에 오류가 발생했습니다. ;(");
+        }
+    }
+
+    private Response getResponse(StatusCode statusCode, String text) {
+        Response resourceNotFound = Response.from(statusCode);
+        resourceNotFound.addHeader("Content-Type", "text/html");
+        resourceNotFound.addBody("<h1><meta charset=\"UTF-8\">" + text + "</h1>");
+        return resourceNotFound;
     }
 }
--- src/main/java/webserver/http/domain/controller/RequestProcessor.java
+++ src/main/java/webserver/http/domain/controller/RequestProcessor.java
@@ -1,5 +1,7 @@
 package webserver.http.domain.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.http.domain.exception.ResourceNotFoundException;
 import webserver.http.domain.request.Request;
 import webserver.http.domain.response.Response;
@@ -8,6 +10,8 @@ import webserver.http.domain.response.StatusCode;
 import java.util.List;
 
 public class RequestProcessor {
+    private static final Logger logger = LoggerFactory.getLogger(RequestProcessor.class);
+
     private final List<Controller> controllers;
 
     public RequestProcessor(List<Controller> controllers) {
@@ -23,15 +27,17 @@ public class RequestProcessor {
 
             return handler.handle(request);
         } catch (ResourceNotFoundException e) {
-            Response resourceNotFound = Response.from(StatusCode.NOT_FOUND);
-            resourceNotFound.addHeader("Content-Type", "text/html");
-            resourceNotFound.addBody("<h1><meta charset=\"UTF-8\">요청하신 리소스를 찾지 못했습니다. ;(</h1>");
-            return resourceNotFound;
+            return getResponse(StatusCode.NOT_FOUND, "요청하신 리소스를 찾지 못했습니다. ;(");
         } catch (RuntimeException e) {
-            Response resourceNotFound = Response.from(StatusCode.INTERNAL_ERROR);
-            resourceNotFound.addHeader("Content-Type", "text/html");
-            resourceNotFound.addBody("<h1><meta charset=\"UTF-8\">서버 내부에 오류가 발생했습니다. ;(</h1>");
-            return resourceNotFound;
+            logger.error("[internal error] - 요청값 처리중 에러 발생 = {}", e.getMessage(), e);
+            return getResponse(StatusCode.INTERNAL_ERROR, "서버 내부에 오류가 발생했습니다. ;(");
         }
+    }
+
+    private Response getResponse(StatusCode statusCode, String text) {
+        Response resourceNotFound = Response.from(statusCode);
+        resourceNotFound.addHeader("Content-Type", "text/html");
+        resourceNotFound.addBody("<h1><meta charset=\"UTF-8\">" + text + "</h1>");
+        return resourceNotFound;
     }
 }
