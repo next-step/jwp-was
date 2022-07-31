ebc7be0bae6ab710f1d9b84bd747ba7d659526f2
miot2j <ck96117@gmail.com>
2022-08-01 01:28:31 +0900

    ♻️ refactor: HttpRequstHeader에 HttpBody 필드 추가 및 Method에 따른 분기 처리 구현

--- src/test/java/utils/parser/HttpRequestHeaderParserTest.java
+++ src/test/java/utils/parser/HttpRequestHeaderParserTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.Map;
 
+import static dummy.HttpRequestHeaderDummy.HTTP_POST_REQUEST_HEADER_STRING_DUMMY;
 import static dummy.HttpRequestHeaderDummy.HTTP_REQUEST_HEADER_STRING_DUMMY;
 import static enums.HttpMethod.GET;
+import static enums.HttpMethod.POST;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -27,6 +30,31 @@ public class HttpRequestHeaderParserTest {
             () -> assertThat(result.getRequestLine().getHttpMethod()).isEqualTo(GET),
             () -> assertThat(result.getPath()).isEqualTo("/jason/test/"),
             () -> assertThat(result.getRequestLine().getWebProtocol().getType()).isEqualTo("HTTP")
+        );
+    }
+
+    @Test
+    @DisplayName("POST 형식의 InputSteam이 입력으로 주어지면 파싱이 완료된 HttpRequestHeader 객체를 반환한다.")
+    void parsePostHttpRequestHeaderTest() throws IOException {
+        String httpPostRequestHeaderStringDummy = HTTP_POST_REQUEST_HEADER_STRING_DUMMY;
+        Map<String, String> filedNameToValue = Map.of(
+            "userId", "javajigi",
+            "password", "password",
+            "name", "박재성",
+            "email", "javajigi@slipp.net"
+        );
+
+        InputStream inputStream = new ByteArrayInputStream(httpPostRequestHeaderStringDummy.getBytes());
+
+        HttpRequestHeader result = HttpRequestHeaderParser.parseHttpRequestHeaderParser(inputStream);
+
+        assertAll(
+            () -> assertThat(result.getClass()).isEqualTo(HttpRequestHeader.class),
+            () -> assertThat(result.getHttpHeader().getValueByKey("Host")).isEqualTo("localhost:8080"),
+            () -> assertThat(result.getRequestLine().getHttpMethod()).isEqualTo(POST),
+            () -> assertThat(result.getPath()).isEqualTo("/user/create"),
+            () -> assertThat(result.getRequestLine().getWebProtocol().getType()).isEqualTo("HTTP"),
+            () -> assertThat(result.getRequestBody()).isEqualTo(filedNameToValue)
         );
     }
 }
