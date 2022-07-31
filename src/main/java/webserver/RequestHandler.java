6c93ab3d47243e537b27bbc56cdbea7224e015a7
KimYongKwon <dydrnjs518@gmail.com>
2022-08-01 01:06:20 +0900

    feat - 기능 요구사항 4 (redirect 구현)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.IOUtils;
 import webserver.request.domain.model.User;
-import webserver.request.domain.request.HttpRequest;
-import webserver.request.domain.request.QueryString;
-import webserver.request.domain.request.RequestBody;
-import webserver.request.domain.request.RequestLine;
+import webserver.request.domain.request.*;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,6 +20,7 @@ public class RequestHandler implements Runnable {
     private User user;
 
     private HttpRequest httpRequest;
+    private HttpResponse httpResponse;
 //    private RequestLine requestLine;
 
     public RequestHandler(Socket connectionSocket) {
@@ -36,15 +34,13 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-            if(in != null) {
+            if (in != null) {
                 httpRequest = new HttpRequest(in);
 
                 DataOutputStream dos = new DataOutputStream(out);
 
-                String path = matchResponse(httpRequest);
-                byte[] body = path.getBytes();
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+                httpResponse = new HttpResponse(dos);
+                matchResponse(httpRequest);
             }
 
         } catch (IOException | URISyntaxException e) {
@@ -52,35 +48,32 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private String matchResponse(HttpRequest httpRequest) throws IOException, URISyntaxException {
+    private void matchResponse(HttpRequest httpRequest) throws IOException, URISyntaxException {
+        String path = httpRequest.getPath();
         String method = httpRequest.getMethod();
 
-        if(method.equals("GET")) {
-            String path = httpRequest.getPath();
+        if (path.equals("/user/create")) {
+            if (method.equals("GET")) {
+                QueryString queryString = httpRequest.getQueryString();
+                httpResponse.redirect(parseQueryString(path, queryString));
+            } else {
+                RequestBody requestBody = httpRequest.getBody();
+                httpResponse.redirect(parseBody(path, requestBody));
+            }
+        } else {
             if (path.endsWith("html")) {
-                return IOUtils.loadFileFromClasspath(path);
+                httpResponse.forward(IOUtils.loadFileFromClasspath(path));
             }
-
-            if (httpRequest.getQueryString() != null) {
-                return IOUtils.loadFileFromClasspath(parseQueryString(path, httpRequest.getQueryString()));
-            }
-            else
-                return IOUtils.loadFileFromClasspath("/index.html");
         }
-
-        if(method.equals("POST")) {
-            String path = httpRequest.getPath();
-            return IOUtils.loadFileFromClasspath(parseBody(path, httpRequest.getBody()));
-        }
-
-        return "";
     }
 
     private String parseQueryString(String parsePath, QueryString queryString) {
-        String[] str = parsePath.split("/");
-        if (str.length > 2) {
-            if (str[1].equals("user")) {
-                userController(queryString.getDataPairs(), str);
+        if (queryString != null) {
+            String[] str = parsePath.split("/");
+            if (str.length > 2) {
+                if (str[1].equals("user")) {
+                    userController(queryString.getDataPairs(), str);
+                }
             }
         }
 
@@ -89,57 +82,18 @@ public class RequestHandler implements Runnable {
 
     private String parseBody(String parsePath, RequestBody requestBody) {
         String[] str = parsePath.split("/");
-        if(str[1].equals("user")) {
-            userController(requestBody.getDataPairs(), str);
+        if (str.length > 2) {
+            if (str[1].equals("user")) {
+                userController(requestBody.getDataPairs(), str);
+            }
         }
 
         return "/index.html";
     }
 
     private void userController(Map<String, String> map, String[] str) {
-        if(str[2].equals("create")) {
+        if (str[2].equals("create")) {
             this.user = User.create(map);
-        }
-    }
-
-//    private void showRequest(InputStream is) {
-//        try {
-//            BufferedReader br = new BufferedReader(new InputStreamReader(is, Charsets.UTF_8));
-//            String line = br.readLine();
-//            RequestLine requestLine = RequestLine.parse(line);
-////            httpRequest.addProperty(line);
-//            logger.debug(requestLine.toString());
-//
-//            StringBuilder stringBuilder = new StringBuilder();
-//            while(!line.equals("")) {
-//                line = br.readLine();
-////                httpRequest.addProperty(line);
-//                stringBuilder.append(line + "\n");
-//            }
-//
-//            logger.debug(String.valueOf(stringBuilder));
-//        } catch (IOException e) {
-//            logger.error(e.getMessage());
-//        }
-//    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
         }
     }
 }
---
+++ src/main/java/webserver/request/domain/request/HttpResponse.java
@@ -0,0 +1,36 @@
+package webserver.request.domain.request;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class HttpResponse {
+
+    private DataOutputStream dos;
+
+    public HttpResponse(OutputStream outputStream) {
+        dos = new DataOutputStream(outputStream);
+    }
+
+    public void forward(String path) throws IOException {
+        int lengthOfBodyContent = path.length();
+
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+
+        responseBody(path.getBytes());
+    }
+
+    public void redirect(String path) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Location: " + path + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    private void responseBody(byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
+    }
+}
