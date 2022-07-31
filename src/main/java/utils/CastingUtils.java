1f363601bd881e300fabaf1337b94bac3cc15240
Jeongsu Yang <mycat83@gmail.com>
2022-07-31 21:58:44 +0900

    feat: 형변환 유틸 추가

---
+++ src/main/java/utils/CastingUtils.java
@@ -0,0 +1,32 @@
+package utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CastingUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CastingUtils.class);
+
+    private CastingUtils() {
+        throw new AssertionError();
+    }
+
+    public static <T> T cast(Object obj, Class<T> returnType) {
+        if (returnType.isInstance(obj)) {
+            return returnType.cast(obj);
+        }
+
+        return castStringToReturnType((String) obj, returnType);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T castStringToReturnType(String obj, Class<T> returnType) {
+        try {
+            if (returnType == Boolean.class) {
+                return (T) Boolean.valueOf(obj);
+            }
+        } catch(ClassCastException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+}
