866e5302014ce6d1b9b06cec1cc7ab79c79d01ae
skid901 <tlrudejr1993@gmail.com>
2022-08-03 00:31:12 +0900

    refactor: change server to accept http requests

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,16 @@
 package webserver;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
-import webserver.http.request.HttpHeaders;
-import webserver.http.request.Path;
-import webserver.http.request.Queries;
-import webserver.http.request.RequestLine;
+import utils.IOUtils;
+import webserver.http.request.*;
 
 import java.io.*;
 import java.net.Socket;
@@ -18,6 +21,14 @@ import java.util.Objects;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Handlebars handlebars;
+
+    static {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        handlebars = new Handlebars(loader);
+    }
 
     private Socket connection;
 
@@ -30,31 +41,68 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            RequestLine requestLine = RequestLine.from(br.readLine());
-
-            List<String> headers = new ArrayList<>();
+            String requestLineString = br.readLine();
+            List<String> headerLineStrings = new ArrayList<>();
             String nextLine = br.readLine();
             while (!Objects.isNull(nextLine) && !"".equals(nextLine)) {
-                headers.add(nextLine);
+                headerLineStrings.add(nextLine);
                 nextLine = br.readLine();
             }
-            HttpHeaders httpHeaders = HttpHeaders.from(String.join("\n", headers));
+            String headerLineString = String.join("\n", headerLineStrings);
+
+            RequestLine requestLine = RequestLine.from(requestLineString);
+            HttpMethod method = requestLine.getMethod();
+            Path path = requestLine.getPath();
+            HttpHeaders httpHeaders = HttpHeaders.from(headerLineString);
+            Cookie cookie = Cookie.from(httpHeaders.get(Cookie.REQUEST_COOKIE_HEADER));
+
+            String requestBody = "";
+            if (HttpMethod.POST.equals(method)) {
+                int contentLength = Integer.parseInt(httpHeaders.get("Content-Length"));
+                requestBody = IOUtils.readData(br, contentLength);
+            }
+
+            Queries queries = HttpMethod.GET.equals(method) ? path.getQueries() : Queries.from(requestBody);
 
             DataOutputStream dos = new DataOutputStream(out);
-            Path path = requestLine.getPath();
-            Queries queries = path.getQueries();
             String pathString = path.toString();
-            byte[] body;
             switch (pathString) {
                 case "user/create":
                     User user = new User(queries.get("userId").get(), queries.get("password").get(), queries.get("name").get(), queries.get("email").get());
-                    body = user.toString().getBytes();
+                    DataBase.addUser(user);
+
+                    response302Header(dos, "index.html");
+                    break;
+                case "user/login":
+                    User loginUser = DataBase.findUserById(queries.get("userId").get());
+                    boolean isLogin = !Objects.isNull(loginUser) && loginUser.checkPassword(queries.get("password").get());
+                    cookie.set("logined", String.valueOf(isLogin));
+
+                    response302Header(dos, isLogin ? "index.html" : "user/login_failed.html");
+                    responseHeader(dos, Cookie.RESPONSE_COOKIE_HEADER, cookie.getResponseCookie());
+                    break;
+                case "user/list":
+                    boolean logined = Boolean.parseBoolean(cookie.get("logined"));
+                    if (!logined) {
+                        response302Header(dos, "user/login.html");
+                        break;
+                    }
+
+                    List<User> users = new ArrayList<>(DataBase.findAll());
+                    Template template = handlebars.compile("user/list");
+                    byte[] listPage = template.apply(users).getBytes();
+
+                    response200Header(dos, listPage.length);
+                    responseBody(dos, listPage);
                     break;
                 default:
-                    body = FileIoUtils.loadFileFromClasspath("./templates/" + (!"".equals(pathString) ? pathString : "index.html"));
+                    pathString = !"".equals(pathString) ? pathString : "index.html";
+                    String prefix = (pathString.contains(".html") || pathString.contains(".ico")) ? "templates" : "static";
+                    byte[] responseBody = FileIoUtils.loadFileFromClasspath("./" + prefix + "/" + pathString);
+
+                    response200Header(dos, responseBody.length);
+                    responseBody(dos, responseBody);
             }
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
@@ -63,8 +111,8 @@ public class RequestHandler implements Runnable {
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            responseHeader(dos, "Content-Type", "text/html;charset=utf-8");
+            responseHeader(dos, "Content-Length", String.valueOf(lengthOfBodyContent));
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
@@ -75,6 +123,23 @@ public class RequestHandler implements Runnable {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            responseHeader(dos, "Location", Path.PATH_DELIMITER + location);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void responseHeader(DataOutputStream dos, String key, String value) {
+        try {
+            dos.writeBytes(key + ": " + value + "\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
