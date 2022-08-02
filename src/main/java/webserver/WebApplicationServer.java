6c83ed4f1814fdd14871a54ea4f87cda18ee9c4e
황정현 <jhhwang@wemakeprice.com>
2022-08-02 15:09:02 +0900

    [feat] thread pool 구현

--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -2,6 +2,10 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +13,10 @@ import org.slf4j.LoggerFactory;
 public class WebApplicationServer {
     private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int CORE_POOL_SIZE = 200;
+    private static final int MAX_POOL_SIZE = 200;
+    private static final long KEEP_ALIVE_TIME = 0L;
+    private static final int QUEUE_CAPACITY = 100;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -18,6 +26,8 @@ public class WebApplicationServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ExecutorService executorService = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(QUEUE_CAPACITY));
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -25,8 +35,9 @@ public class WebApplicationServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executorService.execute(new RequestHandler(connection));
+//                Thread thread = new Thread();
+//                thread.start();
             }
         }
     }
--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,10 +1,13 @@
 package webserver;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import webserver.response.FileResponse;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +15,19 @@ public class HttpRequestTest {
     @Test
     void request_resttemplate() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/index.html", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    @DisplayName("스레드 수가 서버의 MAX_THREAD_SIZE를 넘어간다")
+    void request_over_thread() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        IntStream.range(1, 250)
+                .forEach(n -> {
+                    ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/index.html", String.class);
+                    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+                });
     }
 }
