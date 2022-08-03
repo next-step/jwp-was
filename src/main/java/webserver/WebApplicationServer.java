7c75ae8ca4c37abb8ade86e8d686a682d408dcc8
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-03 14:25:27 +0900

    feat: 요구사항 2번 구현 - 최대 Thread Pool 수 5 - 동시 10개 요청 발생

--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -18,24 +18,28 @@ public class WebApplicationServer {
     private static final int QUEUE_SIZE = 100;
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
+        int port = DEFAULT_PORT;
+        int minCorPoolSize = MIN_CORE_POOL_SIZE;
+        int maxCorPoolSize = MAX_CORE_POOL_SIZE;
+        int queueSize = QUEUE_SIZE;
+
+        if (args != null || args.length == 4) {
             port = Integer.parseInt(args[0]);
+            minCorPoolSize = Integer.parseInt(args[1]);
+            maxCorPoolSize = Integer.parseInt(args[1]);
+            queueSize = Integer.parseInt(args[2]);
         }
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
-            ThreadPoolExecutor executor = new ThreadPoolExecutor(MIN_CORE_POOL_SIZE,
-                                                                 MAX_CORE_POOL_SIZE,
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(minCorPoolSize,
+                                                                 maxCorPoolSize,
                                                                  KEEP_ALIVE_TIME,
                                                                  TimeUnit.MILLISECONDS,
-                                                                 new ArrayBlockingQueue<>(QUEUE_SIZE));
+                                                                 new ArrayBlockingQueue<>(queueSize));
             while ((connection = listenSocket.accept()) != null) {
                 executor.execute(new RequestHandler(connection));
             }
