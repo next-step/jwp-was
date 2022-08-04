44c12695c74bfb2659d8937eab92b261f36b3d48
itdar <thesockerr@gmail.com>
2022-08-05 03:05:03 +0900

    feat: 요구사항 5단계 테스팅
    - 쓰레드풀 스펙 정의 후 오버되는 요청 수 병렬 요청
    	-> 실패 발생 확인 (풀 사이즈 및 대기 큐 사이즈에 따라서 errorCount 변화)

--- src/main/java/webserver/ThreadPool.java
+++ src/main/java/webserver/ThreadPool.java
@@ -1,21 +1,32 @@
 package webserver;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ThreadPool {
-    public static final int CORE_POOL_SIZE = 250;
-    public static final int MAX_POOL_SIZE = 250;
-    public static final int MAX_WORK_QUEUE_SIZE = 100;
-    public static ThreadPoolExecutor tpe = new ScheduledThreadPoolExecutor(CORE_POOL_SIZE);
+
+    private static final Logger logger = LoggerFactory.getLogger(ThreadPool.class);
+
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAX_POOL_SIZE = 10;
+    private static final int MAX_WORK_QUEUE_SIZE = 10;
+    private static final long KEEP_ALIVE_TIME = 0L;
+    public static ThreadPoolExecutor tpe;
     static {
-        tpe.setMaximumPoolSize(MAX_POOL_SIZE);
+        tpe = new ThreadPoolExecutor(
+            CORE_POOL_SIZE,
+            MAX_POOL_SIZE,
+            KEEP_ALIVE_TIME,
+            TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<>(MAX_WORK_QUEUE_SIZE));
     }
 
     public static void execute(Runnable runnable) {
-        if (MAX_WORK_QUEUE_SIZE <= tpe.getQueue().size()) {
-            return;
-        }
         tpe.execute(runnable);
     }
 
--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -14,10 +14,8 @@ public class WebApplicationServer {
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
+        int port = DEFAULT_PORT;
+        if (args != null && args.length != 0) {
             port = Integer.parseInt(args[0]);
         }
 
