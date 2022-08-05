e87739155f8c5b9bb5b747f95a983e0688b9b376
wu2ee <gulb1602@gmail.com>
2022-08-05 23:57:35 +0900

    refactor : 요청 분기 맵핑 로직 추가 & RequestHandler 리팩토링
    - RequestMapping 객체 생성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,9 @@
 package webserver;
 
-import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
-import com.github.jknack.handlebars.io.TemplateLoader;
-import db.DataBase;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.FileIoUtils;
 import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -18,9 +12,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.URISyntaxException;
-import java.util.Collection;
-import java.util.Map;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -37,161 +28,20 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            HttpRequest httpRequest = HttpRequest.of(br);
-            byte[] body = {};
-            boolean isLogin = false;
-
-            if (httpRequest.isLogin()) {
-                isLogin = true;
-            }
-
-            if (httpRequest.getPath().equals("/user/list")) {
-                DataOutputStream dos = new DataOutputStream(out);
-                TemplateLoader loader = new ClassPathTemplateLoader();
-                loader.setPrefix("/templates");
-                loader.setSuffix(".html");
-                Handlebars handlebars = new Handlebars(loader);
-
-                if (isLogin) {
-                    Template template = handlebars.compile("user/list");
-                    Collection<User> users = DataBase.findAll();
-                    body = template.apply(Map.of("users", users)).getBytes();
-                    response200Header(dos, body.length);
-                    responseBody(dos, body);
-                    return;
-                }
-                responseIsNotLoginHeader(dos);
-                return;
-            }
-
-            if (httpRequest.getPath().endsWith(".css")) {
-                DataOutputStream dos = new DataOutputStream(out);
-                body = FileIoUtils.loadFileFromClasspath("./static" + httpRequest.getPath());
-                response200CssHeader(dos, body.length);
-                responseBody(dos, body);
-            }
-
-            if (httpRequest.getPath().equals("/user/create")) {
-                User user = new User(
-                        httpRequest.getParam("userId"),
-                        httpRequest.getParam("password"),
-                        httpRequest.getParam("name"),
-                        httpRequest.getParam("email")
-                );
-                DataBase.addUser(user);
-                DataOutputStream dos = new DataOutputStream(out);
-                response302Header(dos);
-                responseBody(dos, body);
-                return;
-            }
-
-            if (httpRequest.getPath().equals("/user/login")) {
-                User user = DataBase.findUserById(httpRequest.getParam("userId"));
-                DataOutputStream dos = new DataOutputStream(out);
-
-                if (user == null) {
-                    responseLoginFailHeader(dos);
-                    return;
-                }
-
-                if (user.isPasswordCorrect(httpRequest.getParam("password"))) {
-                    response302LoginSuccessHeader(dos);
-                    return;
-                }
-                responseLoginFailHeader(dos);
-                return;
-            }
-
-            if (httpRequest.isFilePath()) {
-                body = FileIoUtils.loadFileFromClasspath("./templates" + httpRequest.getPath());
-            }
             DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException | URISyntaxException e) {
-            logger.error(e.getMessage());
-        }
-    }
+            HttpRequest httpRequest = HttpRequest.of(br);
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+            HttpResponse httpResponse = RequestMapping.process(httpRequest);
+            response(dos, httpResponse);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response(DataOutputStream dos, HttpResponse httpResponse) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response200LoginFailHeader(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("Set-Cookie: logined=false; Path=/ \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: /index.html \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response302LoginSuccessHeader(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Set-Cookie: logined=true; Path=/ \r\n");
-            dos.writeBytes("Location: /index.html \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseLoginFailHeader(DataOutputStream dos) {
-        try {
-            byte[] body = FileIoUtils.loadFileFromClasspath("./templates/user/login_failed.html");
-            response200LoginFailHeader(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException | URISyntaxException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseIsNotLoginHeader(DataOutputStream dos) {
-        try {
-            byte[] body = FileIoUtils.loadFileFromClasspath("./templates/user/login.html");
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException | URISyntaxException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
+            dos.writeBytes(httpResponse.response());
+            dos.write(httpResponse.getBody(), 0, httpResponse.getContentLength());
             dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
---
+++ src/main/java/webserver/RequestMapping.java
@@ -0,0 +1,55 @@
+package webserver;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import webserver.controller.Controller;
+import webserver.controller.StaticController;
+import webserver.controller.TemplateController;
+import webserver.controller.UserCreateController;
+import webserver.controller.UserListController;
+import webserver.controller.UserLoginController;
+import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class RequestMapping {
+    private static Set<Controller> CONTROLLERS = new HashSet<>();
+
+    static {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        CONTROLLERS.addAll(
+                Set.of(
+                        new StaticController(),
+                        new TemplateController(),
+                        new UserCreateController(),
+                        new UserListController(new Handlebars(loader)),
+                        new UserLoginController()
+                )
+        );
+    }
+
+    public static HttpResponse process(HttpRequest httpRequest) {
+        return CONTROLLERS.stream()
+                .filter(controller -> controller.isMatchRequest(httpRequest))
+                .findAny()
+                .map(controller -> requestProcess(httpRequest, controller))
+                .orElseGet(HttpResponse::notFound);
+    }
+
+    private static HttpResponse requestProcess(HttpRequest httpRequest, Controller controller){
+        try {
+            return controller.process(httpRequest);
+        } catch (IOException | URISyntaxException e) {
+            return HttpResponse.notFound();
+        }
+    }
+}
