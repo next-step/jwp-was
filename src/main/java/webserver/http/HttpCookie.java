9c5dbcd9607abb14c0ef5e0fcd8d61f2fdb82235
wjdxor <wjdxortaek@gmail.com>
2022-08-07 14:28:03 +0900

    refactor: 피드백 수정 - 쿠키클래스 생성

---
+++ src/main/java/webserver/http/HttpCookie.java
@@ -0,0 +1,30 @@
+package webserver.http;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpCookie {
+    Map<String, String> cookies = new HashMap<>();
+
+    public HttpCookie(String cookieString) {
+        if (cookieString != null) {
+            cookieParser(cookieString);
+        }
+    }
+
+    public void cookieParser(String cookieString) {
+        for (String cookie : cookieString.split(";")) {
+            String cookieName = cookie.split("=")[0];
+            String cookieValue = cookie.split("=")[1];
+            cookies.put(cookieName, cookieValue);
+        }
+    }
+
+    public boolean isLogin() {
+        if (cookies.get("logined") == null) {
+            return false;
+        }
+        return cookies.get("logined").equals("true");
+    }
+
+}
