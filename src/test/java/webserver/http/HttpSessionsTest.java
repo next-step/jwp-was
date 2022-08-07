0df59b97283bb119760bd5f7d7e22ef810dd37da
HyungjuJang <dacapolife87@gmail.com>
2022-08-07 15:31:27 +0900

    test : Session 관련 getAttribute, setAttribute, remoteAttribute, invalidate 기능 테스트추가

--- src/test/java/webserver/http/HttpSessionsTest.java
+++ src/test/java/webserver/http/HttpSessionsTest.java
@@ -17,4 +17,40 @@ class HttpSessionsTest {
         assertThat(session).isEqualTo(HttpSessions.getSession(sessionId));
         assertThat(session.getId()).isEqualTo(sessionId);
     }
+
+    @Test
+    void HTTP_세션설정_테스트() {
+        String userId = "hjjang87";
+        String sessionId = UUID.randomUUID().toString();
+
+        HttpSession session = HttpSessions.getSession(sessionId);
+        session.setAttribute("userId", userId);
+
+        assertThat(session.getAttribute("userId")).isEqualTo(userId);
+    }
+
+    @Test
+    void HTTP_세션제거_테스트() {
+        String userId = "hjjang87";
+        String sessionId = UUID.randomUUID().toString();
+
+        HttpSession session = HttpSessions.getSession(sessionId);
+        session.setAttribute("userId", userId);
+
+        session.removeAttribute("userId");
+        assertThat(session.getAttribute("userId")).isNull();
+    }
+
+    @Test
+    void HTTP_세션만료_테스트() {
+        String userId = "hjjang87";
+        String sessionId = UUID.randomUUID().toString();
+
+        HttpSession session = HttpSessions.getSession(sessionId);
+        session.setAttribute("userId", userId);
+        session.invalidate();
+
+        HttpSession invalidateSession = HttpSessions.getSession(sessionId);
+        assertThat(invalidateSession.getAttribute("userId")).isNull();
+    }
 }
