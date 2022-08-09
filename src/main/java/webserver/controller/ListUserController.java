b0a5680b165f67bb731e9671678391666fa39a10
wjdxor <wjdxortaek@gmail.com>
2022-08-10 01:50:02 +0900

    refactor: 피드백 반영 - 세션에 저장된 유저정보를 확인

--- src/main/java/webserver/controller/ListUserController.java
+++ src/main/java/webserver/controller/ListUserController.java
@@ -3,19 +3,20 @@ package webserver.controller;
 import db.DataBase;
 import model.User;
 import utils.HandleBarTemplateLoader;
-import webserver.http.HttpCookie;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 import webserver.http.HttpSession;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Objects;
 
 public class ListUserController extends AbstractController {
     @Override
     public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
         HttpSession session = httpRequest.getSession();
-        if (session == null) {
+        User user = (User) session.getAttribute("user");
+        if (Objects.isNull(user)) {
             httpResponse.sendRedirect("/user/login.html");
             return;
         }
