12bdd905142db9866b7c2bf17950caa85ce4fa00
Jeongsu Yang <mycat83@gmail.com>
2022-08-10 23:49:23 +0900

    feat: 컨트롤러 실행 방식 변경

--- src/main/java/webserver/http/HttpMethod.java
+++ src/main/java/webserver/http/HttpMethod.java
@@ -1,20 +1,5 @@
 package webserver.http;
 
-import webserver.controller.AbstractController;
-
-import java.util.function.BiFunction;
-
 public enum HttpMethod {
-    GET(AbstractController::doGet),
-    POST(AbstractController::doPost);
-
-    private final BiFunction<AbstractController, HttpRequest, HttpResponse> action;
-
-    HttpMethod(BiFunction<AbstractController, HttpRequest, HttpResponse> action) {
-        this.action = action;
-    }
-
-    public BiFunction<AbstractController, HttpRequest, HttpResponse> getAction() {
-        return action;
-    }
+    GET, POST
 }
