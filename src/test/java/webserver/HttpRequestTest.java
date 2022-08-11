d175d8d51c9d79610bd456d2e9ab75de1b5f6093
etff <wish2rich@gmail.com>
2022-08-11 09:24:38 +0900

    feat: step5 요구사항 구현

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -5,6 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestTest {
@@ -13,5 +18,23 @@ public class HttpRequestTest {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void request_resttemplate_with_thread() {
+        ExecutorService executor = Executors.newFixedThreadPool(250);
+        RestTemplate restTemplate = new RestTemplate();
+        IntStream.range(0, 250).forEach(n -> executor.execute(() ->
+                {
+                    try {
+                        restTemplate.getForEntity("http://localhost:8080", String.class);
+
+                        executor.shutdownNow();
+                        executor.awaitTermination(100, TimeUnit.SECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                })
+        );
     }
 }
