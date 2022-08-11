c9ad61090d956853258799987571d55d1ad19d15
etff <wish2rich@gmail.com>
2022-08-11 09:28:00 +0900

    style: 상수값 수정

--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -13,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class WebApplicationServer {
     private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
     private static final int CORE_POOL_SIZE = 250;
+    private static final int MAX_POOL_SIZE = 250;
     private static final int QUEUE_SIZE = 100;
     private static final int KEEP_ALIVE_TIME = 0;
     private static final int DEFAULT_PORT = 8080;
@@ -27,7 +28,7 @@ public class WebApplicationServer {
 
         final ExecutorService executorService = new ThreadPoolExecutor(
                 CORE_POOL_SIZE,
-                CORE_POOL_SIZE,
+                MAX_POOL_SIZE,
                 KEEP_ALIVE_TIME,
                 TimeUnit.MILLISECONDS,
                 new LinkedBlockingQueue<>(QUEUE_SIZE)
