e075b39a9aea82c0735de2d0222262a59c4f9d7b
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-08-12 15:56:34 +0900

    refactor: 로그인 여부를 쿠키가 아닌 세션을 통해 확인하도록 수정

--- src/main/java/mvc/controller/UserListController.java
+++ src/main/java/mvc/controller/UserListController.java
@@ -3,6 +3,7 @@ package mvc.controller;
 import db.DataBase;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import webserver.session.HttpSession;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -12,7 +13,7 @@ public class UserListController extends AbstractController {
 
     @Override
     public void doGet(HttpRequest request, HttpResponse response) throws IOException, URISyntaxException {
-        if (isLogin(request)) {
+        if (isLogin(request.getSession())) {
             response.addBodyAttribute("users", DataBase.findAll());
             render(request, response, USER_LIST_TEMPLATE);
             return;
@@ -20,7 +21,8 @@ public class UserListController extends AbstractController {
         response.sendRedirect("/user/login.html");
     }
 
-    private boolean isLogin(HttpRequest request) {
-        return request.getCookieValue("logined").equals("true");
+    private boolean isLogin(HttpSession session) {
+        Object user = session.getAttribute("user");
+        return user != null;
     }
 }
