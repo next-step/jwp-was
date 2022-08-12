1fe501cac288a4a5d2798c237e3843a20fb0d792
rheew <47098896+rheew@users.noreply.github.com>
2022-08-12 18:32:44 +0900

    refactor: - HttpHeader 자료형 변경 Map<String, String> -> getHeader(“필드 이름”) 메소드를 통해 접근 기능 - requestLine 추가

---
+++ src/main/java/model/ResponseLine.java
@@ -0,0 +1,48 @@
+package model;
+
+import webserver.Protocol;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ResponseLine {
+    public static final String EMPTY_SPACE = " ";
+    private Protocol protocol;
+    private String status;
+    private String message;
+
+    public ResponseLine(Protocol protocol, String status, String message) {
+        this.protocol = protocol;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static ResponseLine redirect() {
+        return new ResponseLine(new Protocol("HTTP/1.1"), "302", "OK");
+    }
+
+    public static ResponseLine success() {
+        return new ResponseLine(new Protocol("HTTP/1.1"), "200", "OK");
+    }
+
+    public String getInfo() {
+        return getProtocolInfo() + EMPTY_SPACE + status + EMPTY_SPACE + message;
+    }
+
+    private String getProtocolInfo() {
+        return protocol.getProtocol() + "/" + protocol.getVersion();
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseLine{" +
+                "protocol=" + protocol +
+                ", status='" + status + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+    public void writeOutput(DataOutputStream dos) throws IOException {
+        dos.writeBytes(getInfo() + "\n");
+    }
+}
