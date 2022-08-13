93aad121d531117eecd15c6925e62de8ddec01d6
fistkim101 <fistkim101@gmail.com>
2022-08-13 10:55:49 +0900

    feat : step5 요구사항 1 구현

---
+++ src/main/java/configuration/ThreadConfiguration.java
@@ -0,0 +1,18 @@
+package configuration;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadConfiguration {
+
+    public static final Executor serviceThreadPool = new ThreadPoolExecutor(
+            250,
+            250,
+            0,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<>(100)
+    );
+
+}
