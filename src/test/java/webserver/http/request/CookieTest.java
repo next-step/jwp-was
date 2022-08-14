1c4e1495bc8f2de9a2d173dfbd46bb354b256bb2
Jiwon-Yang <jiwonyang.tech@gmail.com>
2022-08-15 04:01:43 +0900

    feat: Cookie tc 추가

---
+++ src/test/java/webserver/http/request/CookieTest.java
@@ -0,0 +1,17 @@
+package webserver.http.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CookieTest {
+
+    @DisplayName("쿠키를 파싱하고, 쿠키값을 검증한다.")
+    @Test
+    void parseAndGetLoginCookie() {
+        Cookie cookie = Cookie.parseFrom("logined=true");
+
+        assertEquals("true", cookie.getLogined());
+    }
+}
