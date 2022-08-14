99e2c5348cd39c0db6f5bd26489d3c5fd9ff018b
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-08-14 17:18:52 +0900

    feat (1.Apply Thread Pool): Apply thread pools based on requirements
    * WebApplicationServer: 요구사항에 맞춰 ThreadPool 생성

--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,11 +23,12 @@ public class WebApplicationServer {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
+            BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(100);
+            ExecutorService executorService = new ThreadPoolExecutor(100, 250, 60, TimeUnit.SECONDS, workQueue);
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executorService.execute(new RequestHandler(connection));
             }
         }
     }
