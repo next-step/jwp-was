027426ae8d7ea7128f6827ef69bfb5e4fc243a69
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-08-15 09:16:30 +0900

    Step5 리뷰 요청 드립니다! (#371)

--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +23,14 @@ public class WebApplicationServer {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
+            BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(5);
+            ExecutorService executorService = new ThreadPoolExecutor(5, 10, 60, TimeUnit.SECONDS, workQueue);
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executorService.execute(new RequestHandler(connection));
             }
+            executorService.shutdown();
         }
     }
 }
--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,17 +1,48 @@
 package webserver;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestTest {
+    private static final Logger logger = LoggerFactory.getLogger(ExecutorsTest.class);
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
     @Test
     void request_resttemplate() {
+        logger.info("start_request_rest_template");
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/index.html", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        logger.info("end_request_rest_template");
+    }
+
+    @DisplayName("rest template을 통해 threadPool이 5로 되어있는 환경에 10개의 스레드 요청을 한다.")
+    @Test
+    void executeMultiRequest() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(15);
+
+        Future<?> future = null;
+        for (int i = 0; i < 100; i++) {
+            future = executorService.submit(() -> {
+                int idx = counter.addAndGet(1);
+                request_resttemplate();
+                logger.info("Thread {}", idx);
+            });
+        }
+        future.get();
+        executorService.shutdown();
     }
 }
