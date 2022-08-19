306d092738d6a46d18451f9d165fc9db9b2d466b
mincheolkk <kmc04080408@gmail.com>
2022-08-20 01:24:26 +0900

    refactor : 일부 변경

--- src/main/java/webserver/RequestBody.java
+++ src/main/java/webserver/RequestBody.java
@@ -11,12 +11,18 @@ import java.util.Map;
 public class RequestBody {
     private String body;
 
+    private Map<String, String> parameter;
     public RequestBody() {
+    }
+
+    public RequestBody(Map<String, String> parameter) {
+        this.parameter = parameter;
     }
 
     private static final Logger logger = LoggerFactory.getLogger(RequestBody.class);
 
     public void toBody(BufferedReader bufferedReader, HttpHeader httpHeader) throws IOException {
+
         String body = IOUtils.readData(bufferedReader, Integer.parseInt(httpHeader.getValue("Content-Length")));
 
         logger.debug("request Body : {}", body);
@@ -24,12 +30,22 @@ public class RequestBody {
         this.body = body;
     }
 
-    public Map<String, String> getParameters(String body) {
+    public Map<String, String> createParameter(String body) {
         QueryString queryString = QueryString.parse(body);
-        return queryString.getQueryData();
+        return this.parameter = queryString.getQueryData();
     }
 
     public String getBody() {
         return body;
     }
+
+    public Map<String, String> getParameter() {
+        return parameter;
+    }
+
+    public String getParameterValue(String name) {
+        return parameter.get(name);
+    }
+
+
 }
