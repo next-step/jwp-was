7ec36706c764260af54b3be91706f7a726373b6f
younlll <youn.lee96@gmail.com>
2022-08-21 00:14:51 +0900

    feat: index 및 회원가입 페이지 리다리랙션

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import db.DataBase;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -7,14 +8,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import model.HttpPath;
+import java.util.Map;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String PATH_DELIMITER = "/";
 
     private Socket connection;
 
@@ -37,13 +41,39 @@ public class RequestHandler implements Runnable {
                 logger.debug("header : {}", line);
             }
 
+            DataOutputStream dos = new DataOutputStream(out);
+
             String path = requestLine.getHttpPath().getPath();
             logger.debug("http path : {}", path);
 
-            DataOutputStream dos = new DataOutputStream(out);
+            RequestPathQueryString requestPathQueryString = requestLine.getRequestPathQueryString();
+            Map<String, String> queryStringOfPath = requestPathQueryString.getQueryStringOfPath();
+            if (path.endsWith(".html")) {
+                path = "./templates" + path;
+                byte[] body = FileIoUtils.loadFileFromClasspath(path);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else if ("/user/create".equals(path)) {
+                User user = new User(queryStringOfPath.get("userId"), queryStringOfPath.get("password"), queryStringOfPath.get("name"), queryStringOfPath.get("email"));
+                DataBase.addUser(user);
+
+                response302Header(dos, "index.html");
+            }
+
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            responseHeader(dos, "Location", PATH_DELIMITER + location);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -55,6 +85,14 @@ public class RequestHandler implements Runnable {
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void responseHeader(DataOutputStream dos, String key, String value) {
+        try {
+            dos.writeBytes(key + ": " + value + "\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
