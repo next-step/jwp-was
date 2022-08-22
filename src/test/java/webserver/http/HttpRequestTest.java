42fcbc25c433ce6b0e73f3762bcd17134609bc46
skid901 <tlrudejr1993@gmail.com>
2022-08-23 01:12:59 +0900

    refactor: modify test codes to use assertAll method

--- src/test/java/webserver/http/HttpRequestTest.java
+++ src/test/java/webserver/http/HttpRequestTest.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class HttpRequestTest {
     private final String testDirectory = "./src/test/resources/";
@@ -16,10 +17,12 @@ class HttpRequestTest {
         InputStream in = new FileInputStream(testDirectory + "Http_GET.txt");
         HttpRequest request = HttpRequest.from(in);
 
-        assertThat(request.getMethod()).isEqualTo(HttpMethod.GET);
-        assertThat(request.getPath()).isEqualTo("/user/create");
-        assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
-        assertThat(request.getParameter("userId")).isEqualTo("javajigi");
+        assertAll(
+                () -> assertThat(request.getMethod()).isEqualTo(HttpMethod.GET),
+                () -> assertThat(request.getPath()).isEqualTo("/user/create"),
+                () -> assertThat(request.getHeader("Connection")).isEqualTo("keep-alive"),
+                () -> assertThat(request.getParameter("userId")).isEqualTo("javajigi")
+        );
     }
 
     @Test
@@ -27,10 +30,12 @@ class HttpRequestTest {
         InputStream in = new FileInputStream(testDirectory + "Http_POST.txt");
         HttpRequest request = HttpRequest.from(in);
 
-        assertThat(request.getMethod()).isEqualTo(HttpMethod.POST);
-        assertThat(request.getPath()).isEqualTo("/user/create");
-        assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
-        assertThat(request.getParameter("userId")).isEqualTo("javajigi");
+        assertAll(
+                () -> assertThat(request.getMethod()).isEqualTo(HttpMethod.POST),
+                () -> assertThat(request.getPath()).isEqualTo("/user/create"),
+                () -> assertThat(request.getHeader("Connection")).isEqualTo("keep-alive"),
+                () -> assertThat(request.getParameter("userId")).isEqualTo("javajigi")
+        );
     }
 
     @Test
@@ -38,10 +43,12 @@ class HttpRequestTest {
         InputStream in = new FileInputStream(testDirectory + "Http_POST2.txt");
         HttpRequest request = HttpRequest.from(in);
 
-        assertThat(request.getMethod()).isEqualTo(HttpMethod.POST);
-        assertThat(request.getPath()).isEqualTo("/user/create");
-        assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
-        assertThat(request.getParameter("id")).isEqualTo("1");
-        assertThat(request.getParameter("userId")).isEqualTo("javajigi");
+        assertAll(
+                () -> assertThat(request.getMethod()).isEqualTo(HttpMethod.POST),
+                () -> assertThat(request.getPath()).isEqualTo("/user/create"),
+                () -> assertThat(request.getHeader("Connection")).isEqualTo("keep-alive"),
+                () -> assertThat(request.getParameter("id")).isEqualTo("1"),
+                () -> assertThat(request.getParameter("userId")).isEqualTo("javajigi")
+        );
     }
 }
