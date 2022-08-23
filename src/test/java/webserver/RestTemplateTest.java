92651a77dc9364c4d8f271314310e79bfae9d03b
koola97620 <koola97620@nate.com>
2022-08-24 00:08:52 +0900

    feat: 스레드풀 구현

--- src/test/java/webserver/RestTemplateTest.java
+++ src/test/java/webserver/RestTemplateTest.java
@@ -1,5 +1,9 @@
 package webserver;
 
+import db.DataBase;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +12,39 @@ import org.springframework.web.client.RestTemplate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RestTemplateTest {
+
+    private static final Thread WAS_SERVER = new Thread(() -> {
+        String[] args = {"8080", "5", "10"};
+        try {
+            WebApplicationServer.main(args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    });
+
+    @BeforeAll
+    static void beforeAll() {
+        WAS_SERVER.start();
+    }
+
+    @BeforeEach
+    void setUp() {
+        DataBase.deleteAll();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        WAS_SERVER.interrupt();
+    }
+
     @Test
-    void request_resttemplate() {
+    void test() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        for (int i = 0; i < 20; i++) {
+            ResponseEntity<String> response =
+                    restTemplate.postForEntity("http://localhost:8080/user/create", "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net", String.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+        }
     }
 
     @Test
