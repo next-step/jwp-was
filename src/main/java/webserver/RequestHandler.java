850129505607f358e7d5ac7020760eff024fb668
이정수 <lsecret@naver.com>
2022-08-28 19:06:49 +0900

    feat: 로그인 결과 쿠키에 저장

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,7 +4,6 @@ import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.FileIoUtils;
 import utils.IOUtils;
 import webserver.http.domain.RequestBody;
 import webserver.http.domain.RequestHeader;
@@ -16,11 +15,13 @@ import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import static utils.FileIoUtils.*;
+
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     public static final byte[] HELLO_WORLD = "Hello World".getBytes();
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -37,51 +38,69 @@ public class RequestHandler implements Runnable {
             requestHeader.addRequestHeaders(br);
 
             RequestUrl requestUrl = new RequestUrl(url);
-            byte[] body = checkIndex(requestUrl);
-            body = checkSignUpForm(body, requestUrl);
-            boolean signUpComplete = checkSignUp(requestUrl, br, requestHeader);
-
+            byte[] body = HELLO_WORLD;
             DataOutputStream dos = new DataOutputStream(out);
 
-            if (signUpComplete) {
+            if (checkGetIndexHtml(requestUrl)) {
+                body = loadFileFromClasspath("./templates/index.html");
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else if (checkGetFormHtml(requestUrl)) {
+                body = loadFileFromClasspath("./templates/user/form.html");
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else if (checkSignUp(requestUrl)) {
+                Map<String, String> bodies = bodies(br, requestHeader);
+                String userId = bodies.get("userId");
+                DataBase.addUser(new User(userId, bodies.get("password"), bodies.get("name"), bodies.get("email")));
                 response302Header(dos, "/index.html");
+            } else if (checkLoginHtml(requestUrl)) {
+                body = loadFileFromClasspath("./templates/user/login.html");
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else if (checkLogin(requestUrl)) {
+                Map<String, String> bodies = bodies(br, requestHeader);
+                String userId = bodies.get("userId");
+                User user = DataBase.findUserById(userId);
+
+                if (user.getPassword().equals(bodies.get("password"))) {
+                    response302Header(dos, "/index.html");
+                    responseCookie(dos, "logined", "true");
+                    return;
+                }
+                response302Header(dos, "/login_failed.html");
+                responseCookie(dos, "logined", "false");
             }
 
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private byte[] checkIndex(RequestUrl requestUrl) throws IOException, URISyntaxException {
-        if (requestUrl.isGetMethod() && requestUrl.samePath("/index.html")) {
-            return FileIoUtils.loadFileFromClasspath("./templates/index.html");
-        }
-        return HELLO_WORLD;
+    private boolean checkGetIndexHtml(RequestUrl requestUrl) throws IOException, URISyntaxException {
+        return requestUrl.isGetMethod() && requestUrl.samePath("/index.html");
     }
 
-    private byte[] checkSignUpForm(byte[] body, RequestUrl requestUrl) throws IOException, URISyntaxException {
-        if (requestUrl.isGetMethod() && requestUrl.samePath("/user/form.html")) {
-            return FileIoUtils.loadFileFromClasspath("./templates/user/form.html");
-        }
-        return body;
+    private boolean checkGetFormHtml(RequestUrl requestUrl) throws IOException, URISyntaxException {
+        return requestUrl.isGetMethod() && requestUrl.samePath("/user/form.html");
     }
 
-    private boolean checkSignUp(RequestUrl requestUrl, BufferedReader br, RequestHeader requestHeader) throws IOException, URISyntaxException {
-        if (requestUrl.isPostMethod() && requestUrl.samePath("/user/create")) {
-            String body = IOUtils.readData(br, Integer.parseInt(requestHeader.getValue("Content-Length")));
-            RequestBody requestBody = new RequestBody(body);
-            Map<String, String> bodies = requestBody.bodies();
-            String userId = bodies.get("userId");
+    private boolean checkSignUp(RequestUrl requestUrl) throws IOException, URISyntaxException {
+        return requestUrl.isPostMethod() && requestUrl.samePath("/user/create");
+    }
 
-            DataBase.addUser(new User(userId, bodies.get("password"), bodies.get("name"), bodies.get("email")));
+    private boolean checkLoginHtml(RequestUrl requestUrl) throws IOException, URISyntaxException {
+        return requestUrl.isGetMethod() && requestUrl.samePath("/user/login.html");
+    }
 
-            logger.debug(DataBase.findUserById(userId).toString());
-            return true;
-        }
+    private boolean checkLogin(RequestUrl requestUrl) throws IOException {
+        return requestUrl.isPostMethod() && requestUrl.samePath("/user/login");
+    }
 
-        return false;
+    private Map<String, String> bodies(BufferedReader br, RequestHeader requestHeader) throws IOException {
+        String body = IOUtils.readData(br, Integer.parseInt(requestHeader.getValue("Content-Length")));
+        RequestBody requestBody = new RequestBody(body);
+        return requestBody.bodies();
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -100,6 +119,16 @@ public class RequestHandler implements Runnable {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
             dos.writeBytes("Location: " + location + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void responseCookie(DataOutputStream dos, String key, String value) {
+        try {
+            String cookie = "Set-Cookie: " + key + "=" + value + "; Path=/";
+            logger.debug(cookie);
+            dos.writeBytes(cookie);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
