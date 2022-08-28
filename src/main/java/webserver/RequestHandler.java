5e7d3a68826ddc4bb24ee14bf4107797c4104c61
이정수 <lsecret@naver.com>
2022-08-28 12:39:18 +0900

    feat: 회원 가입 메뉴를 통하여 회원 가입한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,7 @@
 package webserver;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
@@ -9,9 +11,11 @@ import java.io.*;
 import java.net.Socket;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    public static final byte[] HELLO_WORLD = "Hello World".getBytes();
 
     private Socket connection;
 
@@ -26,7 +30,10 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
             RequestLine requestLine = new RequestLine(br.readLine());
+
             byte[] body = checkIndex(requestLine);
+            body = checkSignUpForm(body, requestLine);
+            checkSignUp(requestLine);
 
             DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
@@ -40,7 +47,25 @@ public class RequestHandler implements Runnable {
         if (requestLine.path().equals("/index.html")) {
             return FileIoUtils.loadFileFromClasspath("./templates/index.html");
         }
-        return "Hello World".getBytes();
+        return HELLO_WORLD;
+    }
+
+    private byte[] checkSignUpForm(byte[] body, RequestLine requestLine) throws IOException, URISyntaxException {
+        if (requestLine.path().equals("/user/form.html")) {
+            return FileIoUtils.loadFileFromClasspath("./templates/user/form.html");
+        }
+        return body;
+    }
+
+    private void checkSignUp(RequestLine requestLine) throws IOException, URISyntaxException {
+        if (requestLine.path().equals("/user/create")) {
+            Map<String, String> requestParams = requestLine.requestParams();
+            String userId = requestParams.get("userId");
+
+            DataBase.addUser(new User(userId, requestParams.get("password"), requestParams.get("name"), requestParams.get("email")));
+
+            logger.debug(DataBase.findUserById(userId).toString());
+        }
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
