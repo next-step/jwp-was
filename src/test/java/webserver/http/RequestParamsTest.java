8839a92b58a44c318ae7f445b75d0d626d898377
akayj820 <akayj820@gmail.com>
2022-08-30 17:35:33 +0900

    fix: @Test 애노테이션 추가

--- src/test/java/webserver/http/RequestParamsTest.java
+++ src/test/java/webserver/http/RequestParamsTest.java
@@ -1,11 +1,13 @@
 package webserver.http;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RequestParamsTest {
 	@DisplayName("RequestParas 생성 후 값 조회 테스트")
+	@Test
 	void value_test() {
 		RequestParams params = new RequestParams("userId=javajigi", "password=password&name=JaeSung");
 		assertThat(params.value("userId")).isEqualTo("javajigi");
