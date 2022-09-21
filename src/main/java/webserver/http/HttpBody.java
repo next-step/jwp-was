a0524202f1b88dc6f7c86c2aebb1bfad8227cb58
limhangyeol <hang19663@gmail.com>
2022-09-21 17:24:32 +0900

    refactor: HttpBody 개선
    * HttpBody는 비어있을 수 있다.
    * QueryString은 null을 제한하여, 새로운 HttpBody의 책임을 가진 Map 객체를 구현한다.

--- src/main/java/webserver/http/HttpBody.java
+++ src/main/java/webserver/http/HttpBody.java
@@ -1,48 +1,54 @@
 package webserver.http;
 
-import exception.ValidateException;
 import utils.IOUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class HttpBody {
 
-    private final QueryString queryString;
+    private static final String DELIMITER_KEY = "&";
+    private static final String DELIMITER_VALUE = "=";
+    private static final int KEY = 0;
+    private static final int VALUE = 1;
 
-    private HttpBody(String request) {
-        validate(request);
-        this.queryString = new QueryString(request);
-    }
+    private final Map<String, String> contents;
 
-    public static HttpBody from(String request) {
-        return new HttpBody(request);
+    private HttpBody(Map<String, String> contents) {
+        this.contents = contents;
     }
 
     public static HttpBody of(BufferedReader bufferedReader, int contentLength) throws IOException {
         String request = IOUtils.readData(bufferedReader, contentLength);
-        return new HttpBody(request);
+        return HttpBody.from(request);
     }
 
-    private void validate(String request) {
+    public static HttpBody from(String request) {
         if (request == null || request.length() == 0) {
-            throw new ValidateException();
+            return HttpBody.empty();
         }
+        Map<String, String> contents = Arrays.stream(request.split(DELIMITER_KEY))
+                .map(it -> it.split(DELIMITER_VALUE))
+                .collect(Collectors.toMap(e -> e[KEY], e -> e[VALUE]));
+        return new HttpBody(contents);
     }
 
-    public QueryString getQueryString() {
-        return queryString;
+    public static HttpBody empty() {
+        return new HttpBody(Collections.emptyMap());
     }
 
-    public Map<String, String> getQueryStringMap() {
-        return queryString.getParameterMap();
+    public Map<String, String> getContents() {
+        return contents;
     }
 
     @Override
     public String toString() {
         return "HttpBody{" +
-                "queryString=" + queryString +
+                "contents=" + contents +
                 '}';
     }
 }
--- src/test/java/webserver/http/HttpBodyTest.java
+++ src/test/java/webserver/http/HttpBodyTest.java
@@ -1,13 +1,11 @@
 package webserver.http;
 
-import exception.ValidateException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("Http Body 테스트")
 class HttpBodyTest {
@@ -19,18 +17,19 @@ class HttpBodyTest {
 
         HttpBody actual = HttpBody.from(input);
 
-        assertThat(actual.getQueryStringMap()).hasSize(4);
-        assertThat(actual.getQueryStringMap().get("userId")).isEqualTo("javajigi");
-        assertThat(actual.getQueryStringMap().get("password")).isEqualTo("password");
-        assertThat(actual.getQueryStringMap().get("name")).isEqualTo("JaeSung");
-        assertThat(actual.getQueryStringMap().get("email")).isEqualTo("javajigi@slipp.net");
+        assertThat(actual.getContents()).hasSize(4);
+        assertThat(actual.getContents().get("userId")).isEqualTo("javajigi");
+        assertThat(actual.getContents().get("password")).isEqualTo("password");
+        assertThat(actual.getContents().get("name")).isEqualTo("JaeSung");
+        assertThat(actual.getContents().get("email")).isEqualTo("javajigi@slipp.net");
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    @DisplayName("Body 에 null 혹은 빈값이 들어올 경우 파싱하지 않는다.")
+    @DisplayName("Body 에 null 혹은 빈값이 들어올 경우 빈 ResponseBody를 응답한다..")
     void parseFailedWhenInvalidInput(String input) {
-        assertThatExceptionOfType(ValidateException.class)
-                .isThrownBy(() -> HttpBody.from(input));
+        HttpBody actual = HttpBody.from(input);
+
+        assertThat(actual.getContents()).isEmpty();
     }
 }
