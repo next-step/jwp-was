0497ab8b44a210c3886cb4c43bc62ea859d753ae
limhangyeol <hang19663@gmail.com>
2022-09-22 12:16:37 +0900

    refactor: HttpHeader 추가

--- src/main/java/webserver/http/HttpHeader.java
+++ src/main/java/webserver/http/HttpHeader.java
@@ -3,6 +3,11 @@ package webserver.http;
 public class HttpHeader {
 
     public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String LOCATION = "Location";
+    public static final String COOKIE = "Cookie";
+    public static final String SET_COOKIE = "Set-Cookie";
+    public static final String CONTENT_TYPE = "text/html";
+
 
     private HttpHeader() {
     }
