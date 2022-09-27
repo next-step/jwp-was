79c4e5a626142835088e450e70dc9cad7a59f93f
limhangyeol <hang19663@gmail.com>
2022-09-27 21:32:45 +0900

    feature: ResourceServlet 구현
    * static, template resource 요청을 담당하는 Servlet을 구현한다.

--- src/main/java/webserver/servlet/DispatcherServlet.java
+++ src/main/java/webserver/servlet/DispatcherServlet.java
@@ -1,11 +1,7 @@
 package webserver.servlet;
 
-import webserver.http.HttpHeader;
-import webserver.http.HttpHeaders;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
-import webserver.http.response.ResponseBody;
-import webserver.http.response.ResponseLine;
 
 public enum DispatcherServlet {
     INSTANCE;
@@ -16,19 +12,6 @@ public enum DispatcherServlet {
         if (requestMappingHandler.match(httpRequest)) {
             return requestMappingHandler.doService(httpRequest);
         }
-        return doStaticService(httpRequest);
-    }
-
-    private HttpResponse doStaticService(HttpRequest httpRequest) {
-        ResponseLine responseLine = ResponseLine.ok();
-
-        String filePath = httpRequest.getRequestLine().getPathValue();
-        ResponseBody responseBody = ResponseBody.from(filePath);
-
-        HttpHeaders httpHeaders = HttpHeaders.init();
-        httpHeaders.addResponseHeader(HttpHeader.CONTENT_TYPE, "text/html;charset=utf-8");
-        httpHeaders.addResponseHeader(HttpHeader.CONTENT_LENGTH, String.valueOf(responseBody.getContentsLength()));
-
-        return new HttpResponse(responseLine, httpHeaders, responseBody);
+        return new ResourceServlet().service(httpRequest);
     }
 }
