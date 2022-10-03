8f32eb9d7092b7880152850d2e3d0fd4459f0421
limhangyeol <hang19663@gmail.com>
2022-10-03 22:22:24 +0900

    test: HttpResponse 테스트 작성

--- src/test/java/webserver/http/response/HttpResponseTest.java
+++ src/test/java/webserver/http/response/HttpResponseTest.java
@@ -2,13 +2,24 @@ package webserver.http.response;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import webserver.http.HttpHeader;
 import webserver.http.HttpHeaders;
 import webserver.http.HttpStatus;
+import webserver.http.MediaType;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Http Response 테스트")
 class HttpResponseTest {
+
+    private static final String VIEW_RESOURCES_PATH = "./templates";
 
     @Test
     @DisplayName("Response: 200 OK 일 경우")
@@ -48,5 +59,46 @@ class HttpResponseTest {
 
         assertThat(actual.getResponseLine().toString()).isEqualTo("HTTP/1.1 405 Method Not Allowed");
         assertThat(actual.getBody().getContents()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("정적 리소스를 읽어 응답으로 보내는 forward 응답 Body 에는 index.html 이 포함되어 있어야 한다.")
+    void responseForward() throws URISyntaxException, IOException {
+        MediaType mediaType = MediaType.HTML;
+        byte[] index = getPageToBytes();
+
+        HttpResponse httpResponse = HttpResponse.forward(mediaType, VIEW_RESOURCES_PATH, "/index.html");
+
+        assertThat(httpResponse.getBody().getContentsLength()).isEqualTo(6988);
+        assertThat(httpResponse.getBody().getContents()).isEqualTo(index);
+    }
+
+    @Test
+    @DisplayName("redirect 응답 시 Location Header 정보가 /index.html 로 포함되어 있어야 한다.")
+    void responseRedirect() {
+        HttpHeaders httpHeaders = HttpHeaders.redirect("/index.html");
+
+        HttpResponse httpResponse = HttpResponse.redirect(httpHeaders);
+
+        assertThat(httpResponse.getHeaders().getHeader(HttpHeader.LOCATION)).contains("/index.html");
+    }
+
+    @Test
+    @DisplayName("redirect 응답 시 Header 에 Set-Cookie 값으로 logined=true 값이 포함되어 있어야 한다.")
+    void responseCookies() {
+        HttpHeaders httpHeaders = HttpHeaders.redirect("/index.html");
+        httpHeaders.addResponseHeader(HttpHeader.SET_COOKIE, "logined=true; Path=/");
+
+        HttpResponse httpResponse = HttpResponse.redirect(httpHeaders);
+
+        assertThat(httpResponse.getHeaders().getHeader(HttpHeader.LOCATION)).contains("/index.html");
+        assertThat(httpResponse.getHeaders().getHeader(HttpHeader.SET_COOKIE)).contains("logined=true");
+    }
+
+    private byte[] getPageToBytes() throws IOException, URISyntaxException {
+        Path path = Paths.get(
+                Objects.requireNonNull(this.getClass().getClassLoader().getResource(VIEW_RESOURCES_PATH + "/index.html")).toURI()
+        );
+        return Files.readAllBytes(path);
     }
 }
