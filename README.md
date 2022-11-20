36cae54dbe11cc9adb1ee993ea211f44eb253a46
ljh0326 <hun2@tossInsu.com>
2022-11-20 09:22:44 +0900

    setp5 (#412)
    Co-authored-by: hun2 <hun2@woowahan.com>

--- README.md
+++ README.md
@@ -68,3 +68,18 @@
 * ~~파싱하는 로직 구현을 TDD로 구현한다.~~
 ### 4. enum 적용 (선택)
 * ~~HTTP method인 GET, POST를 enum으로 구현한다.~~
+
+## 5단계 - Thread Pool 적용
+
+### 요구사항 1
+
+- 최대 ThradPool의 크기는 250
+- 모든 Thread가 사용 중인(Busy) 상태면 100명까지 대기 상태
+
+
+### 요구사항 2
+
+- 서버의 ThreadPool 수보다 많은 요청을 동시에 보내는 테스트 작성
+
+> 예를 들어 서버의 최대 Thread Pool 수를 5로 설정  
+> ExecutorsTest의 Executors.newFixedThreadPool(10)과 같이 설정해 동시에 10개의 요청
--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -5,10 +5,16 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class WebApplicationServer {
     private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
     private static final int DEFAULT_PORT = 8090;
+
+    private static final int THREAD_POOL_SIZE = 250;
+    private static final int THREAD_QUEUE_SIZE = 100;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -17,7 +23,7 @@ public class WebApplicationServer {
         } else {
             port = Integer.parseInt(args[0]);
         }
-
+        ThreadPoolExecutor threadPoolExecutor =  threadPoolExecutor(getPoolSize(), getQueueCapacity());
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -25,9 +31,19 @@ public class WebApplicationServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                threadPoolExecutor.execute(new RequestHandler(connection));
             }
         }
+    }
+
+    private static ThreadPoolExecutor threadPoolExecutor(int pollSize, int queueCapacity) {
+        return new ThreadPoolExecutor(pollSize, pollSize, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(queueCapacity));
+    }
+    private static int getPoolSize() {
+        return THREAD_POOL_SIZE;
+    }
+
+    private static int getQueueCapacity() {
+        return THREAD_QUEUE_SIZE;
     }
 }
